06c6a027d658c21e8244129ed89dc49dc7c7ce2d
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-04-20 11:27:31 -0700

    Remove instance ip from API up down counter to reduce grafana cardinality

--- packages/api/internal/cache/instance/monitoring.go
+++ packages/api/internal/cache/instance/monitoring.go
@@ -9,7 +9,6 @@ import (
 
 func (c *InstanceCache) UpdateCounter(instance InstanceInfo, value int64) {
 	c.counter.Add(context.Background(), value, metric.WithAttributes(
-		attribute.String("instance_id", instance.Instance.SandboxID),
 		attribute.String("env_id", instance.Instance.TemplateID),
 		attribute.String("team_id", instance.TeamID.String()),
 	))
