7f96211655e291adbd069dcf1aaf09d91c1f38a2
Jakub Novak <jakub@e2b.dev>
2024-08-06 15:20:11 +0200

    Remove info from remove method

--- packages/envd/internal/services/filesystem/remove.go
+++ packages/envd/internal/services/filesystem/remove.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"os"
-	"path"
 
 	"github.com/e2b-dev/infra/packages/envd/internal/permissions"
 	rpc "github.com/e2b-dev/infra/packages/envd/internal/services/spec/filesystem"
@@ -18,43 +17,15 @@ func (Service) Remove(ctx context.Context, req *connect.Request[rpc.RemoveReques
 		return nil, err
 	}
 
-	resolvedPath, err := permissions.ExpandAndResolve(req.Msg.GetPath(), u)
+	path, err := permissions.ExpandAndResolve(req.Msg.GetPath(), u)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInvalidArgument, err)
 	}
 
-	entry, err := os.Stat(resolvedPath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			return connect.NewResponse(&rpc.RemoveResponse{
-				Entry: &rpc.EntryInfo{
-					Name: path.Base(resolvedPath),
-					Type: rpc.FileType_FILE_TYPE_UNSPECIFIED,
-					Path: resolvedPath,
-				},
-			}), nil
-		}
-
-		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("error statting file or directory: %w", err))
-	}
-
-	err = os.RemoveAll(resolvedPath)
+	err = os.RemoveAll(path)
 	if err != nil {
 		return nil, connect.NewError(connect.CodeInternal, fmt.Errorf("error removing file or directory: %w", err))
 	}
 
-	var t rpc.FileType
-	if entry.IsDir() {
-		t = rpc.FileType_FILE_TYPE_DIRECTORY
-	} else {
-		t = rpc.FileType_FILE_TYPE_FILE
-	}
-
-	return connect.NewResponse(&rpc.RemoveResponse{
-		Entry: &rpc.EntryInfo{
-			Name: path.Base(resolvedPath),
-			Type: t,
-			Path: resolvedPath,
-		},
-	}), nil
+	return connect.NewResponse(&rpc.RemoveResponse{}), nil
 }
