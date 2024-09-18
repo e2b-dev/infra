4d24c1377e905ffe8ea26cb7287b0a39f940526a
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-09-18 21:00:56 +0000

    Add script for migrating to storage

---
+++ packages/template-manager/upload-envs.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Upload envs from disk to GCS
+
+# Get first argument as target dir name
+TARGET_DIR_NAME=$1
+
+# Get second argument as template name
+TEMPLATE_BUCKET_NAME=$2
+
+echo "Uploading envs from ${TARGET_DIR_NAME} to GCS"
+
+COMMAND="gcloud storage cp --verbosity error"
+
+# Initialize counter for uploaded envs
+uploaded_env_count=0
+total_envs=$(ls ${TARGET_DIR_NAME} | wc -l)
+
+# Record the start time
+start_time=$(date +%s)
+
+# iterate over all directories in the target dir
+for template_id in $(ls ${TARGET_DIR_NAME}); do
+  # Increment the counter for uploaded envs
+  uploaded_env_count=$((uploaded_env_count + 1))
+
+  echo -e "\n------------------${uploaded_env_count}/${total_envs}-----------------"
+  echo "Uploading env ${template_id}"
+  # Get the build id by printing the content of build_id text file, skip dir if not build_id file exists
+  if [ ! -f "${TARGET_DIR_NAME}/${template_id}/build_id" ]; then
+    echo "Skip ${template_id} because build_id file does not exist"
+    continue
+  fi
+
+  BUILD_ID=$(cat ${TARGET_DIR_NAME}/${template_id}/build_id)
+  echo "Build ID: ${BUILD_ID}"
+
+  # Upload env to GCS via gcloud storage cp, copy only "memfile", "rootfs.ext4" and "snapfile" from the dir
+  # First get and print the paths to the files
+  MEMFILE_PATH=$(ls ${TARGET_DIR_NAME}/${template_id}/memfile)
+  ROOTFS_EXT4_PATH=$(ls ${TARGET_DIR_NAME}/${template_id}/rootfs.ext4)
+  SNAPFILE_PATH=$(ls ${TARGET_DIR_NAME}/${template_id}/snapfile)
+
+  # Check if files exist
+  if [ ! -f "${MEMFILE_PATH}" ]; then
+    echo "Skip ${template_id} because memfile does not exist"
+    continue
+  fi
+
+  if [ ! -f "${ROOTFS_EXT4_PATH}" ]; then
+    echo "Skip ${template_id} because rootfs.ext4 does not exist"
+    continue
+  fi
+
+  if [ ! -f "${SNAPFILE_PATH}" ]; then
+    echo "Skip ${template_id} because snapfile does not exist"
+    continue
+  fi
+
+  BUCKET_MEMFILE_PATH="gs://${TEMPLATE_BUCKET_NAME}/${template_id}/${BUILD_ID}/memfile"
+  BUCKET_ROOTFS_EXT4_PATH="gs://${TEMPLATE_BUCKET_NAME}/${template_id}/${BUILD_ID}/rootfs.ext4"
+  BUCKET_SNAPFILE_PATH="gs://${TEMPLATE_BUCKET_NAME}/${template_id}/${BUILD_ID}/snapfile"
+
+  # Upload the files
+  echo "Uploading memfile"
+  ${COMMAND} ${MEMFILE_PATH} ${BUCKET_MEMFILE_PATH} &
+
+  echo "Uploading rootfs.ext4"
+  ${COMMAND} ${ROOTFS_EXT4_PATH} ${BUCKET_ROOTFS_EXT4_PATH} &
+
+  echo "Uploading snapfile"
+  ${COMMAND} ${SNAPFILE_PATH} ${BUCKET_SNAPFILE_PATH} &
+
+  # Wait for all background jobs to finish
+  wait
+done
+
+# Print the number of uploaded envs
+echo "Total number of uploaded envs: ${uploaded_env_count}"
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate and print the elapsed time
+elapsed_time=$((end_time - start_time))
+echo "Total elapsed time: ${elapsed_time} seconds"
