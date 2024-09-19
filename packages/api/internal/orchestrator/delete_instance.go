672df6504507ed6f38b1a03e0aed628ff4650f24
Jakub Novak <jakub@e2b.dev>
2024-09-19 02:15:03 +0200

    Fix double delete bug

--- packages/api/internal/orchestrator/delete_instance.go
+++ packages/api/internal/orchestrator/delete_instance.go
@@ -2,35 +2,13 @@ package orchestrator
 
 import (
 	"context"
-	"fmt"
-
-	"github.com/e2b-dev/infra/packages/api/internal/utils"
-	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator"
 )
 
 func (o *Orchestrator) DeleteInstance(ctx context.Context, sandboxID string) error {
-	childCtx, childSpan := o.tracer.Start(ctx, "delete-instance")
+	_, childSpan := o.tracer.Start(ctx, "delete-instance")
 	defer childSpan.End()
 
-	instance, err := o.instanceCache.GetInstance(sandboxID)
-	if err != nil {
-		return fmt.Errorf("failed to get instance '%s': %w", sandboxID, err)
-	}
-
-	nodeID := instance.Instance.ClientID
-	client, err := o.GetClient(nodeID)
-	if err != nil {
-		return fmt.Errorf("failed to get client '%s': %w", nodeID, err)
-	}
-
-	_, err = client.Sandbox.Delete(childCtx, &orchestrator.SandboxRequest{
-		SandboxID: sandboxID,
-	})
-
-	err = utils.UnwrapGRPCError(err)
-	if err != nil {
-		return fmt.Errorf("failed to delete sandbox '%s': %w", sandboxID, err)
-	}
+	o.instanceCache.Kill(sandboxID)
 
 	return nil
 }
