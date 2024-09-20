1d65cb1fd3d226855d5a6b7bd58c2817f36f16af
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-09-20 21:19:04 +0000

    Simplify kernel upload

--- packages/fc-kernels/upload.sh
+++ packages/fc-kernels/upload.sh
@@ -4,19 +4,6 @@ set -euo pipefail
 
 GCP_PROJECT_ID=$1
 
-# Check if the file exists
-if [ -f "kernel_versions.txt" ]; then
-  # Read kernel versions from the file
-  while IFS= read -r version; do
-    # Upload kernel to GCP bucket
-    gsutil -h "Cache-Control:no-cache, max-age=0" cp -r "builds/vmlinux-${version}" "gs://${GCP_PROJECT_ID}-fc-kernels"
-
-    rm -rf "builds/${version}"
-  done <"kernel_versions.txt"
-
-  echo "All kernels uploaded to GCP bucket successfully."
-else
-  echo "Error: kernel_versions.txt not found."
-fi
+gsutil -h "Cache-Control:no-cache, max-age=0" cp -r "builds/*" "gs://${GCP_PROJECT_ID}-fc-kernels"
 
 rm -rf builds/*
