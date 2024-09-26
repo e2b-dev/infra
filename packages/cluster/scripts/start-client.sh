3c04fecd6cd5d09eca3b29d7f794fc50548f0e34
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-09-26 12:01:22 -0700

    Increase the open files limit in start script

--- packages/cluster/scripts/start-client.sh
+++ packages/cluster/scripts/start-client.sh
@@ -10,7 +10,7 @@ set -euo pipefail
 # Inspired by https://alestic.com/2010/12/ec2-user-data-output/
 exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
 
-ulimit -n 65536
+ulimit -n 1048576
 export GOMAXPROCS='nproc'
 
 # --- Mount the persistent disk with Firecracker environments.
