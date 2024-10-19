4522065499851503936a1e02250a1e4b0ecc2fe7
Jakub Novak <jakub@e2b.dev>
2024-10-18 20:58:16 -0700

    Clean up

--- packages/orchestrator/main.go
+++ packages/orchestrator/main.go
@@ -22,7 +22,6 @@ func main() {
 	envID := flag.String("env", "", "env id")
 	instanceID := flag.String("instance", "", "instance id")
 	keepAlive := flag.Int("alive", 0, "keep alive")
-	stressTest := flag.Bool("stress", false, "stress test (instance id required)")
 	count := flag.Int("count", 1, "number of spawned instances")
 
 	port := flag.Int("port", defaultPort, "Port for test HTTP server")
@@ -31,7 +30,7 @@ func main() {
 
 	// If we're running a test, we don't need to start the server
 	if *envID != "" && *instanceID != "" {
-		test.Run(*envID, *instanceID, keepAlive, count, *stressTest)
+		test.Run(*envID, *instanceID, keepAlive, count)
 		return
 	}
 
