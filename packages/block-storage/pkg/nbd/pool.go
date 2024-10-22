0ce73935a18ec6c42ae02f40ead30cf672ade410
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-10-22 04:31:18 +0000

    Ensure ndb is free when releasing slot; Extract nbd pid check

--- packages/block-storage/pkg/nbd/pool.go
+++ packages/block-storage/pkg/nbd/pool.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/bits-and-blooms/bitset"
 )
@@ -21,7 +22,7 @@ type NbdDevicePool struct {
 	mu    sync.Mutex
 }
 
-func getMaxNbdDevices() (int, error) {
+func getMaxNbdDevices() (uint, error) {
 	data, err := os.ReadFile("/sys/module/nbd/parameters/nbds_max")
 
 	if errors.Is(err, os.ErrNotExist) {
@@ -32,12 +33,12 @@ func getMaxNbdDevices() (int, error) {
 		return 0, fmt.Errorf("failed to read nbds_max: %w", err)
 	}
 
-	nbdsMax, err := strconv.Atoi(strings.TrimSpace(string(data)))
+	nbdsMax, err := strconv.ParseUint(strings.TrimSpace(string(data)), 10, 0)
 	if err != nil {
 		return 0, fmt.Errorf("failed to parse nbds_max: %w", err)
 	}
 
-	return nbdsMax, nil
+	return uint(nbdsMax), nil
 }
 
 func NewNbdDevicePool() (*NbdDevicePool, error) {
@@ -51,28 +52,43 @@ func NewNbdDevicePool() (*NbdDevicePool, error) {
 	}
 
 	return &NbdDevicePool{
-		slots: bitset.New(uint(maxDevices)),
+		slots: bitset.New(maxDevices),
 	}, nil
 }
 
 var re = regexp.MustCompile(`^/dev/nbd(\d+)$`)
 
-func (n *NbdDevicePool) getDeviceSlot(path string) (int, error) {
+func (n *NbdDevicePool) getDeviceSlot(path string) (uint, error) {
 	matches := re.FindStringSubmatch(path)
 	if len(matches) != 2 {
 		return 0, fmt.Errorf("invalid nbd path: %s", path)
 	}
 
-	slot, err := strconv.ParseInt(matches[1], 10, 0)
+	slot, err := strconv.ParseUint(matches[1], 10, 0)
 	if err != nil {
 		return 0, fmt.Errorf("failed to parse slot from path: %w", err)
 	}
 
-	return int(slot), nil
+	return uint(slot), nil
 }
 
 func (n *NbdDevicePool) getDevicePath(slot uint) string {
 	return fmt.Sprintf("/dev/nbd%d", slot)
+}
+
+func (n *NbdDevicePool) isDeviceFree(slot uint) (bool, error) {
+	pidFile := fmt.Sprintf("/sys/block/nbd%d/pid", slot)
+
+	_, err := os.Stat(pidFile)
+	if errors.Is(err, os.ErrNotExist) {
+		return true, nil
+	}
+
+	if err != nil {
+		return false, fmt.Errorf("failed to check if device is busy: %w", err)
+	}
+
+	return false, nil
 }
 
 func (n *NbdDevicePool) GetDevice() (string, error) {
@@ -86,14 +102,16 @@ func (n *NbdDevicePool) GetDevice() (string, error) {
 
 	n.slots.Set(slot)
 
-	pidFile := fmt.Sprintf("/sys/block/nbd%d/pid", slot)
-
-	_, err := os.Stat(pidFile)
-	if errors.Is(err, os.ErrNotExist) {
-		return n.getDevicePath(slot), nil
+	free, err := n.isDeviceFree(slot)
+	if err != nil {
+		return "", fmt.Errorf("failed to check if device is free: %w", err)
 	}
 
-	return "", fmt.Errorf("device in use: %s", pidFile)
+	if !free {
+		return "", fmt.Errorf("device in use: %s", n.getDevicePath(slot))
+	}
+
+	return n.getDevicePath(slot), nil
 }
 
 func (n *NbdDevicePool) ReleaseDevice(ctx context.Context, path string) error {
@@ -111,8 +129,30 @@ func (n *NbdDevicePool) ReleaseDevice(ctx context.Context, path string) error {
 		return errors.Join(errs...)
 	}
 
+	ticker := time.NewTicker(100 * time.Millisecond)
+	defer ticker.Stop()
+
+isFree:
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-ticker.C:
+			free, freeErr := n.isDeviceFree(slot)
+			if freeErr != nil {
+				errs = append(errs, fmt.Errorf("failed to check if device is free: %w", freeErr))
+
+				return errors.Join(errs...)
+			}
+
+			if free {
+				break isFree
+			}
+		}
+	}
+
 	n.mu.Lock()
-	n.slots.Clear(uint(slot))
+	n.slots.Clear(slot)
 	n.mu.Unlock()
 
 	return errors.Join(errs...)
