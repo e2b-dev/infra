586fffc23fefe4197e5821a8297c2335f7fa4c3b
Jakub Novak <jakub@e2b.dev>
2024-11-01 14:18:41 -0700

    Fix mock

--- packages/orchestrator/cmd/mock-sandbox/mock.go
+++ packages/orchestrator/cmd/mock-sandbox/mock.go
@@ -8,6 +8,7 @@ import (
 	"strconv"
 	"time"
 
+	"cloud.google.com/go/storage"
 	"golang.org/x/sync/errgroup"
 
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/consul"
@@ -15,6 +16,7 @@ import (
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox"
 	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator"
 	"github.com/e2b-dev/infra/packages/shared/pkg/logs"
+	templateStorage "github.com/e2b-dev/infra/packages/shared/pkg/storage"
 	consulapi "github.com/hashicorp/consul/api"
 
 	"go.opentelemetry.io/otel"
@@ -40,10 +42,15 @@ func main() {
 	dns := dns.New()
 	go dns.Start("127.0.0.4:53")
 
-	consulClient, err := consul.New(context.Background())
+	client, err := storage.NewClient(ctx, storage.WithJSONReads())
 	if err != nil {
-		panic(err)
+		fmt.Fprintf(os.Stderr, "failed to create GCS client: %v\n", err)
+
+		return
 	}
+
+	consulClient, err := consul.New(context.Background())
+	storageBucket := client.Bucket(templateStorage.BucketName)
 
 	networkPool := sandbox.NewNetworkSlotPool(10, 0)
 
@@ -74,6 +81,7 @@ func main() {
 				time.Duration(*keepAlive)*time.Second,
 				networkPool,
 				consulClient,
+				storageBucket,
 			)
 
 			return nil
@@ -96,6 +104,7 @@ func mockSandbox(
 	keepAlive time.Duration,
 	networkPool *sandbox.NetworkSlotPool,
 	consulClient *consulapi.Client,
+	storageBucket *storage.BucketHandle,
 ) {
 	tracer := otel.Tracer(fmt.Sprintf("sandbox-%s", sandboxId))
 	childCtx, _ := tracer.Start(ctx, "mock-sandbox")
@@ -109,6 +118,7 @@ func mockSandbox(
 		consulClient,
 		dns,
 		networkPool,
+		storageBucket,
 		&orchestrator.SandboxConfig{
 			TemplateID:         templateId,
 			FirecrackerVersion: "v1.7.0-dev_8bb88311",
