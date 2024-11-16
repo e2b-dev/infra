7a2b9a7682f3ede4a0b979c1dd65a3ecaacdb21e
Jakub Novak <jakub@e2b.dev>
2024-11-15 16:04:12 -0800

    Remove race condition

--- packages/orchestrator/cmd/mock-sandbox/mock.go
+++ packages/orchestrator/cmd/mock-sandbox/mock.go
@@ -65,14 +65,14 @@ func main() {
 
 	networkPool := network.NewSlotPool(consulClient)
 
+	eg, ctx := errgroup.WithContext(ctx)
+
 	go func() {
 		poolErr := networkPool.Populate(ctx)
 		if poolErr != nil {
 			fmt.Fprintf(os.Stderr, "network pool error: %v\n", poolErr)
 		}
 	}()
-
-	eg, ctx := errgroup.WithContext(ctx)
 
 	for i := 0; i < *count; i++ {
 		fmt.Printf("Starting sandbox %d\n", i)
