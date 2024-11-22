b5f0af516db6b46291ab07e56121ab5c4bcd278e
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-11-22 05:12:39 +0000

    Fix getting default gateway that breaks with too many host routes

--- packages/orchestrator/internal/sandbox/network/host.go
+++ packages/orchestrator/internal/sandbox/network/host.go
@@ -2,8 +2,7 @@ package network
 
 import (
 	"fmt"
-
-	"github.com/vishvananda/netlink"
+	"os/exec"
 
 	"github.com/e2b-dev/infra/packages/shared/pkg/utils"
 )
@@ -15,21 +14,14 @@ const loopbackInterface = "lo"
 var defaultGateway = utils.Must(getDefaultGateway())
 
 func getDefaultGateway() (string, error) {
-	routes, err := netlink.RouteList(nil, netlink.FAMILY_ALL)
+	route, err := exec.Command(
+		"sh",
+		"-c",
+		"ip route show default | awk '{print $5}'",
+	).Output()
 	if err != nil {
-		return "", fmt.Errorf("error fetching routes: %w", err)
+		return "", fmt.Errorf("error fetching default gateway: %w", err)
 	}
 
-	for _, route := range routes {
-		if route.Dst == nil && route.Gw != nil {
-			link, linkErr := netlink.LinkByIndex(route.LinkIndex)
-			if linkErr != nil {
-				return "", fmt.Errorf("error fetching interface for default gateway: %w", linkErr)
-			}
-
-			return link.Attrs().Name, nil
-		}
-	}
-
-	return "", fmt.Errorf("cannot find default gateway")
+	return string(route), nil
 }
