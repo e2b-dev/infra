70bf252a53b13a085c183dff7c78300ea1aa99bb
Jakub Novak <jakub@e2b.dev>
2024-11-23 09:42:02 -0800

    Don't drop the default

--- packages/shared/migrations/20241121225404.sql
+++ packages/shared/migrations/20241121225404.sql
@@ -1,7 +1,6 @@
 -- Modify "team_api_keys" table
 ALTER TABLE "public"."team_api_keys"
     DROP CONSTRAINT "team_api_keys_pkey",
-    ALTER COLUMN "api_key" DROP DEFAULT,
     ADD COLUMN "id" uuid NOT NULL DEFAULT gen_random_uuid(),
     ADD PRIMARY KEY ("id");
 -- Create index "team_api_keys_api_key_key" to table: "team_api_keys"
