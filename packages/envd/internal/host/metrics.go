b936cb5054306f8d166f2e19bb64f0047da2a7e7
0div <tgft73@gmail.com>
2024-12-04 17:53:41 -0800

    added /metrics API endpoint with host.GetMetrics function

---
+++ packages/envd/internal/host/metrics.go
@@ -0,0 +1,37 @@
+package host
+
+import (
+	"math"
+	"time"
+
+	"github.com/shirou/gopsutil/v4/cpu"
+	"github.com/shirou/gopsutil/v4/mem"
+)
+
+type Metrics struct {
+	CPU float64 `json:"cpu_pct"`   // Percent rounded to 2 decimal places
+	Mem uint64  `json:"mem_bytes"` // Total virtual memory in bytes
+}
+
+func GetMetrics() (*Metrics, error) {
+	v, err := mem.VirtualMemory()
+	if err != nil {
+		return nil, err
+	}
+
+	cpuPcts, err := cpu.Percent(time.Second, false)
+	if err != nil {
+		return nil, err
+	}
+
+	cpuPct := cpuPcts[0]
+	cpuPctRounded := cpuPct
+	if cpuPct > 0 {
+		cpuPctRounded = math.Round(cpuPct*100) / 100
+	}
+
+	return &Metrics{
+		CPU: cpuPctRounded,
+		Mem: v.Total,
+	}, nil
+}
