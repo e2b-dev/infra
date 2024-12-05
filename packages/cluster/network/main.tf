ddadece82ce542b3acd811b0018d91e42799b67c
Jakub Novak <jakub@e2b.dev>
2024-12-05 15:12:02 -0800

    Remove api group from logs collector load balancer

--- packages/cluster/network/main.tf
+++ packages/cluster/network/main.tf
@@ -433,18 +433,6 @@ module "gce_lb_http_logs" {
           max_rate_per_instance        = null
           max_rate_per_endpoint        = null
           max_utilization              = null
-          }, {
-          group                        = var.api_instance_group
-          balancing_mode               = null
-          capacity_scaler              = null
-          description                  = null
-          max_connections              = null
-          max_connections_per_instance = null
-          max_connections_per_endpoint = null
-          max_rate                     = null
-          max_rate_per_instance        = null
-          max_rate_per_endpoint        = null
-          max_utilization              = null
         },
       ]
 
