9d7160c25781a35d2cd8ec2a60f9c46f8f7b259a
Jakub Novak <jakub@e2b.dev>
2024-12-05 21:56:44 -0800

    Call sync envd from API

--- packages/api/internal/orchestrator/create_instance.go
+++ packages/api/internal/orchestrator/create_instance.go
@@ -4,6 +4,10 @@ import (
 	"context"
 	_ "embed"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
 	"time"
 
 	"go.opentelemetry.io/otel/attribute"
@@ -17,6 +21,10 @@ import (
 	"github.com/e2b-dev/infra/packages/shared/pkg/models"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
+
+var httpClient = &http.Client{
+	Timeout: 1 * time.Second,
+}
 
 func (o *Orchestrator) CreateSandbox(
 	t trace.Tracer,
@@ -76,6 +84,30 @@ func (o *Orchestrator) CreateSandbox(
 	if err != nil {
 		return nil, fmt.Errorf("failed to create sandbox '%s': %w", templateID, err)
 	}
+
+	go func() {
+		envdUrl, err := url.Parse(fmt.Sprintf("http://49983-%s-%s.localhost:3003/init", sandboxID, res.ClientID))
+		if err != nil {
+			log.Printf("failed to parse envd URL: %v", err)
+		}
+		r, err := httpClient.Do(&http.Request{
+			Method: http.MethodPost,
+			URL:    envdUrl,
+		})
+		if err != nil {
+			log.Printf("failed to call envd: %v", err)
+		}
+		defer r.Body.Close()
+
+		if r.StatusCode != http.StatusOK {
+			log.Printf("envd returned non-200 status code: %d", r)
+		}
+
+		_, err = io.Copy(io.Discard, r.Body)
+		if err != nil {
+			log.Printf("failed to read envd response: %v", err)
+		}
+	}()
 
 	telemetry.ReportEvent(childCtx, "Created sandbox")
 
