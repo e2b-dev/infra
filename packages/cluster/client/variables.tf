1684851604864e04cd0e70a1921938a834a8636d
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-12-12 22:31:05 -0800

    Increase default disk

--- packages/cluster/client/variables.tf
+++ packages/cluster/client/variables.tf
@@ -106,7 +106,7 @@ variable "custom_metadata" {
 variable "root_volume_disk_size_gb" {
   description = "The size, in GB, of the root disk volume on each Consul node."
   type        = number
-  default     = 200
+  default     = 500
 }
 
 variable "root_volume_disk_type" {
