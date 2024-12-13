96ba8c12e5d4a32591b705827bbd6d8791acc81f
Jakub Novak <jakub@e2b.dev>
2024-12-13 15:38:55 +0100

    Clean up

--- packages/shared/pkg/consts/sandboxes.go
+++ packages/shared/pkg/consts/sandboxes.go
@@ -4,7 +4,4 @@ import "os"
 
 const NodeIDLength = 8
 
-var (
-	OrchestratorPort = os.Getenv("ORCHESTRATOR_PORT")
-	SessionProxyPort = os.Getenv("SESSION_PROXY_PORT")
-)
+var OrchestratorPort = os.Getenv("ORCHESTRATOR_PORT")
