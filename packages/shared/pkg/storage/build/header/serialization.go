49193f6e75b83fd17658593cb5c890b298673e6c
Tomas Valenta <valenta.and.thomas@gmail.com>
2024-12-14 00:52:24 +0000

    Cleanup header

--- packages/shared/pkg/storage/build/header/serialization.go
+++ packages/shared/pkg/storage/build/header/serialization.go
@@ -19,16 +19,6 @@ type Metadata struct {
 	BaseBuildId uuid.UUID
 }
 
-// Start, Length and SourceStart are in bytes of the data file
-// Length will be a multiple of BlockSize
-// The list of block mappings will be in order of increasing Start, covering the entire file
-type BuildMap struct {
-	// Offset defines which block of the current layer this mapping starts at
-	Offset             uint64
-	Length             uint64
-	BuildId            uuid.UUID
-	BuildStorageOffset uint64
-}
 
 func Serialize(metadata *Metadata, mappings []*BuildMap) (io.Reader, error) {
 	var buf bytes.Buffer
