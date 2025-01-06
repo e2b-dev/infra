0596d5665c8edfa4462efe0ec7495570b1390522
Jakub Novak <jakub@e2b.dev>
2025-01-06 17:18:01 +0100

    Fix envd upload

--- packages/envd/upload.sh
+++ packages/envd/upload.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 GCP_PROJECT_ID=$1
 
-chmod +x dist/envd_linux_amd64_v1/envd
+chmod +x bin/envd
 
 gsutil -h "Cache-Control:no-cache, max-age=0" \
-  cp dist/envd_linux_amd64_v1/envd "gs://${GCP_PROJECT_ID}-fc-env-pipeline/envd"
+  cp bin/envd "gs://${GCP_PROJECT_ID}-fc-env-pipeline/envd"
