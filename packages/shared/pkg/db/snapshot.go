3cb3bf05945613ed2ec7b3154875024bd11a808b
Mish Ushakov <mishushakov@users.noreply.github.com>
2025-02-13 18:26:43 +0100

    removed SetSandboxStartedAt on subsequent pause

--- packages/shared/pkg/db/snapshot.go
+++ packages/shared/pkg/db/snapshot.go
@@ -89,7 +89,6 @@ func (db *DB) NewSnapshotBuild(
 			Snapshot.
 			UpdateOne(s).
 			SetMetadata(snapshotConfig.Metadata).
-			SetSandboxStartedAt(snapshotConfig.SandboxStartedAt).
 			Save(ctx)
 		if err != nil {
 			return nil, fmt.Errorf("failed to update snapshot '%s': %w", snapshotConfig.SandboxID, err)
