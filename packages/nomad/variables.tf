20d6465027a9b7f4c1ecb819101af710dcf14fc4
r33drichards <rwendt1337@gmail.com>
2025-02-14 15:31:42 -0800

    Revert "Create Grafana Stack with terraform and plumb relevant data to gcp for telemetry (#291)"
    This reverts commit 4ab471c33154d009076fe899d47d97e21199a005, reversing
    changes made to 81c95eefeaae96abb0a751b94dd8fffe39b0f3f5.

--- packages/nomad/variables.tf
+++ packages/nomad/variables.tf
@@ -84,10 +84,6 @@ variable "postgres_connection_string_secret_name" {
   type = string
 }
 
-variable "grafana_otel_collector_token_secret_name" {
-  type = string
-}
-
 # Proxies
 variable "session_proxy_service_name" {
   type = string
@@ -138,6 +134,34 @@ variable "logs_health_proxy_port" {
     port        = number
     health_path = string
   })
+}
+
+variable "grafana_api_key_secret_name" {
+  type = string
+}
+
+variable "grafana_logs_username_secret_name" {
+  type = string
+}
+
+variable "grafana_traces_username_secret_name" {
+  type = string
+}
+
+variable "grafana_metrics_username_secret_name" {
+  type = string
+}
+
+variable "grafana_logs_endpoint_secret_name" {
+  type = string
+}
+
+variable "grafana_traces_endpoint_secret_name" {
+  type = string
+}
+
+variable "grafana_metrics_endpoint_secret_name" {
+  type = string
 }
 
 variable "analytics_collector_host_secret_name" {
@@ -201,20 +225,4 @@ variable "redis_port" {
     name = string
     port = number
   })
-}
-
-variable "grafana_username_secret_name" {
-  type = string
-}
-
-variable "grafana_api_key_logs_collector_secret_name" {
-  type = string
-}
-
-variable "grafana_logs_url_secret_name" {
-  type = string
-}
-
-variable "grafana_logs_username_secret_name" {
-  type = string
 }
