0f36a6d6331ba0c3621e1a30b4f306be760fb845
Jakub Novak <jakub@e2b.dev>
2025-02-19 18:08:31 +0100

    Remove unnecessary open port

--- terraform/network-setup/main.tf
+++ terraform/network-setup/main.tf
@@ -2,24 +2,6 @@ variable "gcp_project_id" {
   default = "e2b-"
 }
 
-resource "google_compute_firewall" "allow_dns" {
-  project = var.gcp_project_id
-  name    = "allow-dns-ports"
-  network = "default" # Change to your VPC name if not using the default one
-
-  allow {
-    protocol = "udp"
-    ports    = ["5353"]
-  }
-
-  direction = "INGRESS"
-  priority  = 1000
-
-  source_ranges = ["0.0.0.0/0"] # Change this to restrict access
-
-  description = "Allow UDP traffic on ports 5353 (mDNS)"
-}
-
 resource "google_compute_firewall" "allow_50001" {
   project = var.gcp_project_id
   name    = "allow-50001-ingress"
