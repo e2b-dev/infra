71c4f3b9b940db72a3fd5fd10724387783774622
Jakub Dobry <jakub.dobry8@gmail.com>
2025-02-22 23:05:38 -0800

    Pass seed file name as argument

--- packages/integration-tests/seed.go
+++ packages/integration-tests/seed.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -17,6 +18,13 @@ type SeedData struct {
 }
 
 func main() {
+	fileName := flag.String("file", "", "file name to seed")
+	flag.Parse()
+
+	if fileName == nil || *fileName == "" {
+		log.Fatalf("File name is required")
+	}
+
 	connectionString := os.Getenv("POSTGRES_CONNECTION_STRING")
 	if connectionString == "" {
 		log.Fatalf("POSTGRES_CONNECTION_STRING is not set")
@@ -29,7 +37,7 @@ func main() {
 	defer db.Close()
 
 	// Execute the seed
-	tmpl, err := template.ParseFiles("seed.sql")
+	tmpl, err := template.ParseFiles(*fileName)
 	if err != nil {
 		log.Fatalf("Failed to parse seed file: %v", err)
 	}
@@ -39,6 +47,7 @@ func main() {
 		EnvId:   os.Getenv("TESTS_SANDBOX_TEMPLATE_ID"),
 		BuildId: os.Getenv("TESTS_SANDBOX_BUILD_ID"),
 	})
+
 	if err != nil {
 		log.Fatalf("Failed to execute seed: %v", err)
 	}
