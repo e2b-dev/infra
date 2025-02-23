6f08025bd5e71987c502e529e4012f525d0fd945
Jakub Dobry <jakub.dobry8@gmail.com>
2025-02-23 05:44:08 +0000

    fixes

--- packages/integration-tests/internal/tests/api/sandbox_test.go
+++ packages/integration-tests/internal/tests/api/sandbox_test.go
@@ -27,5 +27,36 @@ func TestSandboxCreate(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	t.Cleanup(func() {
+		if t.Failed() {
+			t.Logf("Response: %s", string(resp.Body))
+		}
+	})
+
+	assert.Equal(t, http.StatusCreated, resp.StatusCode())
+}
+
+func TestSandboxCreate2(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c := setup.GetAPIClient(t)
+
+	sbxTimeout := int32(60)
+	resp, err := c.PostSandboxesWithResponse(ctx, api.NewSandbox{
+		TemplateID: setup.SandboxTemplateID,
+		Timeout:    &sbxTimeout,
+	}, setup.WithAPIKey())
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Cleanup(func() {
+		if t.Failed() {
+			t.Logf("Response: %s", string(resp.Body))
+		}
+	})
+
 	assert.Equal(t, http.StatusCreated, resp.StatusCode())
 }
