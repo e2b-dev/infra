5b4def8341f2411bc1a9b7a8afe26668d2f26873
Jakub Dobry <jakub.dobry8@gmail.com>
2025-02-25 22:12:05 -0800

    Revert "orchestrator: process initialization and shut down (#297)"
    This reverts commit f922407943c9b6021c492b7ed26b04a9449047ef.
    
    # Conflicts:
    #	packages/orchestrator/internal/server/main.go

--- packages/orchestrator/internal/dns/server.go
+++ packages/orchestrator/internal/dns/server.go
@@ -1,7 +1,6 @@
 package dns
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net"
@@ -16,13 +15,8 @@ import (
 const ttl = 0
 
 type DNS struct {
+	mu      sync.Mutex
 	records *smap.Map[string]
-
-	closer struct {
-		once sync.Once
-		op   func(context.Context) error
-		err  error
-	}
 }
 
 func New() *DNS {
@@ -88,16 +82,10 @@ func (d *DNS) Start(address string, port int) error {
 
 	server := resolver.Server{Addr: fmt.Sprintf("%s:%d", address, port), Net: "udp", Handler: mux}
 
-	if err := server.ListenAndServe(); err != nil {
-		return fmt.Errorf("DNS server encounterted error: %w", err)
+	err := server.ListenAndServe()
+	if err != nil {
+		return fmt.Errorf("failed to start DNS server: %w", err)
 	}
 
-	d.closer.op = server.ShutdownContext
-
 	return nil
-}
-
-func (d *DNS) Close(ctx context.Context) error {
-	d.closer.once.Do(func() { d.closer.err = d.closer.op(ctx) })
-	return d.closer.err
 }
--- packages/orchestrator/main.go
+++ packages/orchestrator/main.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"context"
-	"errors"
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"os/signal"
-	"sync"
-	"sync/atomic"
-	"syscall"
+	"net"
 
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/server"
 	"github.com/e2b-dev/infra/packages/shared/pkg/env"
@@ -23,87 +18,28 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	sig, sigCancel := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
-	defer sigCancel()
+	port := flag.Int("port", defaultPort, "orchestrator server port")
 
-	var port uint
-
-	flag.UintVar(&port, "port", defaultPort, "orchestrator server port")
 	flag.Parse()
-
-	wg := &sync.WaitGroup{}
-	exitCode := &atomic.Int32{}
-	telemetrySignal := make(chan struct{})
-
-	// defer waiting on the waitgroup so that this runs even when
-	// there's a panic.
-	defer wg.Wait()
 
 	if !env.IsLocal() {
 		shutdown := telemetry.InitOTLPExporter(ctx, server.ServiceName, "no")
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			<-telemetrySignal
-			if err := shutdown(ctx); err != nil {
-				log.Printf("telemetry shutdown: %v", err)
-				exitCode.Add(1)
-			}
-		}()
+		defer shutdown(context.TODO())
 	}
 
-	srv, err := server.New(ctx, port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s, err := server.New()
 	if err != nil {
 		log.Fatalf("failed to create server: %v", err)
 	}
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		var err error
+	log.Printf("starting server on port %d", *port)
 
-		defer func() {
-			// recover the panic because the service manages a number of go routines
-			// that can panic, so catching this here allows for the rest of the process
-			// to terminate in a more orderly manner.
-			if perr := recover(); perr != nil {
-				// many of the panics use log.Panicf which means we're going to log
-				// some panic messages twice, but this seems ok, and temporary while
-				// we clean up logging.
-				log.Printf("caught panic in service: %v", perr)
-				exitCode.Add(1)
-				err = errors.Join(err, fmt.Errorf("server panic: %v", perr))
-			}
-
-			// if we encountered an err, but the signal context was NOT canceled, then
-			// the outer context needs to be canceled so the remainder of the service
-			// can shutdown.
-			if err != nil && sig.Err() == nil {
-				log.Printf("service ended early without signal")
-				cancel()
-			}
-		}()
-
-		// this sets the error declared above so the function
-		// in the defer can check it.
-		if err = srv.Start(ctx); err != nil {
-			log.Printf("orchestrator service: %v", err)
-			exitCode.Add(1)
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		defer close(telemetrySignal)
-		<-sig.Done()
-		if err := srv.Close(ctx); err != nil {
-			log.Printf("grpc service: %v", err)
-			exitCode.Add(1)
-		}
-	}()
-
-	wg.Wait()
-
-	os.Exit(int(exitCode.Load()))
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
