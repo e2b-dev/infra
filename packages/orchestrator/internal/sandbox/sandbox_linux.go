ab672359bf35e43a57c96fad94d03a907ac7867c
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-02-27 17:34:16 -0800

    Add more logs for orchestrator exit

--- packages/orchestrator/internal/sandbox/sandbox_linux.go
+++ packages/orchestrator/internal/sandbox/sandbox_linux.go
@@ -222,6 +222,8 @@ func NewSandbox(
 		uffdWaitErr := <-fcUffd.Exit
 		uffdExit <- uffdWaitErr
 
+		zap.L().Info("UFFD exited", zap.String("sandbox_id", config.SandboxId), zap.Error(uffdWaitErr))
+
 		cancelUffdStartCtx(fmt.Errorf("uffd process exited: %w", errors.Join(uffdWaitErr, context.Cause(uffdStartCtx))))
 	}()
 
@@ -297,9 +299,13 @@ func NewSandbox(
 			errs = append(errs, fmt.Errorf("failed to stop uffd: %w", uffdStopErr))
 		}
 
-		healthcheckCtx.Lock()
-		healthcheckCtx.Cancel()
-		healthcheckCtx.Unlock()
+		go func() {
+			zap.L().Info("stopping healthcheck during cleanup", zap.String("sandbox_id", config.SandboxId))
+			healthcheckCtx.Lock()
+			healthcheckCtx.Cancel()
+			healthcheckCtx.Unlock()
+			zap.L().Info("stopped healthcheck during cleanup", zap.String("sandbox_id", config.SandboxId))
+		}()
 
 		return errors.Join(errs...)
 	})
@@ -350,11 +356,13 @@ func NewSandbox(
 func (s *Sandbox) Wait(ctx context.Context) error {
 	select {
 	case fcErr := <-s.process.Exit:
+		zap.L().Info("FC exited", zap.String("sandbox_id", s.Config.SandboxId))
 		stopErr := s.Stop(ctx)
 		uffdErr := <-s.uffdExit
 
 		return errors.Join(fcErr, stopErr, uffdErr)
 	case uffdErr := <-s.uffdExit:
+		zap.L().Info("UFFD exited", zap.String("sandbox_id", s.Config.SandboxId))
 		stopErr := s.Stop(ctx)
 		fcErr := <-s.process.Exit
 
