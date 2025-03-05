324628062e07e43dfa4cdcfa3e14ebad50c60854
r33drichards <rwendt1337@gmail.com>
2025-03-05 10:07:00 -0800

    support incremental and parallel deployment

--- packages/api/internal/handlers/sandbox_metrics.go
+++ packages/api/internal/handlers/sandbox_metrics.go
@@ -2,11 +2,17 @@ package handlers
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"slices"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/grafana/loki/pkg/loghttp"
+	"github.com/grafana/loki/pkg/logproto"
 	"go.opentelemetry.io/otel/attribute"
 	"go.uber.org/zap"
 
@@ -20,6 +26,75 @@ import (
 const defaultLimit = 100
 
 func (a *APIStore) getSandboxesSandboxIDMetrics(
+	ctx context.Context,
+	sandboxID string,
+	teamID string,
+	limit int,
+	duration time.Duration,
+) ([]api.SandboxMetric, error) {
+	sandboxID = utils.ShortID(sandboxID)
+
+	end := time.Now()
+	start := end.Add(-duration)
+
+	// Sanitize ID
+	// https://grafana.com/blog/2021/01/05/how-to-escape-special-characters-with-lokis-logql/
+	id := strings.ReplaceAll(sandboxID, "`", "")
+
+	// equivalent CLI query:
+	// logcli query '{source="logs-collector", service="envd", teamID="65d165ab-69f6-4b5c-9165-6b93cd341503", sandboxID="izuhqjlfabd8ataeixrtl", category="metrics"}' --from="2025-01-19T10:00:00Z"
+	query := fmt.Sprintf(
+		"{source=\"logs-collector\", service=\"envd\", teamID=`%s`, sandboxID=`%s`, category=\"metrics\"}", teamID, id)
+
+	res, err := a.lokiClient.QueryRange(query, limit, start, end, logproto.BACKWARD, time.Duration(0), time.Duration(0), true)
+	if err != nil {
+		return nil, fmt.Errorf("error when returning metrics for sandbox: %w", err)
+	}
+
+	if res.Data.Result.Type() != loghttp.ResultTypeStream {
+		return nil, fmt.Errorf("unexpected value type %T", res.Data.Result.Type())
+	}
+
+	value := res.Data.Result.(loghttp.Streams)
+	metrics := make([]api.SandboxMetric, 0)
+
+	for _, stream := range value {
+		for _, entry := range stream.Entries {
+
+			var metric struct {
+				CPUUsedPct  float32 `json:"cpuUsedPct"`
+				CPUCount    int32   `json:"cpuCount"`
+				MemTotalMiB int64   `json:"memTotalMiB"`
+				MemUsedMiB  int64   `json:"memUsedMiB"`
+			}
+
+			err := json.Unmarshal([]byte(entry.Line), &metric)
+			if err != nil {
+				zap.L().Error("Failed to unmarshal metric", zap.String("sandbox_id", sandboxID), zap.Error(err))
+				telemetry.ReportCriticalError(ctx, fmt.Errorf("failed to unmarshal metric: %w", err))
+
+				continue
+			}
+
+			metrics = append(metrics, api.SandboxMetric{
+				Timestamp:   entry.Timestamp,
+				CpuUsedPct:  metric.CPUUsedPct,
+				CpuCount:    metric.CPUCount,
+				MemTotalMiB: metric.MemTotalMiB,
+				MemUsedMiB:  metric.MemUsedMiB,
+			})
+		}
+	}
+
+	// Sort metrics by timestamp (they are returned by the time they arrived in Loki)
+	slices.SortFunc(metrics, func(a, b api.SandboxMetric) int {
+		return a.Timestamp.Compare(b.Timestamp)
+	})
+
+	return metrics, nil
+}
+
+func (a *APIStore) getSandboxesSandboxIDMetricsFromClickhouse(
 	ctx context.Context,
 	sandboxID string,
 	teamID string,
@@ -63,7 +138,18 @@ func (a *APIStore) GetSandboxesSandboxIDMetrics(
 		attribute.String("team.id", teamID),
 	)
 
-	metrics, err := a.getSandboxesSandboxIDMetrics(ctx, sandboxID, teamID, defaultLimit, oldestLogsLimit)
+	// Get metrics and health status on sandbox startup
+	readMetricsFromClickhouse := os.Getenv("READ_METRICS_FROM_CLICKHOUSE") == "true"
+
+	var metrics []api.SandboxMetric
+	var err error
+
+	if readMetricsFromClickhouse {
+		metrics, err = a.getSandboxesSandboxIDMetricsFromClickhouse(ctx, sandboxID, teamID, defaultLimit, oldestLogsLimit)
+	} else {
+		metrics, err = a.getSandboxesSandboxIDMetrics(ctx, sandboxID, teamID, defaultLimit, oldestLogsLimit)
+	}
+
 	if err != nil {
 		zap.L().Error("Error returning metrics for sandbox",
 			zap.Error(err),
