571d464df5a38766bef3c74aaae0d1482dc9b4b1
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-03-05 18:09:33 +0100

    feat(template-manager): template status endpoint will pulling of build changes

---
+++ packages/template-manager/internal/server/template_status.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/template-manager"
+	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
+	"go.uber.org/zap"
+	"time"
+)
+
+const statusTimeout = time.Minute * 5
+
+func (s *serverStore) TemplateBuildStatus(in *template_manager.TemplateStatusRequest, stream template_manager.TemplateService_TemplateBuildStatusServer) error {
+	ctx, cancel := context.WithTimeout(stream.Context(), statusTimeout)
+	defer cancel()
+
+	childCtx, childSpan := s.tracer.Start(ctx, "template-build-status-request")
+	defer childSpan.End()
+
+	logger := s.logger.With(zap.String("buildID", in.BuildID), zap.String("envID", in.TemplateID))
+
+	// stream updated status until build is done or timeout will kick in
+	for {
+		buildInfo, err := s.buildCache.Get(in.BuildID)
+		if err != nil {
+			logger.Error("error while getting build info from cache", zap.Error(err))
+			return fmt.Errorf("error while getting build info, maybe already expired")
+		}
+
+		var metadata *template_manager.TemplateBuildMetadata = nil
+		if !buildInfo.IsRunning() {
+			metadata = &template_manager.TemplateBuildMetadata{
+				RootfsSizeKey:  buildInfo.GetRootFsSizeKey(),
+				EnvdVersionKey: buildInfo.GetEnvdVersionKey(),
+			}
+		}
+
+		err = stream.Send(
+			&template_manager.TemplateBuildStatusResponse{
+				Done:     !buildInfo.IsRunning(),
+				Failed:   buildInfo.IsFailed(),
+				Metadata: metadata,
+			},
+		)
+
+		if err != nil {
+			logger.Error("Error while sending status stream back to API", zap.Error(err))
+			telemetry.ReportError(childCtx, err)
+			return err
+		}
+
+		if buildInfo.IsFailed() {
+			logger.Error("Template build failed")
+			return fmt.Errorf("template build failed")
+		}
+
+		if !buildInfo.IsRunning() {
+			logger.Info("Template build finished")
+			break
+		}
+
+		time.Sleep(time.Second * 5)
+	}
+
+	return nil
+}
