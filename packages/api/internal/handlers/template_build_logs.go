43503c9dc0983cfacee590be7defbe7fdac1be9b
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-03-06 10:25:54 +0100

    chore(api): template builds logs are using db instead of locally cached data

--- packages/api/internal/handlers/template_build_logs.go
+++ packages/api/internal/handlers/template_build_logs.go
@@ -46,27 +46,21 @@ func (a *APIStore) GetTemplatesTemplateIDBuildsBuildIDStatus(c *gin.Context, tem
 	if err != nil {
 		errMsg := fmt.Errorf("error when parsing build id: %w", err)
 		telemetry.ReportError(ctx, errMsg)
-
 		a.sendAPIStoreError(c, http.StatusBadRequest, "Invalid build id")
-
 		return
 	}
 
-	dockerBuild, err := a.buildCache.Get(templateID, buildUUID)
+	templateDB, _, err := a.db.GetEnv(ctx, templateID)
 	if err != nil {
-		msg := fmt.Errorf("error finding cache for env %s and build %s", templateID, buildID)
-		telemetry.ReportError(ctx, msg)
-
-		a.sendAPIStoreError(c, http.StatusNotFound, fmt.Sprintf("Build (%s) not found", buildID))
-
+		errMsg := fmt.Errorf("error when getting env: %w", err)
+		telemetry.ReportError(ctx, errMsg)
+		a.sendAPIStoreError(c, http.StatusNotFound, fmt.Sprintf("Template '%s' not found", templateID))
 		return
 	}
-
-	templateTeamID := dockerBuild.GetTeamID()
 
 	var team *models.Team
 	for _, t := range teams {
-		if t.ID == templateTeamID {
+		if t.ID == templateDB.TeamID {
 			team = t
 			break
 		}
@@ -81,7 +75,13 @@ func (a *APIStore) GetTemplatesTemplateIDBuildsBuildIDStatus(c *gin.Context, tem
 		return
 	}
 
-	status := dockerBuild.GetStatus()
+	buildDB, err := a.db.GetEnvBuild(ctx, buildUUID)
+	if err != nil {
+		errMsg := fmt.Errorf("error when getting build: %w", err)
+		telemetry.ReportError(ctx, errMsg)
+		a.sendAPIStoreError(c, http.StatusNotFound, fmt.Sprintf("Build '%s' not found", buildID))
+		return
+	}
 
 	// Sanitize env ID
 	// https://grafana.com/blog/2021/01/05/how-to-escape-special-characters-with-lokis-logql/
@@ -138,7 +138,7 @@ func (a *APIStore) GetTemplatesTemplateIDBuildsBuildIDStatus(c *gin.Context, tem
 		Logs:       logs,
 		TemplateID: templateID,
 		BuildID:    buildID,
-		Status:     status,
+		Status:     api.TemplateBuildStatus(buildDB.Status),
 	}
 
 	c.JSON(http.StatusOK, result)
