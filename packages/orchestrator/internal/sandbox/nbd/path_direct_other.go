1663dfe031b404bd1b0177ac5578ff9a720da0e8
r33drichards <rwendt1337@gmail.com>
2025-03-07 14:32:31 -0800

    rm unused fields

--- packages/orchestrator/internal/sandbox/nbd/path_direct_other.go
+++ packages/orchestrator/internal/sandbox/nbd/path_direct_other.go
@@ -6,30 +6,17 @@ package nbd
 import (
 	"context"
 	"errors"
-	"net"
 
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/block"
 )
 
 type DirectPathMount struct {
-	Backend     block.Device
-	ctx         context.Context
-	dispatcher  *Dispatch
-	conn        net.Conn
-	deviceIndex uint32
-	blockSize   uint64
-	cancelfn    context.CancelFunc
+	Backend block.Device
 }
 
 func NewDirectPathMount(b block.Device) *DirectPathMount {
-	ctx, cancelfn := context.WithCancel(context.Background())
 
-	return &DirectPathMount{
-		Backend:   b,
-		ctx:       ctx,
-		cancelfn:  cancelfn,
-		blockSize: 4096,
-	}
+	return nil
 }
 
 func (d *DirectPathMount) Open(ctx context.Context) (uint32, error) {
