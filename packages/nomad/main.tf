488795bf3141c00e37889ec1af9b120b19dced1d
r33drichards <rwendt1337@gmail.com>
2025-03-07 12:53:12 -0800

    Deploy clickhouse on nomad (#370)

--- packages/nomad/main.tf
+++ packages/nomad/main.tf
@@ -377,3 +377,49 @@ resource "nomad_job" "loki" {
     }
   }
 }
+
+# create a bucket for clickhouse
+resource "google_storage_bucket" "clickhouse_bucket" {
+  name     = "${var.prefix}clickhouse-bucket"
+  location = var.gcp_region
+}
+
+
+// create service account for bucket
+resource "google_service_account" "clickhouse_service_account" {
+  account_id   = "${var.prefix}clickhouse-service-account"
+  display_name = "${var.prefix}clickhouse-service-account"
+}
+
+# attach service account to bucket 
+resource "google_storage_bucket_iam_member" "clickhouse_service_account_iam" {
+  bucket = google_storage_bucket.clickhouse_bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.clickhouse_service_account.email}"
+}
+
+# hmac key for service account
+resource "google_storage_hmac_key" "clickhouse_hmac_key" {
+  service_account_email = google_service_account.clickhouse_service_account.email
+}
+
+# generate password
+resource "random_password" "clickhouse_password" {
+  length  = 32
+  special = false
+}
+
+
+# Add this with your other Nomad jobs
+resource "nomad_job" "clickhouse" {
+  jobspec = templatefile("${path.module}/clickhouse.hcl", {
+    zone                = var.gcp_zone
+    clickhouse_version  = "25.1.5.31" # Or make this a variable
+    gcs_bucket          = google_storage_bucket.clickhouse_bucket.name
+    gcs_folder          = "clickhouse-data"
+    hmac_key            = google_storage_hmac_key.clickhouse_hmac_key.access_id
+    hmac_secret         = google_storage_hmac_key.clickhouse_hmac_key.secret
+    username            = "clickhouse"
+    password_sha256_hex = sha256(random_password.clickhouse_password.result)
+  })
+}
