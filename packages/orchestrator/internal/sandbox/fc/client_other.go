6ee10793387666e4b76fec79de15d52ef4ec5acf
r33drichards <rwendt1337@gmail.com>
2025-03-07 09:28:31 -0800

    rm todo list

--- packages/orchestrator/internal/sandbox/fc/client_other.go
+++ packages/orchestrator/internal/sandbox/fc/client_other.go
@@ -16,12 +16,6 @@ func newApiClient(socketPath string) *apiClient {
 	return nil
 }
 
-// internal/sandbox/fc/process.go:260:17: p.client.loadSnapshot undefined (type *apiClient has no field or method loadSnapshot)
-// internal/sandbox/fc/process.go:272:17: p.client.resumeVM undefined (type *apiClient has no field or method resumeVM)
-// internal/sandbox/fc/process.go:279:17: p.client.setMmds undefined (type *apiClient has no field or method setMmds)
-// internal/sandbox/fc/process.go:320:18: p.client.pauseVM undefined (type *apiClient has no field or method pauseVM)
-// internal/sandbox/fc/process.go:328:18: p.client.createSnapshot undefined (type *apiClient has no field or method createSnapshot)
-
 func (c *apiClient) loadSnapshot(ctx context.Context, uffdSocketPath string, uffdReady chan struct{}, snapfile template.File) error {
 	return nil
 }
