4e173054a871ea1bc7ad9556736861c20a82d813
Jakub Dobry <jakub.dobry8@gmail.com>
2025-03-10 16:21:43 +0100

    Add orchestrator client setup to the integration tests (#403)
    Co-authored-by: Jakub Novák <jakub@e2b.dev>

--- tests/integration/internal/setup/constants.go
+++ tests/integration/internal/setup/constants.go
@@ -13,4 +13,6 @@ var (
 	APIServerURL      = os.Getenv("TESTS_API_SERVER_URL")
 	SandboxTemplateID = os.Getenv("TESTS_SANDBOX_TEMPLATE_ID")
 	APIKey            = os.Getenv("TESTS_E2B_API_KEY")
+
+	OrchestratorHost = os.Getenv("TESTS_ORCHESTRATOR_HOST")
 )
