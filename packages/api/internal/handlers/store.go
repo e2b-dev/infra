3b81051e33d85253842489bb20de31f9b6e56901
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-03-10 11:09:54 +0100

    reguster template manager for api store, run background process for build cleanups

--- packages/api/internal/handlers/store.go
+++ packages/api/internal/handlers/store.go
@@ -25,7 +25,7 @@ import (
 	authcache "github.com/e2b-dev/infra/packages/api/internal/cache/auth"
 	templatecache "github.com/e2b-dev/infra/packages/api/internal/cache/templates"
 	"github.com/e2b-dev/infra/packages/api/internal/orchestrator"
-	template_manager "github.com/e2b-dev/infra/packages/api/internal/template-manager"
+	"github.com/e2b-dev/infra/packages/api/internal/template-manager"
 	"github.com/e2b-dev/infra/packages/api/internal/utils"
 	"github.com/e2b-dev/infra/packages/shared/pkg/chdb"
 	"github.com/e2b-dev/infra/packages/shared/pkg/db"
@@ -122,10 +122,13 @@ func NewAPIStore(ctx context.Context) *APIStore {
 		zap.L().Fatal("initializing Orchestrator client", zap.Error(err))
 	}
 
-	templateManager, err := template_manager.New()
+	templateManager, err := template_manager.New(dbClient)
 	if err != nil {
 		zap.L().Fatal("initializing Template manager client", zap.Error(err))
 	}
+
+	// Start the periodic sync of template builds statuses
+	go templateManager.BuildsStatusPeriodicalSync()
 
 	var lokiClient *loki.DefaultClient
 	if laddr := os.Getenv("LOKI_ADDRESS"); laddr != "" {
