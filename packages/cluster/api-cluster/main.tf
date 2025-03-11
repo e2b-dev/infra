4e79f66e93cf79fd4636ab74ae72d2b2b4812874
Jakub Dobry <jakub.dobry8@gmail.com>
2025-03-11 18:48:09 +0100

    Fix opportunistic typo (#418)

--- packages/cluster/api-cluster/main.tf
+++ packages/cluster/api-cluster/main.tf
@@ -48,7 +48,7 @@ resource "google_compute_instance_group_manager" "api_cluster" {
   # Server is a stateful cluster, so the update strategy used to roll out a new GCE Instance Template must be
   # a rolling update.
   update_policy {
-    type                    = var.environment == "dev" ? "PROACTIVE" : "OPPORUNISTIC"
+    type                    = var.environment == "dev" ? "PROACTIVE" : "OPPORTUNISTIC"
     minimal_action          = var.instance_group_update_policy_minimal_action
     max_surge_fixed         = var.instance_group_update_policy_max_surge_fixed
     max_surge_percent       = var.instance_group_update_policy_max_surge_percent
