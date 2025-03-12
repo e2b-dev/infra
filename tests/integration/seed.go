25ad2ca0cc66ae644fa59f2bb0e811aad31879ec
Jakub Dobry <jakub.dobry8@gmail.com>
2025-03-12 14:55:43 +0100

    Seed DB using golang script

--- tests/integration/seed.go
+++ tests/integration/seed.go
@@ -1,67 +1,123 @@
 package main
 
 import (
-	"bytes"
-	"database/sql"
-	"flag"
+	"context"
 	"fmt"
 	"log"
 	"os"
-	"text/template"
 
-	_ "github.com/lib/pq"
+	"github.com/google/uuid"
+	
+	"github.com/e2b-dev/infra/packages/shared/pkg/db"
 )
 
 type SeedData struct {
 	APIKey  string
-	EnvId   string
-	BuildId string
-	TeamId  string
-	UserId  string
+	EnvID   string
+	BuildID uuid.UUID
+	TeamID  uuid.UUID
+	UserID  uuid.UUID
 }
 
 func main() {
-	fileName := flag.String("file", "", "file name to seed")
-	flag.Parse()
-
-	if fileName == nil || *fileName == "" {
-		log.Fatalf("File name is required")
-	}
-
 	connectionString := os.Getenv("POSTGRES_CONNECTION_STRING")
 	if connectionString == "" {
 		log.Fatalf("POSTGRES_CONNECTION_STRING is not set")
 	}
 
-	db, err := sql.Open("postgres", connectionString)
+	database, err := db.NewClient()
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-	defer db.Close()
+	defer database.Close()
 
-	// Execute the seed
-	tmpl, err := template.ParseFiles(*fileName)
-	if err != nil {
-		log.Fatalf("Failed to parse seed file: %v", err)
-	}
-
-	var parsed bytes.Buffer
-	err = tmpl.Execute(&parsed, SeedData{
+	data := SeedData{
 		APIKey:  os.Getenv("TESTS_E2B_API_KEY"),
-		EnvId:   os.Getenv("TESTS_SANDBOX_TEMPLATE_ID"),
-		BuildId: os.Getenv("TESTS_SANDBOX_BUILD_ID"),
-		TeamId:  os.Getenv("TESTS_SANDBOX_TEAM_ID"),
-		UserId:  os.Getenv("TESTS_SANDBOX_USER_ID"),
-	})
-
-	if err != nil {
-		log.Fatalf("Failed to execute seed: %v", err)
+		EnvID:   os.Getenv("TESTS_SANDBOX_TEMPLATE_ID"),
+		BuildID: uuid.MustParse(os.Getenv("TESTS_SANDBOX_BUILD_ID")),
+		TeamID:  uuid.MustParse(os.Getenv("TESTS_SANDBOX_TEAM_ID")),
+		UserID:  uuid.MustParse(os.Getenv("TESTS_SANDBOX_USER_ID")),
 	}
 
-	_, err = db.Exec(parsed.String())
+	err = seed(database, data)
 	if err != nil {
 		log.Fatalf("Failed to execute seed: %v", err)
 	}
 
 	fmt.Println("Seed completed successfully.")
+}
+
+func seed(db *db.DB, data SeedData) error {
+	ctx := context.Background()
+
+	// Team
+	t, err := db.Client.Team.Create().
+		SetID(data.TeamID).
+		SetEmail("test-integration@e2b.dev").
+		SetName("E2B").
+		SetTier("base_v1").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	// User-Team
+	_, err = db.Client.UsersTeams.Create().
+		SetUserID(data.UserID).
+		SetTeamID(t.ID).
+		SetIsDefault(true).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Team API Key
+	_, err = db.Client.TeamAPIKey.Create().
+		SetTeam(t).
+		SetAPIKey(data.APIKey).
+		SetName("Integration Tests API Key").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	// Base image build
+	_, err = db.Client.Env.Create().
+		SetID(data.EnvID).
+		SetTeamID(t.ID).
+		SetPublic(false).
+		SetBuildCount(1).
+		SetSpawnCount(0).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Client.EnvBuild.Create().
+		SetID(data.BuildID).
+		SetEnvID(data.EnvID).
+		SetDockerfile("FROM e2bdev/base:latest").
+		SetStatus("uploaded").
+		SetVcpu(2).
+		SetRAMMB(512).
+		SetFreeDiskSizeMB(512).
+		SetTotalDiskSizeMB(1982).
+		SetKernelVersion("vmlinux-6.1.102").
+		SetFirecrackerVersion("v1.10.1_1fcdaec").
+		SetEnvdVersion("0.1.5").
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Client.EnvAlias.Create().
+		SetID("base").
+		SetIsRenamable(true).
+		SetEnvID(data.EnvID).
+		Save(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
