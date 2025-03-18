6fada309f72036c3c7b770865e7fde7b9de9e631
Jakub Dobry <jakub.dobry8@gmail.com>
2025-03-18 11:20:32 +0100

    fix migration

--- packages/shared/migrations/20250211160814.sql
+++ packages/shared/migrations/20250211160814.sql
@@ -1,15 +1,15 @@
 -- Add new columns to team_api_keys table
 ALTER TABLE team_api_keys
-    ADD COLUMN api_key_hash character text UNIQUE,
-    ADD COLUMN api_key_mask character varying(44);
+    ADD COLUMN api_key_hash TEXT UNIQUE,
+    ADD COLUMN api_key_mask VARCHAR(44);
 
 -- Add new columns to access_tokens table
 ALTER TABLE access_tokens
-    ADD COLUMN id uuid DEFAULT gen_random_uuid(),
-    ADD COLUMN access_token_hash text UNIQUE,
-    ADD COLUMN access_token_mask text,
-    ADD COLUMN name text NOT NULL DEFAULT 'Unnamed Access Token';
+    ADD COLUMN id UUID DEFAULT gen_random_uuid(),
+    ADD COLUMN access_token_hash TEXT UNIQUE,
+    ADD COLUMN access_token_mask TEXT,
+    ADD COLUMN name TEXT NOT NULL DEFAULT 'Unnamed Access Token';
 
 -- Mark sensitive columns as sensitive
 COMMENT ON COLUMN team_api_keys.api_key_hash IS 'sensitive';
-COMMENT ON COLUMN access_tokens.access_token_hash IS 'sensitive'; 
+COMMENT ON COLUMN access_tokens.access_token_hash IS 'sensitive';
