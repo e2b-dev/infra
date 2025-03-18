173046028a73c50683cf25dba40e318e9d60d38c
r33drichards <rwendt1337@gmail.com>
2025-03-18 04:00:47 -0700

    Revert "Refactor Supabase JWT secret handling in Nomad and API modules" (#464)

--- packages/nomad/variables.tf
+++ packages/nomad/variables.tf
@@ -88,7 +88,7 @@ variable "postgres_connection_string_secret_name" {
   type = string
 }
 
-variable "supabase_jwt_secrets_secret_data" {
+variable "supabase_jwt_secrets_secret_name" {
   type = string
 }
 
