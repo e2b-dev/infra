db89c0fcc144fac7e4328ead64d0b02b128f2185
Jakub Dobry <jakub.dobry8@gmail.com>
2025-03-21 20:51:07 +0100

    Fix migration scripts

--- packages/shared/pkg/db/migrations/20241120222814_add_team_api_key_metadata.up.sql
+++ packages/shared/pkg/db/migrations/20241120222814_add_team_api_key_metadata.up.sql
@@ -5,7 +5,14 @@ ALTER TABLE "public"."team_api_keys"
     ADD COLUMN IF NOT EXISTS "updated_at" timestamptz NULL,
     ADD COLUMN IF NOT EXISTS "name" text NOT NULL DEFAULT 'Unnamed API Key',
     ADD COLUMN IF NOT EXISTS "last_used" timestamptz NULL,
-    ADD COLUMN IF NOT EXISTS "created_by" uuid NULL,
-    ADD CONSTRAINT IF NOT EXISTS "team_api_keys_users_created_api_keys" FOREIGN KEY ("created_by") REFERENCES "auth"."users" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
+    ADD COLUMN IF NOT EXISTS "created_by" uuid NULL;
+
+-- Add constraint separately
+ALTER TABLE "public"."team_api_keys"
+    ADD CONSTRAINT "team_api_keys_users_created_api_keys"
+        FOREIGN KEY ("created_by")
+            REFERENCES "auth"."users" ("id")
+            ON UPDATE NO ACTION
+            ON DELETE SET NULL;
 
 COMMIT; 
--- packages/shared/pkg/db/migrations/20241127174604_add_env_creator.up.sql
+++ packages/shared/pkg/db/migrations/20241127174604_add_env_creator.up.sql
@@ -2,7 +2,14 @@ BEGIN;
 
 -- Modify "envs" table
 ALTER TABLE "public"."envs"
-    ADD COLUMN IF NOT EXISTS "created_by" uuid NULL,
-    ADD CONSTRAINT IF NOT EXISTS "envs_users_created_envs" FOREIGN KEY ("created_by") REFERENCES "auth"."users" ("id") ON UPDATE NO ACTION ON DELETE SET NULL;
+    ADD COLUMN IF NOT EXISTS "created_by" uuid NULL;
+
+-- Add constraint
+ALTER TABLE "public"."envs"
+    ADD CONSTRAINT "envs_users_created_envs"
+        FOREIGN KEY ("created_by")
+            REFERENCES "auth"."users" ("id")
+            ON UPDATE NO ACTION
+            ON DELETE SET NULL;
 
 COMMIT; 
