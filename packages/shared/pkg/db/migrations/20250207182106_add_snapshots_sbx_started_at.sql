deeae261ff0dc233bd851e47649346a0117e2220
Jakub Novak <jakub@e2b.dev>
2025-03-26 15:03:28 +0100

    Add Begin and Commit

--- packages/shared/pkg/db/migrations/20250207182106_add_snapshots_sbx_started_at.sql
+++ packages/shared/pkg/db/migrations/20250207182106_add_snapshots_sbx_started_at.sql
@@ -1,4 +1,6 @@
-ALTER TABLE "public"."snapshots" 
+BEGIN;
+
+ALTER TABLE "public"."snapshots"
   ADD COLUMN IF NOT EXISTS "sandbox_started_at" timestamp with time zone NOT NULL 
   DEFAULT TIMESTAMP WITH TIME ZONE '1970-01-01 00:00:00 UTC';
 
@@ -15,3 +17,5 @@ WHERE s.sandbox_id = latest_starts.sandbox_id;
 -- Remove the default constraint after populating data
 ALTER TABLE "public"."snapshots"
 ALTER COLUMN "sandbox_started_at" DROP DEFAULT;
+
+COMMIT;
