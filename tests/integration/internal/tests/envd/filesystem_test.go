d73ac8c04fa5260938715365dc077ce970434726
r33drichards <rwendt1337@gmail.com>
2025-04-04 05:23:43 -0700

    Use chown of chmod for user home dir to fix permission and ownership issues (#512)

--- tests/integration/internal/tests/envd/filesystem_test.go
+++ tests/integration/internal/tests/envd/filesystem_test.go
@@ -5,11 +5,13 @@ import (
 	"context"
 	"mime/multipart"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/e2b-dev/infra/tests/integration/internal/api"
 	envdapi "github.com/e2b-dev/infra/tests/integration/internal/envd/api"
 	"github.com/e2b-dev/infra/tests/integration/internal/envd/filesystem"
+	"github.com/e2b-dev/infra/tests/integration/internal/envd/process"
 	"github.com/e2b-dev/infra/tests/integration/internal/setup"
 
 	"connectrpc.com/connect"
@@ -57,6 +59,47 @@ func TestCreateFile(t *testing.T) {
 	assert.NoError(t, err)
 
 	assert.Equal(t, http.StatusOK, createFileResp.StatusCode())
+}
+
+func TestFilePermissions(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sbx := createSandbox(t, setup.WithAPIKey())
+
+	envdClient := setup.GetEnvdClient(t, ctx)
+	req := connect.NewRequest(&process.StartRequest{
+		Process: &process.ProcessConfig{
+			Cmd:  "ls",
+			Args: []string{"-la", "/home/user"},
+		},
+	})
+	setup.SetSandboxHeader(req.Header(), sbx.JSON201.SandboxID, sbx.JSON201.ClientID)
+	setup.SetUserHeader(req.Header(), "user")
+	stream, err := envdClient.ProcessClient.Start(
+		ctx,
+		req,
+	)
+
+	assert.NoError(t, err)
+
+	defer stream.Close()
+
+	out := []string{}
+
+	for stream.Receive() {
+		msg := stream.Msg()
+		out = append(out, msg.String())
+	}
+
+	// in the output, we should see the files .bashrc and .profile, and they should have the correct permissions
+	for _, line := range out {
+		if strings.Contains(line, ".bashrc") || strings.Contains(line, ".profile") {
+			assert.Contains(t, line, "-rw-r--r--")
+			assert.Contains(t, line, "user user")
+		}
+	}
+
 }
 
 func createSandbox(t *testing.T, reqEditors ...api.RequestEditorFn) *api.PostSandboxesResponse {
