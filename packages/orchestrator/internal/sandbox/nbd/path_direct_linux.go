65117de911d564947f610ed80c1956c93037729a
Jakub Dobry <jakub.dobry8@gmail.com>
2025-04-08 13:34:36 +0200

    close all socks on reconnect and direct path close

--- packages/orchestrator/internal/sandbox/nbd/path_direct_linux.go
+++ packages/orchestrator/internal/sandbox/nbd/path_direct_linux.go
@@ -43,7 +43,8 @@ type DirectPathMount struct {
 	blockSize   uint64
 
 	dispatchers []*Dispatch
-	socks       []io.Closer
+	socksClient []*os.File
+	socksServer []io.Closer
 
 	handlersWg sync.WaitGroup
 }
@@ -52,13 +53,14 @@ func NewDirectPathMount(tracer trace.Tracer, b block.Device, devicePool *DeviceP
 	ctx, cancelfn := context.WithCancel(context.Background())
 
 	return &DirectPathMount{
-		tracer:     tracer,
-		Backend:    b,
-		ctx:        ctx,
-		cancelfn:   cancelfn,
-		blockSize:  4096,
-		devicePool: devicePool,
-		socks:      make([]io.Closer, 0),
+		tracer:      tracer,
+		Backend:     b,
+		ctx:         ctx,
+		cancelfn:    cancelfn,
+		blockSize:   4096,
+		devicePool:  devicePool,
+		socksClient: make([]*os.File, 0),
+		socksServer: make([]io.Closer, 0),
 	}
 }
 
@@ -68,14 +70,14 @@ func (d *DirectPathMount) Open(ctx context.Context) (deviceIndex uint32, err err
 		return 0, err
 	}
 
-	// TODO: Do we need retry getting the device or it is enough to retry the dispatchers?
 	d.deviceIndex, err = d.devicePool.GetDevice(ctx)
 	if err != nil {
 		return 0, err
 	}
 
 	for {
-		socks := make([]*os.File, 0)
+		d.socksClient = make([]*os.File, 0)
+		d.socksServer = make([]io.Closer, 0)
 		d.dispatchers = make([]*Dispatch, 0)
 
 		for i := 0; i < connections; i++ {
@@ -108,8 +110,8 @@ func (d *DirectPathMount) Open(ctx context.Context) (deviceIndex uint32, err err
 				)
 			}()
 
-			d.socks = append(d.socks, serverc)
-			socks = append(socks, client)
+			d.socksServer = append(d.socksServer, serverc)
+			d.socksClient = append(d.socksClient, client)
 			d.dispatchers = append(d.dispatchers, dispatch)
 		}
 
@@ -120,7 +122,7 @@ func (d *DirectPathMount) Open(ctx context.Context) (deviceIndex uint32, err err
 
 		serverFlags := nbdnl.FlagHasFlags | nbdnl.FlagCanMulticonn
 
-		idx, err := nbdnl.Connect(d.deviceIndex, socks, uint64(size), 0, serverFlags, opts...)
+		idx, err := nbdnl.Connect(d.deviceIndex, d.socksClient, uint64(size), 0, serverFlags, opts...)
 		if err == nil {
 			// The idx should be the same as d.deviceIndex, because we are connecting to it,
 			// but we will use the one returned by nbdnl
@@ -133,7 +135,10 @@ func (d *DirectPathMount) Open(ctx context.Context) (deviceIndex uint32, err err
 
 		// Sometimes (rare), there seems to be a BADF error here. Lets just retry for now...
 		// Close things down and try again...
-		for _, sock := range socks {
+		for _, sock := range d.socksClient {
+			sock.Close()
+		}
+		for _, sock := range d.socksServer {
 			sock.Close()
 		}
 
@@ -175,9 +180,9 @@ func (d *DirectPathMount) Close(ctx context.Context) error {
 	telemetry.ReportEvent(childCtx, "canceling context")
 	d.cancelfn()
 
-	// Close all the socket pairs...
-	telemetry.ReportEvent(childCtx, "closing socket pairs")
-	for _, v := range d.socks {
+	// Close all server socket pairs...
+	telemetry.ReportEvent(childCtx, "closing socket pairs server")
+	for _, v := range d.socksServer {
 		err := v.Close()
 		if err != nil {
 			return err
@@ -217,6 +222,15 @@ func (d *DirectPathMount) Close(ctx context.Context) error {
 			break
 		}
 		time.Sleep(100 * time.Nanosecond)
+	}
+
+	// Close all client socket pairs...
+	telemetry.ReportEvent(childCtx, "closing socket pairs client")
+	for _, v := range d.socksClient {
+		err := v.Close()
+		if err != nil {
+			return err
+		}
 	}
 
 	// Release the device back to the pool, retry if it is in use
