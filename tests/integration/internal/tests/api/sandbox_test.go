1b46b7ea7fd4a2cf37013a494b21cf47920a40ad
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-04-11 16:56:15 +0200

    test error when resuming unknown sandbox
    check not just proper handling of unknown sandbox but also new sql query
    that is used for query of sbx snapshots here.

--- tests/integration/internal/tests/api/sandbox_test.go
+++ tests/integration/internal/tests/api/sandbox_test.go
@@ -40,3 +40,26 @@ func TestSandboxCreate(t *testing.T) {
 
 	assert.Equal(t, http.StatusCreated, resp.StatusCode())
 }
+
+func TestSandboxResumeUnknownSandbox(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	c := setup.GetAPIClient()
+
+	sbxCreate, err := c.PostSandboxesWithResponse(ctx, api.NewSandbox{TemplateID: setup.SandboxTemplateID}, setup.WithAPIKey())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// try to generate non-existing sandbox id but with real client part
+	unknownSbxId := "xxx" + sbxCreate.JSON201.SandboxID[3:] + "-" + sbxCreate.JSON201.ClientID
+
+	sbxResume, err := c.PostSandboxesSandboxIDResumeWithResponse(ctx, unknownSbxId, api.PostSandboxesSandboxIDResumeJSONRequestBody{}, setup.WithAPIKey())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, http.StatusNotFound, sbxResume.StatusCode())
+	assert.Equal(t, "{\"code\":404,\"message\":\"Sandbox snapshot not found\"}", string(sbxResume.Body))
+}
