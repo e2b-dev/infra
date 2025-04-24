730346f47656950b14d0c0655c15e522ab7849ef
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-04-24 18:48:25 +0200

    Reduce the lb keepalive further

--- packages/cluster/network/main.tf
+++ packages/cluster/network/main.tf
@@ -312,7 +312,7 @@ resource "google_compute_url_map" "orch_map" {
 resource "google_compute_target_https_proxy" "default" {
   name                        = "${var.prefix}https-proxy"
   url_map                     = google_compute_url_map.orch_map.self_link
-  http_keep_alive_timeout_sec = 540
+  http_keep_alive_timeout_sec = 300
 
   certificate_map = "//certificatemanager.googleapis.com/${google_certificate_manager_certificate_map.certificate_map.id}"
 }
