de58bd0f3d333598b5621b582dcdbc7d7c919d53
Jakub Novak <jakub@e2b.dev>
2025-04-30 11:14:50 +0200

    Fix query

--- packages/db/queries/get_env_with_build.sql
+++ packages/db/queries/get_env_with_build.sql
@@ -10,6 +10,6 @@ SELECT
 FROM public.envs e
 LEFT JOIN public.env_aliases ea ON ea.env_id = e.id
 JOIN public.env_builds eb ON eb.env_id = e.id
-WHERE e.id = $1 OR ea.alias = $1
+WHERE (e.id = $1 OR ea.alias = $1) and eb.status = 'uploaded'
 ORDER BY eb.finished_at DESC
 LIMIT 1;
