f19ed449b19050c7c9b8a3d2fcc4c02cbfc61b2f
Jakub Novak <jakub@e2b.dev>
2025-05-04 19:14:24 +0200

    Extract /health call to separate function and report node status

--- packages/api/internal/orchestrator/client.go
+++ packages/api/internal/orchestrator/client.go
@@ -11,6 +11,7 @@ import (
 	"github.com/jellydator/ttlcache/v3"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/otel/attribute"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
@@ -66,33 +67,30 @@ func (o *Orchestrator) connectToNode(ctx context.Context, node *node.NodeInfo) e
 	buildCache := ttlcache.New[string, interface{}]()
 	go buildCache.Start()
 
-	resp, err := o.httpClient.Get(fmt.Sprintf("http://%s/health", node.OrchestratorAddress))
+	status := api.NodeStatusUnhealthy
+	version := "unknown"
+
+	nodeStatus, err := o.getNodeHealth(node)
 	if err != nil {
-		return fmt.Errorf("failed to check node health: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("node is not healthy: %s", resp.Status)
+		zap.L().Error("Failed to get node health", zap.Error(err))
 	}
 
-	// Check if the node is healthy
-	var healthResp e2bHealth.Response
-	err = json.NewDecoder(resp.Body).Decode(&healthResp)
-	if err != nil {
-		return fmt.Errorf("failed to decode health response: %w", err)
-	}
+	if nodeStatus != nil {
+		if nodeStatus.Status == e2bHealth.Healthy {
+			status = api.NodeStatusReady
+		} else {
+			status = api.NodeStatusUnhealthy
+		}
 
-	if healthResp.Status != e2bHealth.Healthy {
-		return fmt.Errorf("node is not healthy: %s", healthResp.Status)
+		version = nodeStatus.Version
 	}
 
 	n := &Node{
 		Client:         client,
 		buildCache:     buildCache,
 		sbxsInProgress: smap.New[*sbxInProgress](),
-		status:         api.NodeStatusReady,
-		version:        healthResp.Version,
+		status:         status,
+		version:        version,
 		Info:           node,
 		createFails:    atomic.Uint64{},
 	}
@@ -108,4 +106,25 @@ func (o *Orchestrator) GetClient(nodeID string) (*GRPCClient, error) {
 	}
 
 	return n.Client, nil
+}
+
+func (o *Orchestrator) getNodeHealth(node *node.NodeInfo) (*e2bHealth.Response, error) {
+	resp, err := o.httpClient.Get(fmt.Sprintf("http://%s/health", node.OrchestratorAddress))
+	if err != nil {
+		return nil, fmt.Errorf("failed to check node health: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("node is not healthy: %s", resp.Status)
+	}
+
+	// Check if the node is healthy
+	var healthResp e2bHealth.Response
+	err = json.NewDecoder(resp.Body).Decode(&healthResp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode health response: %w", err)
+	}
+
+	return &healthResp, nil
 }
