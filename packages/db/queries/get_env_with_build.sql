3ee78c27913c2316c96dbf62ce02e48ded7228c7
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-07 14:50:06 +0200

    adjust getn env with latest build with index optimization
    previously when we tried to found `env.id = x OR env_alias.alias = x`
    env id or alias alias index was nost used causing sequential visit of
    whole table.

--- packages/db/queries/get_env_with_build.sql
+++ packages/db/queries/get_env_with_build.sql
@@ -1,15 +1,21 @@
 -- name: GetEnvWithBuild :one
-SELECT
-  sqlc.embed(e),
-  sqlc.embed(eb),
-  (
-    SELECT array_agg(alias)::text[]
-    FROM public.env_aliases
-    WHERE env_id = e.id
-  ) AS aliases
-FROM public.envs e
-LEFT JOIN public.env_aliases ea ON ea.env_id = e.id
-JOIN public.env_builds eb ON eb.env_id = e.id
-WHERE (e.id = $1 OR ea.alias = $1) and eb.status = 'uploaded'
+WITH s AS NOT MATERIALIZED (
+    SELECT ea.env_id as env_id
+    FROM public.env_aliases as ea
+    WHERE ea.alias = @alias_or_env_id
+    UNION
+    SELECT @alias_or_env_id as env_id
+)
+
+SELECT sqlc.embed(e), sqlc.embed(eb), aliases
+FROM s
+JOIN public.envs AS e  ON e.id = s.env_id
+JOIN public.env_builds AS eb ON eb.env_id = e.id
+AND eb.status = 'uploaded'
+CROSS  JOIN LATERAL (
+    SELECT array_agg(alias)::text[] AS aliases
+    FROM   public.env_aliases
+    WHERE  env_id = e.id
+) AS al
 ORDER BY eb.finished_at DESC
-LIMIT 1;
+LIMIT  1;
