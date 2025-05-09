876862fa7b5ef8f6672122dcefdbe349a34bb710
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-09 15:32:11 +0200

    cleanup shutdown process, edge service creation

--- packages/client-proxy/main.go
+++ packages/client-proxy/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	_ "embed"
 	"errors"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -18,104 +17,150 @@ import (
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 
-	client_proxy "github.com/e2b-dev/infra/packages/proxy/internal/proxy"
-	e2bLogger "github.com/e2b-dev/infra/packages/shared/pkg/logger"
-
 	"github.com/e2b-dev/infra/packages/proxy/internal/edge"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/api"
+	e2bProxy "github.com/e2b-dev/infra/packages/proxy/internal/proxy"
 	"github.com/e2b-dev/infra/packages/shared/pkg/env"
+	e2bLogger "github.com/e2b-dev/infra/packages/shared/pkg/logger"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
 
 const (
-	ServiceName     = "client-proxy"
-	healthCheckPort = 3001
-	port            = 3002
+	ServiceName = "client-proxy"
+
+	edgePort  = 3001
+	proxyPort = 3002
+
+	shutdownDrainingWait  = 30 * time.Second
+	shutdownUnhealthyWait = 30 * time.Second
 )
 
 var commitSHA string
 
 func run() int {
 	exitCode := atomic.Int32{}
+	exitTermination := true
+
 	wg := sync.WaitGroup{}
 
-	ctx := context.Background()
+	ctx, ctxCancel := context.WithCancel(context.Background())
+	defer ctxCancel()
+
 	signalCtx, sigCancel := signal.NotifyContext(ctx, syscall.SIGTERM, syscall.SIGINT)
 	defer sigCancel()
 
 	instanceID := uuid.New().String()
-	stopOtlp := telemetry.InitOTLPExporter(ctx, ServiceName, commitSHA, instanceID)
+	stopOTLP := telemetry.InitOTLPExporter(ctx, ServiceName, commitSHA, instanceID)
+
 	defer func() {
-		err := stopOtlp(ctx)
+		err := stopOTLP(ctx)
 		if err != nil {
 			log.Printf("telemetry shutdown:%v\n", err)
 		}
 	}()
 
-	logger := zap.Must(e2bLogger.NewLogger(ctx, e2bLogger.LoggerConfig{
-		ServiceName: ServiceName,
-		IsInternal:  true,
-		IsDebug:     env.IsDebug(),
-		Cores:       []zapcore.Core{e2bLogger.GetOTELCore(ServiceName)},
-	}))
+	logger := zap.Must(
+		e2bLogger.NewLogger(
+			ctx, e2bLogger.LoggerConfig{
+				ServiceName: ServiceName,
+				IsInternal:  true,
+				IsDebug:     env.IsDebug(),
+				Cores:       []zapcore.Core{e2bLogger.GetOTELCore(ServiceName)},
+			},
+		),
+	)
+
 	defer func() {
 		err := logger.Sync()
 		if err != nil {
 			log.Printf("logger sync error: %v\n", err)
 		}
 	}()
-	zap.ReplaceGlobals(logger)
 
+	zap.ReplaceGlobals(logger)
 	logger.Info("Starting client proxy", zap.String("commit", commitSHA), zap.String("instance_id", instanceID))
 
 	// Proxy request to the correct node
-	proxy, err := client_proxy.NewClientProxy(port)
+	proxy, err := e2bProxy.NewClientProxy(proxyPort)
 	if err != nil {
 		logger.Error("failed to create client proxy", zap.Error(err))
-
 		return 1
 	}
 
-	proxyDrainingHandler := func() {
-		logger.Info("draining http proxy")
-		if err := server.Shutdown(ctx); err != nil {
-			logger.Error("failed to shutdown server", zap.Error(err))
-		}
+	edgeApiDrainingHandler := func(terminate bool) {
+		// todo: for now we want to always terminate, but sometimes we dont want to because systemd will restart us
+		//exitTermination = terminate
+		exitTermination = true
+
+		sigCancel() // trigger shutdown workflow
 	}
 
+	edgeApiStore, err := edge.NewEdgeAPIStore(ctx, logger, &edgeApiDrainingHandler)
+	if err != nil {
+		logger.Error("failed to create edge api store", zap.Error(err))
+		return 1
+	}
+
+	edgeApiSwagger, err := api.GetSwagger()
+	if err != nil {
+		logger.Error("failed to get swagger", zap.Error(err))
+		return 1
+	}
+
+	// Edge API server
+	edgerGinServer := edge.NewGinServer(ctx, logger, edgeApiStore, edgePort, edgeApiSwagger)
+
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		err := edge.Run(ctx, logger, proxyDrainingHandler)
-		if err != nil {
-			exitCode.Add(1)
-			logger.Error("edge service error", zap.Error(err))
 
-			// todo: just for now, we need proper exit handling
-			os.Exit(1)
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
 		// make sure to cancel the parent context before this
 		// goroutine returns, so that in the case of a panic
 		// or error here, the other thread won't block until
 		// signaled.
 		defer sigCancel()
 
-		logger.Info("http service starting", zap.Int("port", port))
+		edgeRunLogger := logger.With(zap.Int("edge_port", edgePort))
+		edgeRunLogger.Info("edge http service starting")
+
+		err := edgerGinServer.ListenAndServe()
+		if err != nil {
+			switch {
+			case errors.Is(err, http.ErrServerClosed):
+				edgeRunLogger.Info("edge http service shutdown successfully")
+			case err != nil:
+				exitCode.Add(1)
+				edgeRunLogger.Error("edge http service encountered error", zap.Error(err))
+			default:
+				edgeRunLogger.Info("edge http service exited without error")
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		// make sure to cancel the parent context before this
+		// goroutine returns, so that in the case of a panic
+		// or error here, the other thread won't block until
+		// signaled.
+		defer sigCancel()
+
+		proxyRunLogger := logger.With(zap.Int("proxy_port", proxyPort))
+		proxyRunLogger.Info("proxy http service starting")
+
 		err := proxy.ListenAndServe()
 		// Add different handling for the error
 		switch {
 		case errors.Is(err, http.ErrServerClosed):
-			logger.Info("http service shutdown successfully", zap.Int("port", port))
+			proxyRunLogger.Info("http service shutdown successfully")
 		case err != nil:
 			exitCode.Add(1)
-			logger.Error("http service encountered error", zap.Int("port", port), zap.Error(err))
+			proxyRunLogger.Error("http service encountered error", zap.Error(err))
 		default:
 			// this probably shouldn't happen...
-			logger.Error("http service exited without error", zap.Int("port", port))
+			proxyRunLogger.Error("http service exited without error")
 		}
 	}()
 
@@ -123,22 +168,51 @@ func run() int {
 	go func() {
 		defer wg.Done()
 		<-signalCtx.Done()
-		logger.Info("shutting down http service", zap.Int("port", healthCheckPort))
-		// TODO: We might want to wait for the idle connections to transition to closed instead of closing them.
-		if err := healthServer.Shutdown(ctx); err != nil {
-			exitCode.Add(1)
-			logger.Error("http service shutdown error", zap.Int("port", healthCheckPort), zap.Error(err))
-		}
 
+		shutdownLogger := logger.With(zap.Int("proxy_port", proxyPort), zap.Int("edge_port", edgePort))
+		shutdownLogger.Info("shutting down services")
+
+		edgeApiStore.SetDraining()
+
+		// we should wait for health check manager to notice that we are not ready for new traffic
 		if env.IsProduction() {
-			logger.Info("waiting 15 seconds before shutting down http service")
-			time.Sleep(15 * time.Second)
+			shutdownLogger.Info("waiting for draining state propagation", zap.Float64("wait_in_seconds", shutdownDrainingWait.Seconds()))
+			time.Sleep(shutdownDrainingWait)
 		}
 
-		logger.Info("shutting down http service", zap.Int("port", port))
-		if err := proxy.Shutdown(ctx); err != nil {
+		proxyShutdownCtx, proxyShutdownCtxCancel := context.WithTimeout(ctx, 24*time.Hour)
+		defer proxyShutdownCtxCancel()
+
+		// gracefully shutdown the proxy http server
+		err := proxy.Shutdown(proxyShutdownCtx)
+		if err != nil {
 			exitCode.Add(1)
-			logger.Error("http service shutdown error", zap.Int("port", port), zap.Error(err))
+			shutdownLogger.Error("proxy http service shutdown error", zap.Error(err))
+		} else {
+			shutdownLogger.Info("proxy http service shutdown successfully")
+		}
+
+		edgeApiStore.SetUnhealthy()
+
+		// wait for the health check manager to notice that we are not healthy at all
+		if env.IsProduction() {
+			shutdownLogger.Info("waiting for unhealthy state propagation", zap.Float64("wait_in_seconds", shutdownUnhealthyWait.Seconds()))
+			time.Sleep(shutdownUnhealthyWait)
+		}
+
+		edgeApiStore.GracefullyShutdown()
+
+		ginErr := edgerGinServer.Shutdown(ctx)
+		if ginErr != nil {
+			exitCode.Add(1)
+			shutdownLogger.Error("edge http service shutdown error", zap.Error(ginErr))
+		}
+
+		// todo: when running with systemd, we should not exit because it will be restarted
+		// there should be flag for exit disabling or some other way to signal that we are done without restarting process
+		// still its should be able to exit because to self-update requests that
+		if !exitTermination {
+			// todo: implement this
 		}
 	}()
 
