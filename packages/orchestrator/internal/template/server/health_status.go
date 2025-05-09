f62715da03fd9f7f28d821155a9d54c7c2e19279
Jakub Dobry <jakub.dobry8@gmail.com>
2025-05-09 18:15:28 +0200

    remove logging of template status

--- packages/orchestrator/internal/template/server/health_status.go
+++ packages/orchestrator/internal/template/server/health_status.go
@@ -3,7 +3,6 @@ package server
 import (
 	"context"
 
-	"go.uber.org/zap"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/template-manager"
@@ -12,8 +11,6 @@ import (
 func (s *ServerStore) HealthStatus(ctx context.Context, req *emptypb.Empty) (*template_manager.HealthStatusResponse, error) {
 	ctx, ctxSpan := s.tracer.Start(ctx, "health-status-request")
 	defer ctxSpan.End()
-
-	s.logger.Debug("Template manager health status request", zap.String("status", s.healthStatus.String()))
 
 	return &template_manager.HealthStatusResponse{
 		Status: s.healthStatus,
