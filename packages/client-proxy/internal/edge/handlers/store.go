15501070ed4a2d7c864f93fcb813565e7706fedc
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-09 15:31:06 +0200

    edge api store

--- packages/client-proxy/internal/edge/handlers/store.go
+++ packages/client-proxy/internal/edge/handlers/store.go
@@ -1,13 +1,17 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
-	service_discovery "github.com/e2b-dev/infra/packages/proxy/internal/edge/service-discovery"
-	"github.com/e2b-dev/infra/packages/proxy/internal/edge/updater"
+
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
 
 	"github.com/e2b-dev/infra/packages/proxy/internal/edge/api"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/orchestrators"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/updater"
+	servicediscovery "github.com/e2b-dev/infra/packages/proxy/internal/service-discovery"
+	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
 
 type APIStore struct {
@@ -16,23 +20,36 @@ type APIStore struct {
 	selfUpdateHandler *func() updater.UpdaterResponse
 	selfDrainHandler  *func() error
 
-	logger           *zap.Logger
-	serviceDiscovery *service_discovery.ServiceDiscovery
+	logger            *zap.Logger
+	serviceDiscovery  servicediscovery.ServiceDiscoveryAdapter
+	orchestratorsPool *orchestrators.Pool
 }
 
-func NewStore(serviceDiscovery *service_discovery.ServiceDiscovery, logger *zap.Logger, selfUpdateHandler *func() updater.UpdaterResponse, selfDrainHandler *func() error) (*APIStore, error) {
+func NewStore(serviceDiscovery servicediscovery.ServiceDiscoveryAdapter, logger *zap.Logger, selfUpdateHandler *func() updater.UpdaterResponse, selfDrainHandler *func() error) (*APIStore, error) {
+	pool := orchestrators.NewOrchestratorsPool(context.TODO(), logger, serviceDiscovery)
+
 	return &APIStore{
-		serviceDiscovery: serviceDiscovery,
-		logger:           logger,
-		healthStatus:     api.Healthy,
+		serviceDiscovery:  serviceDiscovery,
+		orchestratorsPool: pool,
+
+		logger:       logger,
+		healthStatus: api.Healthy,
 
 		selfDrainHandler:  selfDrainHandler,
 		selfUpdateHandler: selfUpdateHandler,
 	}, nil
 }
 
-func (a *APIStore) SetHealth(health api.ClusterNodeStatus) {
-	a.healthStatus = health
+func (a *APIStore) SetDraining() {
+	a.healthStatus = api.Draining
+}
+
+func (a *APIStore) SetUnhealthy() {
+	a.healthStatus = api.Unhealthy
+}
+
+func (a *APIStore) GracefullyShutdown() {
+	a.serviceDiscovery.SetSelfStatus(servicediscovery.StatusDraining)
 }
 
 func (a *APIStore) sendAPIStoreError(c *gin.Context, code int, message string) {
@@ -43,4 +60,15 @@ func (a *APIStore) sendAPIStoreError(c *gin.Context, code int, message string) {
 
 	c.Error(fmt.Errorf(message))
 	c.JSON(code, apiErr)
+}
+
+func parseBody[B any](ctx context.Context, c *gin.Context) (body B, err error) {
+	err = c.Bind(&body)
+	if err != nil {
+		bodyErr := fmt.Errorf("error when parsing request: %w", err)
+		telemetry.ReportCriticalError(ctx, bodyErr)
+		return body, bodyErr
+	}
+
+	return body, nil
 }
