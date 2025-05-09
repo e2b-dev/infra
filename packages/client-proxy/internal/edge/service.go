ae632f6c14e9fc2fd1a39074f0139337e95319d8
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-09 15:32:11 +0200

    cleanup shutdown process, edge service creation

--- packages/client-proxy/internal/edge/service.go
+++ packages/client-proxy/internal/edge/service.go
@@ -2,42 +2,26 @@ package edge
 
 import (
 	"context"
-	"fmt"
-	"github.com/e2b-dev/infra/packages/proxy/internal/edge/api"
-	configuration "github.com/e2b-dev/infra/packages/proxy/internal/edge/configurator"
-	"github.com/e2b-dev/infra/packages/proxy/internal/edge/handlers"
-	"github.com/e2b-dev/infra/packages/proxy/internal/edge/service-discovery"
-	updater "github.com/e2b-dev/infra/packages/proxy/internal/edge/updater"
-	"github.com/e2b-dev/infra/packages/shared/pkg/env"
-	"github.com/google/uuid"
-	"github.com/redis/go-redis/v9"
-	"go.uber.org/zap"
-	"net"
-	"net/http"
-	"os"
 	"time"
+
+	"github.com/google/uuid"
+	"go.uber.org/zap"
+
+	"github.com/e2b-dev/infra/packages/proxy/internal/configurator"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/handlers"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/updater"
+	"github.com/e2b-dev/infra/packages/proxy/internal/service-discovery"
 )
 
-type Service struct {
-	serverPort  int
-	server      *http.Server
-	serverStore *handlers.APIStore
-
-	logger *zap.Logger
-
-	serviceDiscovery *service_discovery.ServiceDiscovery
-	updater          *updater.Updater
-}
-
 const (
-	serviceVersion = "v1.1.0"
+	serviceVersion = "v2.0.0"
 	serviceType    = "edge"
 
 	configSetupTimeout = 5 * time.Second
 )
 
-func NewService(ctx context.Context, logger *zap.Logger, proxyDrainingHandler func()) (*Service, error) {
-	configAdapter, err := configuration.NewAutoConfigurationAdapter()
+func NewEdgeAPIStore(ctx context.Context, logger *zap.Logger, drainingHandler *func(terminate bool)) (*handlers.APIStore, error) {
+	configAdapter, err := configuration.NewConfigurationAdapter()
 	if err != nil {
 		return nil, err
 	}
@@ -50,16 +34,15 @@ func NewService(ctx context.Context, logger *zap.Logger, proxyDrainingHandler fu
 		return nil, err
 	}
 
-	opts, err := redis.ParseURL(config.RedisUrl)
-	if err != nil {
-		return nil, err
-	}
+	//opts, err := redis.ParseURL(config.RedisUrl)
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	serviceId := uuid.NewString()
-	serviceDiscovery := service_discovery.NewServiceDiscovery(
-		&service_discovery.ServiceDiscoveryConfig{
-			RedisClient: redis.NewClient(opts),
-			Logger:      logger,
+	serviceDiscovery := service_discovery.NewDnsServiceDiscovery(
+		&service_discovery.DnsServiceDiscoveryConfig{
+			Logger: logger,
 
 			NodePort: config.ServicePort,
 			NodeIp:   config.ServiceIpv4,
@@ -68,163 +51,45 @@ func NewService(ctx context.Context, logger *zap.Logger, proxyDrainingHandler fu
 			ServiceType:    serviceType,
 			ServiceVersion: serviceVersion,
 			ServiceStatus:  service_discovery.StatusHealthy,
+
+			// todo: this should be ideally taken from some ENV
+			OrchestratorsDomain: "orchestrator.service.consul",
+			OrchestratorsPort:   5008,
 		},
 	)
 
-	// todo: make it configurable
-	updaterUrl := "https://e2b-eu-west-1-assets.s3.eu-west-1.amazonaws.com/edge-agent"
-	updaterService := updater.NewUpdater(updaterUrl, logger)
+	/*
+		serviceDiscovery := service_discovery.NewRedisServiceDiscovery(
+			ctx,
+			&service_discovery.RedisServiceDiscoveryConfig{
+				RedisClient: redis.NewClient(opts),
+				Logger:      logger,
 
-	var serverStore *handlers.APIStore
-	var server *http.Server
+				NodePort: config.ServicePort,
+				NodeIp:   config.ServiceIpv4,
 
-	selfUpdateHandler := func() updater.UpdaterResponse {
-		logger.Info("self update handler called")
-
-		if updaterService == nil {
-			err := fmt.Errorf("service updater is not configured")
-			return updater.UpdaterResponse{
-				Success: false,
-				Message: err.Error(),
-				Error:   err,
-			}
-		}
-
-		updateCtx, updateCtxCancel := context.WithTimeout(context.Background(), 15*time.Second)
-		defer updateCtxCancel()
-
-		// let's check if its possible to update
-		// todo: add some handling of current version in some catalog or something
-		updateResp := updaterService.Update(updateCtx, nil)
-		if !updateResp.Success {
-			return updateResp
-		}
-
-		// let's make service as draining and start shutdown process
-		serviceDiscovery.SetStatus(service_discovery.StatusDraining)
-
-		go func() {
-			// wait for services to realize we are unhealthy)
-			if !env.IsLocal() {
-				time.Sleep(30 * time.Second)
-			}
-
-			// start draining of http proxy
-			proxyDrainingHandler()
-
-			serviceDiscovery.SetStatus(service_discovery.StatusUnhealthy)
-			serverStore.SetHealth(service_discovery.StatusUnhealthy)
-
-			// wait for services to realize we are unhealthy
-			// mainly just for in-process requests etc
-			time.Sleep(15 * time.Second)
-
-			// shutdown edge http server
-			shutdownCtx, shutdownCtxCancel := context.WithTimeout(context.Background(), 15*time.Second)
-			defer shutdownCtxCancel()
-
-			err := server.Shutdown(shutdownCtx)
-			if err != nil {
-				logger.Error("failed to shutdown server", zap.Error(err))
-			}
-
-			// todo: this should be done carefully in service main
-			os.Exit(0)
-		}()
-
-		// todo: shutdown process must be handled async because we are still in-request
-		return updater.UpdaterResponse{
-			Success: true,
-		}
-	}
+				ServiceId:      serviceId,
+				ServiceType:    serviceType,
+				ServiceVersion: serviceVersion,
+				ServiceStatus:  service_discovery.StatusHealthy,
+			},
+		)
+	*/
 
 	selfDrainHandler := func() error {
-		logger.Info("self drain handler called")
-		serviceDiscovery.SetStatus(service_discovery.StatusDraining)
-
-		// let's make service as draining and start shutdown process
-		serviceDiscovery.SetStatus(service_discovery.StatusDraining)
-
-		go func() {
-			// wait for services to realize we are unhealthy)
-			if !env.IsLocal() {
-				time.Sleep(30 * time.Second)
-			}
-
-			// start draining of http proxy
-			proxyDrainingHandler()
-
-			serviceDiscovery.SetStatus(service_discovery.StatusUnhealthy)
-			serverStore.SetHealth(service_discovery.StatusUnhealthy)
-
-			// wait for services to realize we are unhealthy
-			// mainly just for in-process requests etc
-			time.Sleep(15 * time.Second)
-
-			// shutdown edge http server
-			shutdownCtx, shutdownCtxCancel := context.WithTimeout(context.Background(), 15*time.Second)
-			defer shutdownCtxCancel()
-
-			err := server.Shutdown(shutdownCtx)
-			if err != nil {
-				logger.Error("failed to shutdown server", zap.Error(err))
-			}
-
-			// we wil just wait for scaling manager to kill whole instance
-			// because otherwise we will be just started again with systemd manager
-		}()
-
+		(*drainingHandler)(false) // program should stay alive and terminated whole instance
 		return nil
 	}
 
-	serverStore, err = handlers.NewStore(serviceDiscovery, logger, &selfUpdateHandler, &selfDrainHandler)
+	selfUpdateHandler := func() updater.UpdaterResponse {
+		(*drainingHandler)(true) // we want to restart service after update
+		panic("not implemented")
+	}
+
+	store, err := handlers.NewStore(serviceDiscovery, logger, &selfUpdateHandler, &selfDrainHandler)
 	if err != nil {
 		return nil, err
 	}
 
-	serverSwagger, err := api.GetSwagger()
-	if err != nil {
-		return nil, err
-	}
-
-	server = NewGinServer(ctx, logger, serverStore, config.ServicePort, serverSwagger)
-
-	return &Service{
-		serverPort:  config.ServicePort,
-		serverStore: serverStore,
-		server:      server,
-
-		logger: logger,
-
-		updater:          updaterService,
-		serviceDiscovery: serviceDiscovery,
-	}, nil
-}
-
-func (s *Service) Start() error {
-	zap.L().Info("starting edge service", zap.Int("port", s.serverPort))
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", s.serverPort))
-	if err != nil {
-		return fmt.Errorf("failed to listen on port %d: %w", s.serverPort, err)
-	}
-
-	if err := s.server.Serve(lis); err != nil {
-		println("edge server err")
-		return err
-	}
-
-	return nil
-}
-
-func (s *Service) Shutdown(ctx context.Context) error {
-	if s.server != nil {
-		return s.server.Shutdown(ctx)
-	}
-
-	return nil
-}
-
-func (s *Service) StartServiceDiscovery(ctx context.Context) {
-	go func() { s.serviceDiscovery.StartSelfRegistration(ctx) }()
+	return store, nil
 }
