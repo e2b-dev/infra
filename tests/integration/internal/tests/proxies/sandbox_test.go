ae4bcc6f78083df51a9c60270c7277a4320b3419
Jakub Novak <jakub@e2b.dev>
2025-05-13 13:11:08 +0200

    Add browser test

--- tests/integration/internal/tests/proxies/sandbox_test.go
+++ tests/integration/internal/tests/proxies/sandbox_test.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
+	"strings"
 	"testing"
 	"time"
 
@@ -76,7 +78,13 @@ func TestSandboxProxyPorts(t *testing.T) {
 		goodResp, err = client.Do(req)
 		if err == nil && goodResp.StatusCode == http.StatusOK {
 			break
+		} else if err != nil {
+			t.Logf("Error: %v", err)
+		} else {
+			t.Logf("Status code: %d", goodResp.StatusCode)
+			_ = goodResp.Body.Close()
 		}
+
 		time.Sleep(500 * time.Millisecond)
 	}
 	require.NoError(t, err)
@@ -106,7 +114,7 @@ func TestSandboxProxyPorts(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, badResp)
 	assert.Equal(t, http.StatusBadGateway, badResp.StatusCode)
-
+	assert.Equal(t, "application/json; charset=utf-8", badResp.Header.Get("Content-Type"))
 	// Parse error response
 	var errorResp struct {
 		Message   string `json:"message"`
@@ -118,4 +126,38 @@ func TestSandboxProxyPorts(t *testing.T) {
 	assert.Equal(t, "The sandbox is running but port is not open", errorResp.Message)
 	assert.Equal(t, sbx.SandboxID, errorResp.SandboxID)
 	assert.Equal(t, badPort, errorResp.Port)
+
+	// Pretend to be a browser
+	for i := 0; i < 10; i++ {
+		req := &http.Request{
+			Method: http.MethodGet,
+			URL:    url,
+			Header: http.Header{
+				"Host":       []string{host},
+				"User-Agent": []string{"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"},
+			},
+		}
+		badResp, err = client.Do(req)
+		if err == nil && badResp.StatusCode == http.StatusBadGateway {
+			break
+		} else if err != nil {
+			t.Logf("Error: %v", err)
+		} else {
+			t.Logf("Status code: %d", badResp.StatusCode)
+			_ = badResp.Body.Close()
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	require.NoError(t, err)
+	require.NotNil(t, badResp)
+	assert.Equal(t, http.StatusBadGateway, badResp.StatusCode)
+	assert.Equal(t, "text/html; charset=utf-8", badResp.Header.Get("Content-Type"))
+	body, err := io.ReadAll(badResp.Body)
+	require.NoError(t, err)
+
+	assert.True(t, strings.HasPrefix(string(body), "<html"))
+	assert.True(t, strings.Contains(string(body), "The sandbox is running but port is not open"))
+	assert.True(t, strings.Contains(string(body), sbx.SandboxID))
+	assert.True(t, strings.Contains(string(body), fmt.Sprintf("%d", badPort)))
+	assert.True(t, strings.HasSuffix(string(body), "</html>"))
 }
