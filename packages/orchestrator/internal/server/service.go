8a65f79da231cfc9d257edf76125dda3a3932874
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-14 11:43:40 +0200

    orchestrator added info service

---
+++ packages/orchestrator/internal/server/service.go
@@ -0,0 +1,38 @@
+package server
+
+import (
+	"context"
+
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator"
+)
+
+func (s *server) ServiceInfo(ctx context.Context, _ *emptypb.Empty) (*orchestrator.ServiceInfoResponse, error) {
+	info := s.info
+	status := orchestrator.ServiceInfoStatus_Healthy // todo
+
+	return &orchestrator.ServiceInfoResponse{
+		NodeId:         info.ClientId,
+		ServiceId:      info.ServiceId,
+		ServiceVersion: info.ServiceSourceVersion,
+		ServiceCommit:  info.ServiceSourceCommit,
+		ServiceStartup: timestamppb.New(info.ServiceStartup),
+		ServiceStatus:  status,
+
+		CanBuildTemplates: info.CanWorkAsTemplateBuilder,
+		CanSpawnSandboxes: info.CanWorkAsOrchestrator,
+
+		// todo: implement
+		MetricVcpuUsed:          1,
+		MetricMemoryUsedInBytes: 2,
+		MetricDiskUserInByte:    3,
+		MetricSandboxesRunning:  4,
+	}, nil
+}
+
+
+func (s *server) ServiceStatusOverride(ctx context.Context, req *orchestrator.ServiceStatusChangeRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
+}
