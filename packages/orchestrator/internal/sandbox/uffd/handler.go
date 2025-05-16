9d598d8c2067e8e28359e8a47a1de61b9453a37e
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-16 15:43:37 +0200

    removed node_id from logs that are not needed

--- packages/orchestrator/internal/sandbox/uffd/handler.go
+++ packages/orchestrator/internal/sandbox/uffd/handler.go
@@ -44,7 +44,6 @@ type Uffd struct {
 
 	memfile    *block.TrackedSliceDevice
 	socketPath string
-	clientID   string
 }
 
 func (u *Uffd) Disable() error {
@@ -55,7 +54,7 @@ func (u *Uffd) Dirty() *bitset.BitSet {
 	return u.memfile.Dirty()
 }
 
-func New(memfile block.ReadonlyDevice, socketPath string, blockSize int64, clientID string) (*Uffd, error) {
+func New(memfile block.ReadonlyDevice, socketPath string, blockSize int64) (*Uffd, error) {
 	pRead, pWrite, err := os.Pipe()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create exit fd: %w", err)
@@ -81,7 +80,6 @@ func New(memfile block.ReadonlyDevice, socketPath string, blockSize int64, clien
 
 			return nil
 		}),
-		clientID: clientID,
 	}, nil
 }
 
@@ -190,7 +188,6 @@ func (u *Uffd) handle(sandboxId string) (err error) {
 		u.exitReader.Fd(),
 		u.Stop,
 		sandboxId,
-		u.clientID,
 	)
 	if err != nil {
 		return fmt.Errorf("failed handling uffd: %w", err)
