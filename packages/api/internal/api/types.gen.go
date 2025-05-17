a5ac91ec0ab793d794440864352213b7e8632a88
Ben Fornefeld <ben.fornefeld@gmail.com>
2025-05-17 17:48:44 +0200

    WIP: refactor TeamApiKey oapi schema

--- packages/api/internal/api/types.gen.go
+++ packages/api/internal/api/types.gen.go
@@ -373,14 +373,23 @@ type TeamAPIKey struct {
 	// Id Identifier of the API key
 	Id openapi_types.UUID `json:"id"`
 
-	// KeyMask Mask of the API key
-	KeyMask string `json:"keyMask"`
+	// KeyPrefix Prefix that identifies the key type
+	KeyPrefix string `json:"keyPrefix"`
 
 	// LastUsed Last time this API key was used
 	LastUsed *time.Time `json:"lastUsed"`
 
+	// MaskedTokenPrefix Prefix used in masked version of token
+	MaskedTokenPrefix string `json:"maskedTokenPrefix"`
+
+	// MaskedTokenSuffix Suffix used in masked version of token
+	MaskedTokenSuffix string `json:"maskedTokenSuffix"`
+
 	// Name Name of the API key
 	Name string `json:"name"`
+
+	// TokenLength Length of the API key token
+	TokenLength int `json:"tokenLength"`
 }
 
 // TeamUser defines model for TeamUser.
