6ec0caeeac068590b5799c4106240c0efe2a5df8
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-20 11:19:43 +0200

    Return snapshot-base template alias in list of the sandboxes (#639)

--- packages/db/queries/get_last_snapshot.sql
+++ packages/db/queries/get_last_snapshot.sql
@@ -1,8 +1,13 @@
 -- name: GetLastSnapshot :one
-SELECT sqlc.embed(s), sqlc.embed(eb)
+SELECT COALESCE(ea.aliases, ARRAY[]::text[])::text[] AS aliases, sqlc.embed(s), sqlc.embed(eb)
 FROM "public"."snapshots" s
 JOIN "public"."envs" e ON s.env_id  = e.id
 JOIN "public"."env_builds" eb ON e.id = eb.env_id
+LEFT JOIN LATERAL (
+    SELECT ARRAY_AGG(alias ORDER BY alias) AS aliases
+    FROM "public"."env_aliases"
+    WHERE env_id = s.base_env_id
+) ea ON TRUE
 WHERE s.sandbox_id = $1 AND eb.status = 'success' AND e.team_id = $2
 ORDER BY eb.finished_at DESC
 LIMIT 1;
