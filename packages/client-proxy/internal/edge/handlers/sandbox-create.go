7ec0444e1b65a22645f89416c6c6efb3e6045d83
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-21 12:43:08 +0200

    edge api sandbox start endpoint

--- packages/client-proxy/internal/edge/handlers/sandbox-create.go
+++ packages/client-proxy/internal/edge/handlers/sandbox-create.go
@@ -1,76 +1,103 @@
 package handlers
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/api"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/pool"
+	grpcorchestrator "github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator"
+	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
 
 func (a *APIStore) V1CreateSandbox(c *gin.Context) {
-	/*
-		ctx := c.Request.Context()
+	ctx := c.Request.Context()
 
-		body, err := parseBody[api.V1CreateSandboxJSONRequestBody](ctx, c)
-		if err != nil {
-			a.sendAPIStoreError(c, http.StatusBadRequest, fmt.Sprintf("Error when parsing request: %s", err))
-			errMsg := fmt.Errorf("error when parsing request: %w", err)
-			telemetry.ReportCriticalError(ctx, errMsg)
+	body, err := parseBody[api.V1CreateSandboxJSONRequestBody](ctx, c)
+	if err != nil {
+		a.sendAPIStoreError(c, http.StatusBadRequest, fmt.Sprintf("Error when parsing request: %s", err))
+		errMsg := fmt.Errorf("error when parsing request: %w", err)
+		telemetry.ReportCriticalError(ctx, errMsg)
+		return
+	}
+
+	orchestrator, err := a.orchestratorPool.GetOrchestrator(body.Sandbox.OrchestratorId)
+	if err != nil {
+		if errors.Is(err, pool.ErrOrchestratorNotFound) {
+			a.sendAPIStoreError(c, http.StatusBadRequest, fmt.Sprintf("Orchestrator not found: %s", err))
+			telemetry.ReportCriticalError(ctx, fmt.Errorf("orchestrator not found: %w", err))
 			return
 		}
 
-		// todo
-		teamId := uuid.NewString()
-		alias := "base"
-		autoPause := false
+		a.sendAPIStoreError(c, http.StatusInternalServerError, fmt.Sprintf("Error when getting orchestrator: %s", err))
+		telemetry.ReportCriticalError(ctx, fmt.Errorf("error when getting orchestrator: %w", err))
+		return
+	}
 
-		startTime := time.Now()
-		endTime := startTime.Add(2 * time.Minute)
+	if orchestrator.Status != pool.OrchestratorStatusHealthy {
+		a.sendAPIStoreError(c, http.StatusBadRequest, "Orchestrator is not ready for sandbox placement")
+		telemetry.ReportCriticalError(ctx, fmt.Errorf("orchestrator is not ready for sandbox placement: %w", err))
+		return
+	}
 
-		sbxRequest := &orchestrator.SandboxCreateRequest{
-			Sandbox: &orchestrator.SandboxConfig{
-				BaseTemplateId:     body.Sandbox.TemplateId, // todo??
-				TemplateId:         body.Sandbox.TemplateId,
-				BuildId:            body.Sandbox.BuildId,
-				SandboxId:          body.Sandbox.SandboxId,
-				Alias:              &alias,
-				TeamId:             teamId,
-				KernelVersion:      body.Sandbox.KernelVersion,
-				FirecrackerVersion: body.Sandbox.FirecrackerVersion,
-				EnvdVersion:        body.Sandbox.EnvdVersion,
-				Metadata:           make(map[string]string),
-				EnvVars:            make(map[string]string),
-				EnvdAccessToken:    nil,
-				MaxSandboxLength:   24 * 60 * 60,
-				HugePages:          body.Sandbox.HugePages,
-				RamMb:              1024 * 1024,
-				Vcpu:               4,
-				Snapshot:           false,
-				AutoPause:          &autoPause,
-			},
-			StartTime: timestamppb.New(startTime),
-			EndTime:   timestamppb.New(endTime),
-		}
+	sbxMetadata := make(map[string]string)
+	if body.Sandbox.Metadata != nil {
+		sbxMetadata = *body.Sandbox.Metadata
+	}
 
-		o, err := a.orchestratorsPool.GetNode(body.Sandbox.OrchestratorId)
-		if err != nil {
-			if errors.Is(err, orchestratorPool.ErrOrchestratorNotFound) {
-				a.sendAPIStoreError(c, http.StatusBadRequest, fmt.Sprintf("Orchestrator not found: %s", err))
-				telemetry.ReportCriticalError(ctx, fmt.Errorf("orchestrator not found: %w", err))
-				return
-			}
+	sbxEnvVars := make(map[string]string)
+	if body.Sandbox.EnvVars != nil {
+		sbxEnvVars = *body.Sandbox.EnvVars
+	}
 
-			a.sendAPIStoreError(c, http.StatusInternalServerError, fmt.Sprintf("Error when getting orchestrator: %s", err))
-			telemetry.ReportCriticalError(ctx, fmt.Errorf("error when getting orchestrator: %w", err))
-			return
-		}
+	sbxBaseTemplateId := body.Sandbox.TemplateId
+	if body.Sandbox.BaseTemplateId != nil {
+		sbxBaseTemplateId = *body.Sandbox.BaseTemplateId
+	}
 
-		_, err = o.Client.Sandbox.Create(ctx, sbxRequest)
-		if err != nil {
-			a.sendAPIStoreError(c, http.StatusInternalServerError, fmt.Sprintf("Error when creating sandbox: %s", err))
-			errMsg := fmt.Errorf("error when creating sandbox: %w", err)
-			telemetry.ReportCriticalError(ctx, errMsg)
-			return
-		}
+	sbxRequest := &grpcorchestrator.SandboxCreateRequest{
+		Sandbox: &grpcorchestrator.SandboxConfig{
+			BaseTemplateId:     sbxBaseTemplateId,
+			TemplateId:         body.Sandbox.TemplateId,
+			BuildId:            body.Sandbox.BuildId,
+			SandboxId:          body.Sandbox.SandboxId,
+			Alias:              body.Sandbox.Alias,
+			TeamId:             body.Sandbox.TeamId,
+			KernelVersion:      body.Sandbox.KernelVersion,
+			FirecrackerVersion: body.Sandbox.FirecrackerVersion,
+			EnvdVersion:        body.Sandbox.EnvdVersion,
+			EnvdAccessToken:    body.Sandbox.EnvdAccessToken,
+			Metadata:           sbxMetadata,
+			EnvVars:            sbxEnvVars,
+			MaxSandboxLength:   24 * 60 * 60,
+			HugePages:          body.Sandbox.HugePages,
+			RamMb:              body.Sandbox.RamMB,
+			Vcpu:               body.Sandbox.VCPU,
+			Snapshot:           body.Sandbox.Snapshot,
+			AutoPause:          body.Sandbox.AutoPause,
+		},
+		StartTime: timestamppb.New(body.StartTime.UTC()),
+		EndTime:   timestamppb.New(body.EndTime.UTC()),
+	}
 
-		c.Status(http.StatusCreated)
+	sbxResponse, err := orchestrator.Client.Sandbox.Create(ctx, sbxRequest)
+	if err != nil {
+		zap.L().Error("Error when creating sandbox", zap.Error(err))
+		a.sendAPIStoreError(c, http.StatusInternalServerError, fmt.Sprintf("Error when creating sandbox: %s", err))
+		errMsg := fmt.Errorf("error when creating sandbox: %w", err)
+		telemetry.ReportCriticalError(ctx, errMsg)
+		return
+	}
 
-	*/
+	zap.L().Info("Created sandbox", zap.String("sandbox_id", body.Sandbox.SandboxId), zap.String("client_id", sbxResponse.ClientId))
+
+	c.JSON(
+		http.StatusOK,
+		api.SandboxCreateResponse{ClientId: sbxResponse.ClientId},
+	)
 }
