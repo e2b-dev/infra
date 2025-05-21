fedfbeec0cb2b72ccac4188a57b99d1c49425f15
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-21 10:28:49 +0200

    use new local story only for non production envs

--- packages/orchestrator/internal/sandbox/network/storage.go
+++ packages/orchestrator/internal/sandbox/network/storage.go
@@ -1,6 +1,10 @@
 package network
 
-import "go.opentelemetry.io/otel/trace"
+import (
+	"go.opentelemetry.io/otel/trace"
+
+	"github.com/e2b-dev/infra/packages/shared/pkg/env"
+)
 
 type Storage interface {
 	Acquire() (*Slot, error)
@@ -9,5 +13,9 @@ type Storage interface {
 
 // NewStorage creates a new slot storage based on the environment, we are ok with using a memory storage for local
 func NewStorage(slotsSize int, clientID string, tracer trace.Tracer) (Storage, error) {
-	return NewStorageLocal(slotsSize, tracer)
+	if env.IsDevelopment() {
+		return NewStorageLocal(slotsSize, tracer)
+	}
+
+	return NewStorageKV(slotsSize, clientID)
 }
