ff681509da95d2636501794a0ee78e01e211a4a3
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-26 17:58:02 +0200

    edge api pooling

---
+++ packages/client-proxy/internal/edge/http.go
@@ -0,0 +1,77 @@
+package edge
+
+import (
+	"context"
+	"fmt"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/getkin/kin-openapi/openapi3"
+	"github.com/getkin/kin-openapi/openapi3filter"
+	limits "github.com/gin-contrib/size"
+	"github.com/gin-gonic/gin"
+	middleware "github.com/oapi-codegen/gin-middleware"
+	"go.uber.org/zap"
+
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/api"
+	"github.com/e2b-dev/infra/packages/proxy/internal/edge/handlers"
+)
+
+const (
+	maxMultipartMemory = 1 << 27 // 128 MiB
+	maxUploadLimit     = 1 << 28 // 256 MiB
+
+	maxReadHeaderTimeout = 60 * time.Second
+	maxReadTimeout       = 75 * time.Second
+	maxWriteTimeout      = 75 * time.Second
+)
+
+func NewGinServer(ctx context.Context, logger *zap.Logger, store *handlers.APIStore, port int, swagger *openapi3.T) *http.Server {
+	// Clear out the servers array in the swagger spec, that skips validating
+	// that server names match. We don't know how this thing will be run.
+	swagger.Servers = nil
+
+	gin.SetMode(gin.ReleaseMode)
+
+	handler := gin.New()
+
+	// todo: setup CORS, middlewares for logging and auth
+	//handler.Use(cors.New(cors.DefaultConfig()))
+
+	// Use our validation middleware to check all requests against the
+	// OpenAPI schema.
+	handler.Use(
+		gin.Recovery(),
+		limits.RequestSizeLimiter(maxUploadLimit),
+		middleware.OapiRequestValidatorWithOptions(
+			swagger,
+			&middleware.Options{
+				//ErrorHandler:      utils.ErrorHandler,
+				//MultiErrorHandler: utils.MultiErrorHandler,
+				Options: openapi3filter.Options{
+					MultiError: true,
+					AuthenticationFunc: func(ctx context.Context, input *openapi3filter.AuthenticationInput) error {
+						// todo: implement authentication
+						return nil
+					},
+				},
+			},
+		),
+	)
+
+	handler.MaxMultipartMemory = maxMultipartMemory
+
+	api.RegisterHandlersWithOptions(handler, store, api.GinServerOptions{})
+
+	s := &http.Server{
+		Handler:           handler,
+		Addr:              fmt.Sprintf("0.0.0.0:%d", port),
+		ReadHeaderTimeout: maxReadHeaderTimeout,
+		ReadTimeout:       maxReadTimeout,
+		WriteTimeout:      maxWriteTimeout,
+		BaseContext:       func(net.Listener) context.Context { return ctx },
+	}
+
+	return s
+}
---
+++ packages/client-proxy/internal/edge/pool/orchestrator-pool.go
@@ -0,0 +1,207 @@
+package pool
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
+
+	sd "github.com/e2b-dev/infra/packages/proxy/internal/service-discovery"
+	"github.com/e2b-dev/infra/packages/shared/pkg/smap"
+)
+
+type OrchestratorsPool struct {
+	discovery sd.ServiceDiscoveryAdapter
+
+	nodes *smap.Map[*OrchestratorNode]
+	mutex sync.Mutex
+
+	tracer trace.Tracer
+	logger *zap.Logger
+}
+
+const (
+	orchestratorsCacheRefreshInterval = 10 * time.Second
+)
+
+var (
+	ErrOrchestratorNotFound = errors.New("orchestrator not found")
+)
+
+func NewOrchestratorsPool(ctx context.Context, logger *zap.Logger, discovery sd.ServiceDiscoveryAdapter, tracer trace.Tracer) *OrchestratorsPool {
+	pool := &OrchestratorsPool{
+		discovery: discovery,
+
+		nodes: smap.New[*OrchestratorNode](),
+		mutex: sync.Mutex{},
+
+		logger: logger,
+		tracer: tracer,
+	}
+
+	// Background synchronization of orchestrators available in pool
+	go func() { pool.keepInSync(ctx) }()
+
+	return pool
+}
+
+func (p *OrchestratorsPool) GetOrchestrators() map[string]*OrchestratorNode {
+	return p.nodes.Items()
+}
+
+func (p *OrchestratorsPool) GetOrchestrator(id string) (*OrchestratorNode, error) {
+	o, ok := p.nodes.Get(id)
+	if !ok {
+		return nil, ErrOrchestratorNotFound
+	}
+
+	return o, nil
+}
+
+func (p *OrchestratorsPool) keepInSync(ctx context.Context) {
+	// Run the first sync immediately
+	p.syncNodes(ctx)
+
+	ticker := time.NewTicker(orchestratorsCacheRefreshInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			p.logger.Info("Stopping keepInSync")
+			return
+		case <-ticker.C:
+			p.syncNodes(ctx)
+		}
+	}
+}
+
+func (p *OrchestratorsPool) syncNodes(ctx context.Context) {
+	ctxTimeout, cancel := context.WithTimeout(ctx, orchestratorsCacheRefreshInterval)
+	defer cancel()
+
+	spanCtx, span := p.tracer.Start(ctxTimeout, "pool-keep-in-sync")
+	defer span.End()
+
+	// Service discovery targets
+	sdNodes, err := p.discovery.ListNodes(spanCtx)
+	if err != nil {
+		return
+	}
+
+	var wg sync.WaitGroup
+
+	// connect / refresh discovered orchestrators
+	for _, sdNode := range sdNodes {
+		wg.Add(1)
+		go func(sdNode *sd.ServiceDiscoveryItem) {
+			defer wg.Done()
+
+			var found *OrchestratorNode = nil
+
+			host := fmt.Sprintf("%s:%d", sdNode.NodeIp, sdNode.NodePort)
+			for _, node := range p.nodes.Items() {
+				if host == node.Host {
+					found = node
+					break
+				}
+			}
+
+			if found == nil {
+				// newly discovered orchestrator
+				err := p.connectNode(ctx, sdNode)
+				if err != nil {
+					p.logger.Error("Error connecting to node", zap.Error(err))
+				}
+
+				return
+			}
+
+			// already discovered orchestrator, just sync status etc
+			err := p.syncNode(spanCtx, found, true)
+			if err != nil {
+				p.logger.Error("Error syncing orchestrator node", zap.Error(err))
+			}
+		}(sdNode)
+	}
+
+	// wait for all connections to finish
+	wg.Wait()
+
+	// disconnect nodes that are not in the list anymore
+	for _, node := range p.nodes.Items() {
+		wg.Add(1)
+		go func(node *OrchestratorNode) {
+			defer wg.Done()
+
+			found := false
+
+			for _, sdNode := range sdNodes {
+				host := fmt.Sprintf("%s:%d", sdNode.NodeIp, sdNode.NodePort)
+				if host == node.Host {
+					found = true
+					break
+				}
+			}
+
+			// orchestrator is no longer in the list coming from service discovery
+			if !found {
+				err := p.syncNode(spanCtx, node, false)
+				if err != nil {
+					p.logger.Error("Error during node sync", zap.Error(err))
+				}
+			}
+		}(node)
+
+	}
+
+	// wait for all node removals to finish
+	wg.Wait()
+}
+
+func (p *OrchestratorsPool) connectNode(ctx context.Context, node *sd.ServiceDiscoveryItem) error {
+	ctx, childSpan := p.tracer.Start(ctx, "connect-orchestrator-node")
+	defer childSpan.End()
+
+	o, err := NewOrchestrator(ctx, node.NodeIp, node.NodePort)
+	if err != nil {
+		return err
+	}
+
+	p.nodes.Insert(o.ServiceId, o)
+
+	// call initial node sync
+	return p.syncNode(ctx, o, true)
+}
+
+func (p *OrchestratorsPool) syncNode(ctx context.Context, node *OrchestratorNode, foundWithDiscovery bool) error {
+	ctx, childSpan := p.tracer.Start(ctx, "sync-orchestrator-node")
+	defer childSpan.End()
+
+	// close connection with node
+	if !foundWithDiscovery {
+		p.logger.Info("Orchestrator node node connection is not active anymore, closing.", zap.String("node_id", node.ServiceId))
+
+		err := node.Close()
+		if err != nil {
+			p.logger.Error("Error closing connection to node", zap.Error(err))
+		}
+
+		// stop background sync and close everything
+		err = node.Kill()
+		if err != nil {
+			p.logger.Error("Error closing connection to node", zap.Error(err))
+		}
+
+		p.nodes.Remove(node.ServiceId) // remove from pool
+
+		p.logger.Info("Orchestrator node node connection has been closed.", zap.String("node_id", node.ServiceId))
+		return nil
+	}
+
+	return nil
+}
