481a2affffbc72d04447279bb05ebd92b03b518a
Jakub Dobry <jakub.dobry8@gmail.com>
2025-05-26 11:50:24 +0000

    Get network slot asynchronously

--- packages/orchestrator/internal/sandbox/network/pool.go
+++ packages/orchestrator/internal/sandbox/network/pool.go
@@ -22,8 +22,8 @@ type Pool struct {
 	ctx    context.Context
 	cancel context.CancelFunc
 
-	newSlots          chan Slot
-	reusedSlots       chan Slot
+	newSlots          chan *Slot
+	reusedSlots       chan *Slot
 	newSlotCounter    metric.Int64UpDownCounter
 	reusedSlotCounter metric.Int64UpDownCounter
 
@@ -31,8 +31,8 @@ type Pool struct {
 }
 
 func NewPool(ctx context.Context, newSlotsPoolSize, reusedSlotsPoolSize int, clientID string, tracer trace.Tracer) (*Pool, error) {
-	newSlots := make(chan Slot, newSlotsPoolSize-1)
-	reusedSlots := make(chan Slot, reusedSlotsPoolSize)
+	newSlots := make(chan *Slot, newSlotsPoolSize-1)
+	reusedSlots := make(chan *Slot, reusedSlotsPoolSize)
 
 	newSlotCounter, err := meters.GetUpDownCounter(meters.NewNetworkSlotSPoolCounterMeterName)
 	if err != nil {
@@ -106,13 +106,13 @@ func (p *Pool) populate(ctx context.Context) error {
 			}
 
 			p.newSlotCounter.Add(ctx, 1)
-			p.newSlots <- *slot
+			p.newSlots <- slot
 		}
 	}
 }
 
-func (p *Pool) Get(ctx context.Context, tracer trace.Tracer, allowInternet bool) (Slot, error) {
-	var slot Slot
+func (p *Pool) Get(ctx context.Context, tracer trace.Tracer, allowInternet bool) (*Slot, error) {
+	var slot *Slot
 
 	select {
 	case s := <-p.reusedSlots:
@@ -123,7 +123,7 @@ func (p *Pool) Get(ctx context.Context, tracer trace.Tracer, allowInternet bool)
 	default:
 		select {
 		case <-ctx.Done():
-			return Slot{}, ctx.Err()
+			return nil, ctx.Err()
 		case s := <-p.newSlots:
 			p.newSlotCounter.Add(ctx, -1)
 			telemetry.ReportEvent(ctx, "new network slot")
@@ -134,13 +134,13 @@ func (p *Pool) Get(ctx context.Context, tracer trace.Tracer, allowInternet bool)
 
 	err := slot.ConfigureInternet(ctx, tracer, allowInternet)
 	if err != nil {
-		return Slot{}, fmt.Errorf("error setting slot internet access: %w", err)
+		return nil, fmt.Errorf("error setting slot internet access: %w", err)
 	}
 
 	return slot, nil
 }
 
-func (p *Pool) Return(ctx context.Context, tracer trace.Tracer, slot Slot) error {
+func (p *Pool) Return(ctx context.Context, tracer trace.Tracer, slot *Slot) error {
 	err := slot.ResetInternet(ctx, tracer)
 	if err != nil {
 		// Cleanup the slot if resetting internet fails
@@ -164,7 +164,7 @@ func (p *Pool) Return(ctx context.Context, tracer trace.Tracer, slot Slot) error
 	return nil
 }
 
-func (p *Pool) cleanup(slot Slot) error {
+func (p *Pool) cleanup(slot *Slot) error {
 	var errs []error
 
 	err := slot.RemoveNetwork()
@@ -172,7 +172,7 @@ func (p *Pool) cleanup(slot Slot) error {
 		errs = append(errs, fmt.Errorf("cannot remove network when releasing slot '%d': %w", slot.Idx, err))
 	}
 
-	err = p.slotStorage.Release(&slot)
+	err = p.slotStorage.Release(slot)
 	if err != nil {
 		errs = append(errs, fmt.Errorf("failed to release slot '%d': %w", slot.Idx, err))
 	}
