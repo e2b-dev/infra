ff9f9ed485da28e90a847574cc232c8afad278d6
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-05-28 18:40:12 +0200

    fixed template delete service call

--- packages/orchestrator/internal/template/server/delete_template.go
+++ packages/orchestrator/internal/template/server/delete_template.go
@@ -2,21 +2,26 @@ package server
 
 import (
 	"context"
+	"errors"
 
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/template"
-	template_manager "github.com/e2b-dev/infra/packages/shared/pkg/grpc/template-manager"
+	templatemanager "github.com/e2b-dev/infra/packages/shared/pkg/grpc/template-manager"
 )
 
-func (s *ServerStore) TemplateBuildDelete(ctx context.Context, in *template_manager.TemplateBuildDeleteRequest) (*emptypb.Empty, error) {
+func (s *ServerStore) TemplateBuildDelete(ctx context.Context, in *templatemanager.TemplateBuildDeleteRequest) (*emptypb.Empty, error) {
 	childCtx, childSpan := s.tracer.Start(ctx, "template-delete-request")
 	defer childSpan.End()
 
 	s.wg.Add(1)
 	defer s.wg.Done()
 
-	err := template.Delete(childCtx, s.tracer, s.artifactRegistry, s.templateStorage, in.BuildID)
+	if in.TemplateID == "" || in.BuildID == "" {
+		return nil, errors.New("template id and build id are required fields")
+	}
+
+	err := template.Delete(childCtx, s.tracer, s.artefactsRegistry, s.templateStorage, in.TemplateID, in.BuildID)
 	if err != nil {
 		return nil, err
 	}
