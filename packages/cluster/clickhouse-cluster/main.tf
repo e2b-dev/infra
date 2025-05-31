6c1500c84da5b99a19835c082c54c9600c3df349
Jakub Novak <jakub@e2b.dev>
2025-05-31 07:11:16 +0200

    Increase minimal action to REPLACE to prevent stale data

--- packages/cluster/clickhouse-cluster/main.tf
+++ packages/cluster/clickhouse-cluster/main.tf
@@ -37,7 +37,7 @@ resource "google_compute_instance_group_manager" "cluster" {
   # a rolling update.
   update_policy {
     type                  = var.environment == "dev" ? "PROACTIVE" : "OPPORTUNISTIC"
-    minimal_action        = "RESTART"
+    minimal_action        = "REPLACE"
     max_unavailable_fixed = 1
     replacement_method    = "RECREATE"
   }
