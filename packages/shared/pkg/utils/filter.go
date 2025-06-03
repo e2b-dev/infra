8b8f83bd8ca4fdabc2e5e6773602a76a357d2331
Jakub Dobry <jakub.dobry8@gmail.com>
2025-06-03 15:59:04 +0200

    add utils comments

--- packages/shared/pkg/utils/filter.go
+++ packages/shared/pkg/utils/filter.go
@@ -1,5 +1,8 @@
 package utils
 
+// Filter takes a slice of any type T and a predicate function f.
+// It returns a new slice containing only the elements from the input slice
+// for which the predicate function returns true.
 func Filter[T any](input []T, f func(T) bool) []T {
 	var output []T
 	for _, v := range input {
--- packages/shared/pkg/utils/map_values.go
+++ packages/shared/pkg/utils/map_values.go
@@ -1,5 +1,6 @@
 package utils
 
+// MapValues takes a map and returns a slice of all its values.
 func MapValues[K comparable, V any](m map[K]V) []V {
 	values := make([]V, 0, len(m))
 	for _, v := range m {
