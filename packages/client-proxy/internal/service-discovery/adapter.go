34a6c3cef0efcbff63cec662db0a6e8e84db4ead
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-11 13:40:47 -0700

    nits

--- packages/client-proxy/internal/service-discovery/adapter.go
+++ packages/client-proxy/internal/service-discovery/adapter.go
@@ -2,17 +2,12 @@ package service_discovery
 
 import (
 	"context"
-	"errors"
 )
 
 type ServiceDiscoveryItem struct {
 	NodeIp   string `json:"node_ip"`
 	NodePort int    `json:"node_port"`
 }
-
-var (
-	NodeNotFoundErr = errors.New("node not found")
-)
 
 type ServiceDiscoveryAdapter interface {
 	ListNodes(ctx context.Context) ([]*ServiceDiscoveryItem, error)
