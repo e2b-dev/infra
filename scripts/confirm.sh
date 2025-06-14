ad7537f08cffb48be115dd8c34b36bd962987513
Jakub Novak <jakub@e2b.dev>
2025-06-14 10:19:36 -0700

    Require confirmation on non dev environments

--- scripts/confirm.sh
+++ scripts/confirm.sh
@@ -4,7 +4,7 @@ ENV=$1
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 # Check if the ENV variable is set to "prod"
-if [[ "$ENV" == prod* ]]; then
+if [[ "$ENV" != "dev" ]]; then
   # Check if the current branch is "main"
   if [ "$BRANCH" != "main" ]; then
     echo "You are trying to deploy to $ENV from $BRANCH"
