ac9720c48e71235e0a293077630be3d7426f0ec7
Jakub Dobry <jakub.dobry8@gmail.com>
2025-06-20 14:49:18 -0700

    nit improvements

--- packages/orchestrator/internal/template/build/builder/layer_files.go
+++ packages/orchestrator/internal/template/build/builder/layer_files.go
@@ -3,5 +3,5 @@ package builder
 import "fmt"
 
 func GetLayerFilesCachePath(templateID string, hash string) string {
-	return fmt.Sprintf("builder/cache/%s/%s.tar", templateID, hash)
+	return fmt.Sprintf("builder/%s/files/%s.tar", templateID, hash)
 }
