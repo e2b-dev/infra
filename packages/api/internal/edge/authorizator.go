35bed12773cb0cfc8938b41c731cae8b937c888e
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-20 17:24:13 -0700

    wip: cluster with nodes impl

---
+++ packages/api/internal/edge/authorizator.go
@@ -0,0 +1,16 @@
+package edge
+
+import "context"
+
+type clientAuthorization struct {
+	secret string
+	tls    bool
+}
+
+func (a clientAuthorization) GetRequestMetadata(_ context.Context, _ ...string) (map[string]string, error) {
+	return map[string]string{edgeRpcAuthHeader: a.secret}, nil
+}
+
+func (a clientAuthorization) RequireTransportSecurity() bool {
+	return a.tls
+}
