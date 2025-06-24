b74f98200e600672ce8de85b8509f41edb266399
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-24 12:35:52 -0700

    take request headers first before message
    implement commit merged in original library
    (https://github.com/siderolabs/grpc-proxy/commit/80677e04c18d908cacb69566ed95c78c400d4d99#diff-b6ff38b985869ddfd17fe1d0fab88de9fc14b4d6b764f74b272ae05e2c798686)
    that is fixing issue where some clients will send headers first without
    message

--- packages/client-proxy/internal/edge-pass-through/proxy.go
+++ packages/client-proxy/internal/edge-pass-through/proxy.go
@@ -167,25 +167,24 @@ func (s *NodePassThrough) forwardClientToServer(src grpc.ClientStream, dst grpc.
 
 	go func() {
 		f := &emptypb.Empty{}
-		for i := 0; ; i++ {
+
+		md, err := src.Header()
+		if err != nil {
+			ret <- err
+			return
+		}
+
+		if err := dst.SendHeader(md); err != nil {
+			ret <- err
+			return
+		}
+
+		for {
 			if err := src.RecvMsg(f); err != nil {
 				ret <- err // this can be io.EOF which is happy case
 				break
 			}
-			if i == 0 {
-				// This is a bit of a hack, but client to server headers are only readable after first client msg is
-				// received but must be written to server stream before the first msg is flushed.
-				// This is the only place to do it nicely.
-				md, err := src.Header()
-				if err != nil {
-					ret <- err
-					break
-				}
-				if err := dst.SendHeader(md); err != nil {
-					ret <- err
-					break
-				}
-			}
+
 			if err := dst.SendMsg(f); err != nil {
 				ret <- err
 				break
@@ -201,7 +200,7 @@ func (s *NodePassThrough) forwardServerToClient(src grpc.ServerStream, dst grpc.
 
 	go func() {
 		f := &emptypb.Empty{}
-		for i := 0; ; i++ {
+		for {
 			if err := src.RecvMsg(f); err != nil {
 				ret <- err // this can be io.EOF which is happy case
 				break
