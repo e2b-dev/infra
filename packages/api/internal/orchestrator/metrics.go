228d781c574629714335ee9d0c30925d34c33b0b
Jakub Novak <jakub@e2b.dev>
2025-06-24 15:31:22 -0700

    Add metric per node

--- packages/api/internal/orchestrator/metrics.go
+++ packages/api/internal/orchestrator/metrics.go
@@ -7,25 +7,17 @@ import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/metric"
 
-	"github.com/e2b-dev/infra/packages/api/internal/api"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
 
 func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) error {
 	meter := meterProvider.Meter("api.orchestrator")
 	_, err := telemetry.GetObservableUpDownCounter(meter, telemetry.ApiOrchestratorCountMeterName, func(ctx context.Context, observer metric.Int64Observer) error {
-		nodeStatus := make(map[api.NodeStatus]int64)
-
-		for _, n := range o.nodes.Items() {
-			status := n.Status()
-			if _, ok := nodeStatus[status]; !ok {
-				nodeStatus[status] = 0
-			}
-			nodeStatus[status]++
-		}
-
-		for status, count := range nodeStatus {
-			observer.Observe(count, metric.WithAttributes(attribute.String("status", string(status))))
+		for _, node := range o.nodes.Items() {
+			observer.Observe(1, metric.WithAttributes(
+				attribute.String("status", string(node.status)),
+				attribute.String("node_id", node.orchestratorID),
+			))
 		}
 
 		return nil
