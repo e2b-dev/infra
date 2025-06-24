798becf435864d5e7ce714339d147ddcf8155b66
Jakub Novak <jakub@e2b.dev>
2025-06-23 21:55:09 -0700

    Fix variable

--- packages/orchestrator/internal/metrics/sandboxes.go
+++ packages/orchestrator/internal/metrics/sandboxes.go
@@ -133,6 +133,7 @@ func (so *SandboxObserver) startObserving() (metric.Registration, error) {
 					continue
 				}
 
+				sbx := sbx // Capture the sandbox in the loop variable to avoid data race
 				wg.Go(func() error {
 					// Make sure the sandbox doesn't change while we are getting metrics (the slot could be assigned to another sandbox)
 					sbxMetrics, err := sbx.Checks.GetMetrics(timeoutGetMetrics)
