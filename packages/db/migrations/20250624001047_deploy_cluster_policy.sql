2a3fc89cac3d67b8a1e8aa2c184aeee54b6d4af0
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-23 17:17:39 -0700

    added down migration

--- packages/db/migrations/20250624001047_deploy_cluster_policy.sql
+++ packages/db/migrations/20250624001047_deploy_cluster_policy.sql
@@ -5,4 +5,5 @@ ALTER TABLE "public"."clusters" ENABLE ROW LEVEL SECURITY;
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE "public"."clusters" DISABLE ROW LEVEL SECURITY;
 -- +goose StatementEnd
