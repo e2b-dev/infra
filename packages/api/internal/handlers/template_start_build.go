97045e654711e2116b59d36ca2e31d9f26fdf98d
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-24 17:24:55 -0700

    added explonation for template build start cluser node to assign err

--- packages/api/internal/handlers/template_start_build.go
+++ packages/api/internal/handlers/template_start_build.go
@@ -141,6 +141,7 @@ func (a *APIStore) PostTemplatesTemplateIDBuildsBuildID(c *gin.Context, template
 		return
 	}
 
+	// team is part of the cluster but template build is not assigned to a cluster node so its invalid stats
 	if team.ClusterID != nil && build.ClusterNodeID == nil {
 		a.sendAPIStoreError(c, http.StatusInternalServerError, "build is not assigned to a cluster node")
 		telemetry.ReportCriticalError(ctx, "build is not assigned to a cluster node", fmt.Errorf("build is not assigned to a cluster node"), telemetry.WithTemplateID(templateID))
