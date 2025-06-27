5a2a2d324777176958d608333abb7e754a061208
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-06-26 18:52:10 -0700

    cluster nodes sync is now using synchronization util

--- packages/api/internal/edge/cluster_nodes.go
+++ packages/api/internal/edge/cluster_nodes.go
@@ -9,14 +9,16 @@ import (
 	"sync"
 	"time"
 
+	"go.opentelemetry.io/otel/trace"
 	"go.uber.org/zap"
 	"google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/e2b-dev/infra/packages/api/internal/utils"
 	infogrpc "github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator-info"
+	api "github.com/e2b-dev/infra/packages/shared/pkg/http/edge"
 	l "github.com/e2b-dev/infra/packages/shared/pkg/logger"
-	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
+	"github.com/e2b-dev/infra/packages/shared/pkg/synchronization"
 )
 
 type ClusterNode struct {
@@ -29,112 +31,22 @@ type ClusterNode struct {
 	roles  []infogrpc.ServiceInfoRole
 	status infogrpc.ServiceInfoStatus
 	mutex  sync.RWMutex
+	tracer trace.Tracer
 }
 
 const (
 	clusterNodesSyncInterval = 15 * time.Second
+	clusterNodesSyncTimeout  = 15 * time.Second
 )
 
 func (c *Cluster) syncBackground() {
-	timer := time.NewTicker(clusterNodesSyncInterval)
-	defer timer.Stop()
-
-	for {
-		select {
-		case <-c.close:
-			zap.L().Info("Cluster nodes sync ended", l.WithClusterID(c.ID))
-			return
-		case <-timer.C:
-			syncTimeout, syncCancel := context.WithTimeout(context.Background(), clusterNodesSyncInterval)
-			err := c.sync(syncTimeout)
-			syncCancel()
-
-			if err != nil {
-				zap.L().Error("Failed to sync cluster nodes", zap.Error(err), l.WithClusterID(c.ID))
-			}
-		}
-	}
-}
-
-func (c *Cluster) sync(ctx context.Context) error {
-	spanCtx, span := c.tracer.Start(ctx, "keep-in-sync-cluster-nodes")
-	span.SetAttributes(telemetry.WithClusterID(c.ID))
-	defer span.End()
-
-	// fetch cluster nodes with use of service discovery
-	res, err := c.httpClient.V1ServiceDiscoveryGetOrchestratorsWithResponse(spanCtx)
-	if err != nil {
-		return fmt.Errorf("failed to get cluster nodes from service discovery: %w", err)
+	synchronize := synchronization.Synchronize[api.ClusterOrchestratorNode, string, *ClusterNode]{
+		Tracer:           c.tracer,
+		TracerSpanPrefix: "cluster-nodes",
+		Store:            clusterSynchronizationStore{cluster: c},
 	}
 
-	if res.StatusCode() != http.StatusOK {
-		return fmt.Errorf("failed to get builders from api: %s", res.Status())
-	}
-
-	if res.JSON200 == nil {
-		return errors.New("request to get builders returned nil response")
-	}
-
-	// register newly discovered nodes
-	_, spanNewlyDiscovered := c.tracer.Start(spanCtx, "cluster-nodes-sync-newly-discovered")
-	for _, sdNode := range *res.JSON200 {
-		clusterId := sdNode.Id
-		if _, ok := c.nodes.Get(clusterId); ok {
-			// node already exists in the pool, skip it
-			continue
-		}
-
-		zap.L().Info("Adding new node into cluster nodes pool", l.WithClusterID(c.ID), l.WithClusterNodeID(sdNode.Id))
-		node := &ClusterNode{
-			ID:     sdNode.Id,
-			NodeID: sdNode.NodeId,
-
-			// initial values before first sync
-			status: infogrpc.ServiceInfoStatus_OrchestratorUnhealthy,
-			roles:  make([]infogrpc.ServiceInfoRole, 0),
-
-			Version:       sdNode.Version,
-			VersionCommit: sdNode.Commit,
-
-			mutex: sync.RWMutex{},
-		}
-
-		c.nodes.Insert(sdNode.Id, node)
-	}
-	spanNewlyDiscovered.End()
-
-	// remove nodes that are no longer present in the service discovery
-	var wg sync.WaitGroup
-	_, spanOutdatedNodes := c.tracer.Start(spanCtx, "cluster-nodes-sync-outdated-nodes")
-	for nodeId, node := range c.nodes.Items() {
-		found := false
-		for _, sdNode := range *res.JSON200 {
-			if sdNode.Id == nodeId {
-				found = true
-				break
-			}
-		}
-
-		// synchronize node state
-		if found {
-			wg.Add(1)
-			go func() {
-				c.syncNode(ctx, node)
-				wg.Done()
-			}()
-
-			continue
-		}
-
-		zap.L().Info("Removing node from cluster nodes pool", l.WithClusterID(c.ID), l.WithClusterNodeID(nodeId))
-		c.nodes.Remove(nodeId)
-	}
-
-	// wait for all nodes to be synced
-	wg.Wait()
-	spanOutdatedNodes.End()
-
-	return nil
+	synchronize.SyncInBackground(c.close, clusterNodesSyncInterval, clusterNodesSyncTimeout, true)
 }
 
 func (c *Cluster) syncNode(ctx context.Context, node *ClusterNode) {
@@ -175,4 +87,71 @@ func (n *ClusterNode) IsBuilderNode() bool {
 
 func (n *ClusterNode) IsOrchestratorNode() bool {
 	return n.hasRole(infogrpc.ServiceInfoRole_Orchestrator)
+}
+
+// SynchronizationStore defines methods for synchronizing cluster nodes
+type clusterSynchronizationStore struct {
+	cluster *Cluster
+}
+
+func (d clusterSynchronizationStore) SourceList(ctx context.Context) ([]api.ClusterOrchestratorNode, error) {
+	// fetch cluster nodes with use of service discovery
+	res, err := d.cluster.httpClient.V1ServiceDiscoveryGetOrchestratorsWithResponse(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get cluster nodes from service discovery: %w", err)
+	}
+
+	if res.StatusCode() != http.StatusOK {
+		return nil, fmt.Errorf("failed to get builders from api: %s", res.Status())
+	}
+
+	if res.JSON200 == nil {
+		return nil, errors.New("request to get builders returned nil response")
+	}
+
+	return *res.JSON200, nil
+}
+
+func (d clusterSynchronizationStore) SourceKey(item api.ClusterOrchestratorNode) string {
+	return item.Id
+}
+
+func (d clusterSynchronizationStore) PoolList(ctx context.Context) map[string]*ClusterNode {
+	return d.cluster.nodes.Items()
+}
+
+func (d clusterSynchronizationStore) PoolExists(ctx context.Context, key string) bool {
+	_, found := d.cluster.nodes.Get(key)
+	return found
+}
+
+func (d clusterSynchronizationStore) PoolInsert(ctx context.Context, sdNodeID string, sdNode api.ClusterOrchestratorNode) error {
+	zap.L().Info("Adding new node into cluster nodes pool", l.WithClusterID(d.cluster.ID), l.WithClusterNodeID(sdNode.Id))
+	node := &ClusterNode{
+		ID:     sdNode.Id,
+		NodeID: sdNode.NodeId,
+
+		// initial values before first sync
+		status: infogrpc.ServiceInfoStatus_OrchestratorUnhealthy,
+		roles:  make([]infogrpc.ServiceInfoRole, 0),
+
+		Version:       sdNode.Version,
+		VersionCommit: sdNode.Commit,
+
+		tracer: d.cluster.tracer,
+		mutex:  sync.RWMutex{},
+	}
+
+	d.cluster.nodes.Insert(sdNode.Id, node)
+	return nil
+}
+
+func (d clusterSynchronizationStore) PoolSynchronize(ctx context.Context, nodeID string, node *ClusterNode) {
+	d.cluster.syncNode(ctx, node)
+}
+
+func (d clusterSynchronizationStore) PoolRemove(ctx context.Context, cluster *ClusterNode) error {
+	zap.L().Info("Removing node from cluster nodes pool", l.WithClusterID(d.cluster.ID), l.WithClusterNodeID(cluster.ID))
+	d.cluster.nodes.Remove(cluster.ID)
+	return nil
 }
