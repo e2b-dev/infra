56d1d412bb93e5b0dbff866704d140083080d001
Jakub Novak <jakub@e2b.dev>
2025-07-09 10:52:26 +0200

    Remove the transaction

--- packages/db/migrations/20250708135400_snapshots_migrations.sql
+++ packages/db/migrations/20250708135400_snapshots_migrations.sql
@@ -1,9 +1,6 @@
+-- +goose NO TRANSACTION
 -- +goose Up
--- +goose StatementBegin
 CREATE INDEX CONCURRENTLY idx_snapshots_sandbox_id ON public.snapshots (sandbox_id);
--- +goose StatementEnd
 
 -- +goose Down
--- +goose StatementBegin
 DROP INDEX CONCURRENTLY idx_snapshots_sandbox_id;
--- +goose StatementEnd
