67703fd71104d434c31f254ced4bddf57b8e25e9
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-07-10 12:04:09 +0200

    use e2b hex to ascii for client id

--- packages/shared/pkg/consts/sandboxes.go
+++ packages/shared/pkg/consts/sandboxes.go
@@ -7,6 +7,6 @@ const NodeIDLength = 8
 // ClientID Sandbox ID client part used during migration when we are still returning client but its no longer serving its purpose,
 // and we are returning it only for backward compatibility with SDK clients.
 // We don't want to use some obviously dummy value such as empty zeros, because for users it will look like something is wrong with the sandbox id
-const ClientID = "5a544a69"
+const ClientID = "6532622b"
 
 var OrchestratorPort = os.Getenv("ORCHESTRATOR_PORT")
