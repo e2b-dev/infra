8fe28cf04c40ca49ac89655951b4a7cfca282d80
0div <tgft73@gmail.com>
2025-07-14 18:24:03 -0700

    add migration file for sandbox events table

---
+++ packages/clickhouse/migrations/20250714223340_add_sandbox_events.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE sandbox_events (
+    timestamp DateTime64(9),
+    sandbox_uptime_secs UInt64,
+    sandbox_id UUID,
+    sandbox_execution_id UUID,
+    sandbox_template_id UUID,
+    sandbox_team_id String,
+    event_category String,
+    event_label String,
+    event_data JSON NULL
+) ENGINE = MergeTree ORDER BY (timestamp, sandbox_id, category, label);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS sandbox_events;
+-- +goose StatementEnd
