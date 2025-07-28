be906efcf549938871a179ddb7e5083e16d03222
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-07-28 12:39:42 -0400

    add metrics for sandbox creation
    this allows us to track:
    * count of sandboxes scheduled
    * distribution of attempts to schedule
    * resume vs create counts
    * success vs failed attempts to use node affinity

--- packages/api/internal/orchestrator/metrics.go
+++ packages/api/internal/orchestrator/metrics.go
@@ -10,11 +10,11 @@ import (
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
 
-func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) (metric.Registration, error) {
+func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) error {
 	meter := meterProvider.Meter("api.orchestrator")
 	gauge, err := telemetry.GetGaugeInt(meter, telemetry.ApiOrchestratorCountMeterName)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create orchestrators gauge: %w", err)
+		return fmt.Errorf("failed to create orchestrators gauge: %w", err)
 	}
 
 	_, err = telemetry.GetObservableCounter(meter, telemetry.ApiOrchestratorSbxCreateSuccess, func(ctx context.Context, observer metric.Int64Observer) error {
@@ -26,7 +26,7 @@ func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) (metric.
 		return nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create sandbox create success counter: %w", err)
+		return fmt.Errorf("failed to create sandbox create success counter: %w", err)
 	}
 
 	_, err = telemetry.GetObservableCounter(meter, telemetry.ApiOrchestratorSbxCreateFailure, func(ctx context.Context, observer metric.Int64Observer) error {
@@ -38,10 +38,10 @@ func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) (metric.
 		return nil
 	})
 	if err != nil {
-		return nil, fmt.Errorf("failed to create sandbox create failure counter: %w", err)
+		return fmt.Errorf("failed to create sandbox create failure counter: %w", err)
 	}
 
-	registration, err := meter.RegisterCallback(
+	o.metricsRegistration, err = meter.RegisterCallback(
 		func(ctx context.Context, obs metric.Observer) error {
 			for _, node := range o.nodes.Items() {
 				obs.ObserveInt64(gauge, 1, metric.WithAttributes(
@@ -53,8 +53,12 @@ func (o *Orchestrator) setupMetrics(meterProvider metric.MeterProvider) (metric.
 			return nil
 		}, gauge)
 	if err != nil {
-		return nil, fmt.Errorf("failed to register orchestrators gauge: %w", err)
+		return fmt.Errorf("failed to register orchestrators gauge: %w", err)
 	}
 
-	return registration, nil
+	if o.createdSandboxesCounter, err = meter.Int64Counter("api.orchestrator.created_sandboxes"); err != nil {
+		return fmt.Errorf("failed to create sandboxes counter: %w", err)
+	}
+
+	return nil
 }
