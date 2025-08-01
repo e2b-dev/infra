323ad0a744bd7297e2f914a7ed153be31c7364c1
Jakub Dobry <jakub.dobry8@gmail.com>
2025-08-01 10:24:11 +0200

    add comment with FromImage latest explanation

--- packages/orchestrator/internal/template/build/hash_index.go
+++ packages/orchestrator/internal/template/build/hash_index.go
@@ -94,6 +94,11 @@ func hashBase(template config.TemplateConfig) (string, error) {
 		// When building from template, use the base template metadata
 		baseSource = fmt.Sprintf("template:%s", template.FromTemplate.GetBuildID())
 	} else {
+		// Note: When "latest" tag is used, the cached version might become ambiguous (not always latest)
+		// To update it now, you need to force the rebuild of the template, which will update this layer for all templates
+		// in the team. This is okay for now, as the cache is not shared between teams, but it might need to be changed
+		// when global caches are implemented.
+
 		// When building from image, use the image name
 		baseSource = template.FromImage
 	}
