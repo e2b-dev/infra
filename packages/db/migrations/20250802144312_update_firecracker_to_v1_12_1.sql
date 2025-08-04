81ff07dea2e917ca8da3ac9d801faa66a600c1af
Jakub Dobry <jakub.dobry8@gmail.com>
2025-08-04 10:30:10 +0200

    drop default fc version in db

--- packages/db/migrations/20250802144312_update_firecracker_to_v1_12_1.sql
+++ packages/db/migrations/20250802144312_update_firecracker_to_v1_12_1.sql
@@ -1,17 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
-
--- Update firecracker version default in env_builds table
+-- Drop firecracker version default in env_builds table
 ALTER TABLE "public"."env_builds"
-ALTER COLUMN "firecracker_version" SET DEFAULT 'v1.12.1_d990331';
-
+ALTER COLUMN "firecracker_version" DROP DEFAULT;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-
--- Rollback firecracker version default in env_builds table
+-- Restore firecracker version default in env_builds table
 ALTER TABLE "public"."env_builds"
 ALTER COLUMN "firecracker_version" SET DEFAULT 'v1.10.1_1fcdaec';
-
 -- +goose StatementEnd
