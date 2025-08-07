a42d55ddb48919b708b4a248f7f5e24fdfd39982
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-08-07 01:25:34 -0700

    Add link directly to the file

--- packages/orchestrator/internal/sandbox/uffd/userfaultfd/constants.go
+++ packages/orchestrator/internal/sandbox/uffd/userfaultfd/constants.go
@@ -3,7 +3,7 @@ package userfaultfd
 // https://docs.kernel.org/admin-guide/mm/userfaultfd.html
 // https://man7.org/linux/man-pages/man2/userfaultfd.2.html
 // https://github.com/torvalds/linux/blob/master/fs/userfaultfd.c
-// https://github.com/loopholelabs/userfaultfd-go/tree/main/pkg/constants
+// https://github.com/loopholelabs/userfaultfd-go/blob/main/pkg/constants/cgo.go
 
 /*
 #include <sys/syscall.h>
