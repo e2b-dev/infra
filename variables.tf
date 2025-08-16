5a9a188461ff5663a2e2aff01479146624056a6f
Jakub Novak <jakub@e2b.dev>
2025-08-16 07:29:40 +0200

    Improve example

--- variables.tf
+++ variables.tf
@@ -293,15 +293,20 @@ variable "additional_domains" {
 
 variable "additional_api_services_json" {
   type        = string
-  description = "Additional path rules to add to the API path matcher. Format: json string of an array of objects with 'path' and 'service' keys. Example: '[{\"paths\": [\"/api/v1\"], \"service_id\": \"projects/e2b/global/backendServices/example\"}]'"
-  default     = ""
-  # type = list(object({
-  #   paths      = list(string)
-  #   service_id = string
-  #
-  #   api_node_group_port_name = string
-  #   api_node_group_port      = number
-  # }))
+  description = <<EOT
+Additional path rules to add to the API path matcher.
+Format: json string of an array of objects with 'path' and 'service' keys.
+Example:
+[
+  {
+    "paths": ["/api/v1"],
+    "service_id": "projects/e2b/global/backendServices/example",
+    "api_node_group_port_name": "example-port",
+    "api_node_group_port": 8080
+  }
+]
+EOT
+default     = ""
 }
 
 variable "prefix" {
