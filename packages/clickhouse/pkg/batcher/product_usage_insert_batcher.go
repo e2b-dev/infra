fe69d45b655a0d471391643973e69868bc71ecf2
0div <tgft73@gmail.com>
2025-08-25 16:21:57 -0700

    * refactor clickhouse insert batcher to be generic * implement product useage insert batcher

---
+++ packages/clickhouse/pkg/batcher/product_usage_insert_batcher.go
@@ -0,0 +1,106 @@
+package batcher
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
+
+	clickhouse "github.com/e2b-dev/infra/packages/clickhouse/pkg"
+)
+
+type ProductUsageInsertBatcher struct {
+	*Batcher[clickhouse.ProductUsage]
+	errorHandler func(error)
+	conn         driver.Conn
+}
+
+const InsertProductUsageQuery = `
+INSERT INTO product_usage
+(
+    timestamp,
+    team_id,
+    category,
+    action,
+    label
+)
+VALUES (
+    ?,
+    ?,
+    ?,
+    ?,
+    ?
+)`
+
+func NewProductUsageInsertsBatcher(conn driver.Conn, opts BatcherOptions) (*ProductUsageInsertBatcher, error) {
+	b := &ProductUsageInsertBatcher{
+		conn: conn,
+	}
+
+	batcher, err := NewBatcher(b.processInsertProductUsageBatch, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := batcher.Start(); err != nil {
+		return nil, err
+	}
+
+	b.Batcher = batcher
+
+	return b, nil
+}
+
+func (b *ProductUsageInsertBatcher) processInsertProductUsageBatch(events []clickhouse.ProductUsage) error {
+	ctx := context.Background()
+	batch, err := b.conn.PrepareBatch(
+		ctx, InsertProductUsageQuery, driver.WithReleaseConnection())
+	if err != nil {
+		return fmt.Errorf("error preparing batch: %w", err)
+	}
+
+	for _, event := range events {
+		err := batch.Append(
+			event.Timestamp,
+			event.TeamID,
+			event.Category,
+			event.Action,
+			event.Label,
+		)
+		if err != nil {
+			return fmt.Errorf("error appending product usage event to batch: %w", err)
+		}
+	}
+
+	err = batch.Send()
+	if err != nil {
+		return fmt.Errorf("error sending product usage events batch: %w", err)
+	}
+
+	return nil
+}
+
+func (b *ProductUsageInsertBatcher) Push(event clickhouse.ProductUsage) error {
+	success, err := b.Batcher.Push(event)
+	if err != nil {
+		return err
+	}
+	if !success {
+		return errors.New("batcher queue is full")
+	}
+	return nil
+}
+
+func (b *ProductUsageInsertBatcher) Close(ctx context.Context) error {
+	stopErr := b.Batcher.Stop()
+	closeErr := b.conn.Close()
+
+	if stopErr != nil {
+		return fmt.Errorf("error stopping batcher: %w", stopErr)
+	}
+	if closeErr != nil {
+		return fmt.Errorf("error closing connection: %w", closeErr)
+	}
+	return nil
+}
