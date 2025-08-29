ad14f8c54483bbc826a7a292326410338bec8f81
Joseph Lombrozo <joe@djeebus.net>
2025-08-29 22:25:01 +0000

    clean up implementation

--- packages/api/internal/utils/error.go
+++ packages/api/internal/utils/error.go
@@ -95,6 +95,8 @@ func MultiErrorHandler(me openapi3.MultiError) error {
 	}
 	err := me[0]
 
+	// Recreate logic from oapi-codegen/gin-middleware to handle the error
+	// Source: https://github.com/oapi-codegen/gin-middleware/blob/main/oapi_validate.go
 	switch e := err.(type) { // nolint:errorlint  // we copied this and don't want it to change
 	case *openapi3filter.RequestError:
 		// We've got a bad request
@@ -103,11 +105,7 @@ func MultiErrorHandler(me openapi3.MultiError) error {
 		errorLines := strings.Split(e.Error(), "\n")
 		return fmt.Errorf("error in openapi3filter.RequestError: %s", errorLines[0])
 	case *openapi3filter.SecurityRequirementsError:
-		var ok bool
-		if err, ok = processCustomErrors(e); ok {
-			return err
-		}
-		return fmt.Errorf("error in openapi3filter.SecurityRequirementsError: %s", err.Error())
+		return processCustomErrors(e) // custom implementation
 	default:
 		// This should never happen today, but if our upstream code changes,
 		// we don't want to crash the server, so handle the unexpected error.
@@ -115,7 +113,7 @@ func MultiErrorHandler(me openapi3.MultiError) error {
 	}
 }
 
-func processCustomErrors(e *openapi3filter.SecurityRequirementsError) (error, bool) {
+func processCustomErrors(e *openapi3filter.SecurityRequirementsError) error {
 	// Return only one security requirement error (there may be multiple securitySchemes)
 	unwrapped := e.Errors
 	err := unwrapped[0]
@@ -129,16 +127,16 @@ func processCustomErrors(e *openapi3filter.SecurityRequirementsError) (error, bo
 		}
 
 		if errors.As(errW, &teamForbidden) {
-			return fmt.Errorf("%s%s", forbiddenErrPrefix, err.Error()), true
+			return fmt.Errorf("%s%s", forbiddenErrPrefix, err.Error())
 		}
 
 		if errors.As(errW, &teamBlocked) {
-			return fmt.Errorf("%s%s", blockedErrPrefix, err.Error()), true
+			return fmt.Errorf("%s%s", blockedErrPrefix, err.Error())
 		}
 
 		err = errW
 		break
 	}
 
-	return err, false
+	return fmt.Errorf("%s%s", securityErrPrefix, err.Error())
 }
