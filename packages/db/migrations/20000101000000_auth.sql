344fd785cb4849ee7cbeb5e3bed9447f7ff2d7d1
Jakub Novak <jakub@e2b.dev>
2025-09-02 15:51:49 +0200

    Use directly goose in Makefile

--- packages/db/migrations/20000101000000_auth.sql
+++ packages/db/migrations/20000101000000_auth.sql
@@ -2,6 +2,17 @@
 -- +goose StatementBegin
 CREATE SCHEMA IF NOT EXISTS auth;
 
+CREATE ROLE authenticated;
+
+CREATE FUNCTION auth.uid() RETURNS uuid AS $func$
+BEGIN
+    RETURN gen_random_uuid();
+END;
+$func$ LANGUAGE plpgsql;
+
+-- Grant execute on auth.uid() to postgres role
+GRANT EXECUTE ON FUNCTION auth.uid() TO postgres;
+
 CREATE TABLE auth.users (
     id uuid NOT NULL DEFAULT gen_random_uuid(),
     email text NOT NULL,
