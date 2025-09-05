6c3b10190cc21370f0da52f57b2a4b35e5a585a1
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-09-05 15:46:14 +0200

    Migration for additional resources, makefile for tf

--- iac/provider-gcp/nomad-cluster/nodepool-client.tf
+++ iac/provider-gcp/nomad-cluster/nodepool-client.tf
@@ -1,5 +1,5 @@
 locals {
-  client_pool_name      = "${var.prefix}${var.client_cluster_name}"
+  client_pool_name = "${var.prefix}${var.client_cluster_name}"
   client_startup_script = templatefile("${path.module}/scripts/start-client.sh", {
     CLUSTER_TAG_NAME             = var.cluster_tag_name
     SCRIPTS_BUCKET               = var.cluster_setup_bucket_name
