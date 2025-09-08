15cb31f52f9cb64f4aa01d55a4af3a7f9ee2599d
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-09-08 15:43:37 +0200

    Make envd api spec file aligned, check sandbox clock drifts (#1136)

--- packages/envd/internal/host/metrics.go
+++ packages/envd/internal/host/metrics.go
@@ -16,11 +16,15 @@ type Metrics struct {
 	CPUUsedPercent float32 `json:"cpu_used_pct"` // Percent rounded to 2 decimal places
 
 	// Deprecated
+	// TODO: Remove when they are no longer used in orchestrator (https://linear.app/e2b/issue/E2B-2998/remove-envd-deprecated-metrics-when-not-used)
 	MemTotalMiB uint64 `json:"mem_total_mib"` // Total virtual memory in MiB
+
 	// Deprecated
+	// TODO: Remove when no longer used in orchestrator
 	MemUsedMiB uint64 `json:"mem_used_mib"` // Used virtual memory in MiB
-	MemTotal   uint64 `json:"mem_total"`    // Total virtual memory in bytes
-	MemUsed    uint64 `json:"mem_used"`     // Used virtual memory in bytes
+
+	MemTotal uint64 `json:"mem_total"` // Total virtual memory in bytes
+	MemUsed  uint64 `json:"mem_used"`  // Used virtual memory in bytes
 
 	DiskUsed  uint64 `json:"disk_used"`  // Used disk space in bytes
 	DiskTotal uint64 `json:"disk_total"` // Total disk space in bytes
