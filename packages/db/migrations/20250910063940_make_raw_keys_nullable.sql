5d28c77f254ea7f506e80fe82e08cabe96f97b12
Jakub Novak <jakub@e2b.dev>
2025-09-10 10:12:14 +0200

    Fill in values

--- packages/db/migrations/20250910063940_make_raw_keys_nullable.sql
+++ packages/db/migrations/20250910063940_make_raw_keys_nullable.sql
@@ -9,7 +9,9 @@ ALTER TABLE "public"."team_api_keys" ALTER COLUMN "api_key" DROP NOT NULL;
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE "public"."access_tokens" DROP CONSTRAINT access_tokens_pkey;
+UPDATE TABLE "public"."access_tokens" SET "access_token" = 'unknown' WHERE "access_token" IS NULL;
 ALTER TABLE "public"."access_tokens" ALTER COLUMN "access_token" SET NOT NULL;
 ALTER TABLE "public"."access_tokens" ADD PRIMARY KEY (access_token);
+UPDATE TABLE "public"."team_api_keys" SET "api_key" = 'unknown' WHERE "api_key" IS NULL;
 ALTER TABLE "public"."team_api_keys" ALTER COLUMN "api_key" SET NOT NULL;
 -- +goose StatementEnd
