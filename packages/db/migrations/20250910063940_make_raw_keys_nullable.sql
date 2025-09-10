1176b7f486638b782235faac315334b9bc386a8c
Jakub Novak <jakub@e2b.dev>
2025-09-10 09:51:26 +0200

    Change primary key

--- packages/db/migrations/20250910063940_make_raw_keys_nullable.sql
+++ packages/db/migrations/20250910063940_make_raw_keys_nullable.sql
@@ -1,11 +1,15 @@
 -- +goose Up
 -- +goose StatementBegin
+ALTER TABLE "public"."access_tokens" DROP CONSTRAINT access_tokens_pkey;
+ALTER TABLE "public"."access_tokens" ADD PRIMARY KEY (id);
 ALTER TABLE "public"."access_tokens" ALTER COLUMN "access_token" DROP NOT NULL;
 ALTER TABLE "public"."team_api_keys" ALTER COLUMN "api_key" DROP NOT NULL;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+ALTER TABLE "public"."access_tokens" DROP CONSTRAINT access_tokens_pkey;
 ALTER TABLE "public"."access_tokens" ALTER COLUMN "access_token" SET NOT NULL;
+ALTER TABLE "public"."access_tokens" ADD PRIMARY KEY (access_token);
 ALTER TABLE "public"."team_api_keys" ALTER COLUMN "api_key" SET NOT NULL;
 -- +goose StatementEnd
