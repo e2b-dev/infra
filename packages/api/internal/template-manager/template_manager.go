e07dd1fcd53d502f266f6d4607c798775851f063
Jakub Novák <jakub@e2b.dev>
2025-09-12 03:25:23 -0700

    Update packages/api/internal/template-manager/template_manager.go

--- packages/api/internal/template-manager/template_manager.go
+++ packages/api/internal/template-manager/template_manager.go
@@ -175,8 +175,8 @@ func (tm *TemplateManager) DeleteBuild(ctx context.Context, buildID uuid.UUID, t
 
 	client, err := tm.GetClusterBuildClient(clusterID, nodeID)
 	if err != nil {
-		// Note: the nodeID can be an orchestrator ID,
-		// we may want to improve this later by adding the Delete method to Orchestrator as well.
+		// nodeID can be an orchestrator ID, if the build is a snapshot.
+		// We may want to improve this later by adding the Delete method to Orchestrator as well.
 		// This way we can remove the build (snapshot) from cache as well
 		nodeID, err = tm.GetAvailableBuildClient(ctx, clusterID)
 		if err != nil {
