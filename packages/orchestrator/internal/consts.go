3d2690d5ee12bab831246ed9374b2b03a414e0c6
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-09-17 17:39:07 +0200

    changed hyperloop ip to 192.0.2.1 to make it look more like local routable for first look

--- packages/orchestrator/internal/consts.go
+++ packages/orchestrator/internal/consts.go
@@ -5,14 +5,14 @@ import (
 )
 
 const (
-	// Private IP don't leave the sandbox through the network bridge, so we use a reserved IP address for it:
-	// See TEST-NET-3 on https://en.wikipedia.org/wiki/Reserved_IP_addresses
-	defaultSandboxHyperloopIP = "203.0.113.0"
+	// Using reserver IPv4 in range that is used for experiments and documentation
+	// https://en.wikipedia.org/wiki/Reserved_IP_addresses
+	defaultHyperloopIP        = "192.0.2.1"
 	defaultHyperloopProxyPort = "5010"
 )
 
-func GetSandboxHyperloopIP() string {
-	return env.GetEnv("SANDBOX_HYPERLOOP_IP", defaultSandboxHyperloopIP)
+func GetHyperloopIP() string {
+	return env.GetEnv("SANDBOX_HYPERLOOP_IP", defaultHyperloopIP)
 }
 
 func GetHyperloopProxyPort() string {
