3b36b0952e03992b9497381731a98a4b8bd73c40
Jiri Sveceny <jiri.sveceny@icloud.com>
2025-09-22 12:35:40 +0200

    impl enpoint for forwarding logs to vector

--- packages/orchestrator/internal/hyperloopserver/handlers/me.go
+++ packages/orchestrator/internal/hyperloopserver/handlers/me.go
@@ -1,1 +1,21 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	api "github.com/e2b-dev/infra/packages/shared/pkg/http/hyperloop"
+	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
+)
+
+func (h *APIStore) Me(c *gin.Context) {
+	sbx, err := h.findSandbox(c)
+	if err != nil {
+		h.sendAPIStoreError(c, http.StatusBadRequest, "Error when finding source sandbox")
+		telemetry.ReportCriticalError(c, "error when parsing request", err)
+		return
+	}
+
+	c.JSON(http.StatusOK, &api.Me{SandboxID: sbx.Runtime.SandboxID})
+}
