ce004d4435eb5c6cfe01067a1796671f756b09a9
Jakub Novak <jakub@e2b.dev>
2025-09-23 22:19:08 +0200

    Set team_id for snapshots

--- packages/db/migrations/20250923103614_make_team_id_non_nullable.sql
+++ packages/db/migrations/20250923103614_make_team_id_non_nullable.sql
@@ -1,5 +1,6 @@
 -- +goose Up
 -- +goose StatementBegin
+UPDATE "public"."snapshots" SET team_id = e.team_id FROM "public"."envs" e WHERE e.id = snapshots.env_id;
 ALTER TABLE "public"."snapshots" ALTER COLUMN "team_id" SET NOT NULL;
 -- +goose StatementEnd
 
