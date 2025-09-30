e72bffea964138db40ea55b6a71963361ca994a5
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-09-30 14:58:50 -0700

    fix casing

--- packages/orchestrator/cmd/clean-nfs-cache/pkg/atime_osx.go
+++ packages/orchestrator/cmd/clean-nfs-cache/pkg/atime_osx.go
@@ -12,20 +12,20 @@ import (
 func GetFileMetadata(path string) (File, error) {
 	stat, err := os.Stat(path)
 	if err != nil {
-		return file{}, fmt.Errorf("could not stat info: %w", err)
+		return File{}, fmt.Errorf("could not stat info: %w", err)
 	}
 
 	actualStruct, ok := stat.Sys().(*syscall.Stat_t)
 	if !ok {
-		return file{}, fmt.Errorf("stat did not return a syscall.Stat_t for %q: %T",
+		return File{}, fmt.Errorf("stat did not return a syscall.Stat_t for %q: %T",
 			stat.Name(), stat.Sys())
 	}
 
-	return file{
-		path:  stat.Name(),
-		size:  stat.Size(),
-		atime: fromTimespec(actualStruct.Atimespec),
-		btime: fromTimespec(actualStruct.Birthtimespec),
+	return File{
+		Path:  stat.Name(),
+		Size:  stat.Size(),
+		ATime: fromTimespec(actualStruct.Atimespec),
+		BTime: fromTimespec(actualStruct.Birthtimespec),
 	}, nil
 }
 
