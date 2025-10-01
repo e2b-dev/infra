d3b1e0edca659f91b570fe705471ebdd104662df
Jakub Novak <jakub@e2b.dev>
2025-10-01 16:22:38 +0200

    Add duration

--- packages/orchestrator/internal/sandbox/envd.go
+++ packages/orchestrator/internal/sandbox/envd.go
@@ -49,7 +49,7 @@ func doRequestWithInfiniteRetries(ctx context.Context, method, address string, r
 			return response, requestCount, nil
 		}
 
-		zap.L().Warn("failed to do request to envd, retrying", logger.WithSandboxID(sandboxID), logger.WithEnvdVersion(envdVersion), zap.Error(err))
+		zap.L().Warn("failed to do request to envd, retrying", logger.WithSandboxID(sandboxID), logger.WithEnvdVersion(envdVersion), zap.Duration("timeout", envdInitRequestTimeout), zap.Error(err))
 
 		select {
 		case <-ctx.Done():
