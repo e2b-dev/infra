0a03731ec6dc37ea805a87408b16e29fadf1c9e7
Jakub Novak <jakub@e2b.dev>
2025-10-01 19:59:49 +0200

    Revert the logic

--- packages/orchestrator/cmd/clean-nfs-cache/main.go
+++ packages/orchestrator/cmd/clean-nfs-cache/main.go
@@ -302,8 +302,8 @@ func parseArgs() (string, opts, error) {
 	var opts opts
 	flags.Float64Var(&opts.targetDiskUsagePercent, "disk-usage-target-percent", 10, "disk usage target as a % (0-100)")
 	flags.BoolVar(&opts.dryRun, "dry-run", true, "dry run")
-	flags.IntVar(&opts.filesPerLoop, "files-per-iteration", 5000, "number of files to gather metadata for per loop")
-	flags.Int64Var(&opts.filesToDeletePerLoop, "max-files-per-iteration", 100, "maximum number of files to delete per loop")
+	flags.IntVar(&opts.filesPerLoop, "files-per-iteration", 10000, "number of files to gather metadata for per loop")
+	flags.Int64Var(&opts.filesToDeletePerLoop, "max-files-per-iteration", 200, "maximum number of files to delete per loop")
 
 	args := os.Args[1:] // skip the command name
 	if err := flags.Parse(args); err != nil {
