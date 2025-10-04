eeddba1e4d44964f7e9b4d88a5b98d4430c56386
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-10-03 18:42:37 -0700

    fix test, tidy

--- packages/client-proxy/internal/cfg/model_test.go
+++ packages/client-proxy/internal/cfg/model_test.go
@@ -8,6 +8,9 @@ import (
 )
 
 func TestParse(t *testing.T) {
+	t.Setenv("LOKI_URL", "loki-url")
+	t.Setenv("SD_ORCHESTRATOR_PROVIDER", "loki-user")
+
 	t.Run("create parse provider", func(t *testing.T) {
 		t.Setenv("SD_EDGE_PROVIDER", "sd-edge-provider")
 		t.Setenv("SD_EDGE_DNS_QUERY", "sd-edge-dns-query")
@@ -16,6 +19,6 @@ func TestParse(t *testing.T) {
 		require.NoError(t, err)
 
 		assert.Equal(t, "sd-edge-provider", config.EdgeServiceDiscovery.Provider)
-		assert.Equal(t, "sd-edge-dns-query", config.EdgeServiceDiscovery.DNSQuery)
+		assert.Equal(t, []string{"sd-edge-dns-query"}, config.EdgeServiceDiscovery.DNSQuery)
 	})
 }
