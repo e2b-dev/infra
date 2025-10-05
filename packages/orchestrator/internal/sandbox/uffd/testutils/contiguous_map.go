1ecc8100e15ba9f56b39d71f462573416ca28560
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-10-05 05:13:52 +0000

    Add access checks to tests

--- packages/orchestrator/internal/sandbox/uffd/testutils/contiguous_map.go
+++ packages/orchestrator/internal/sandbox/uffd/testutils/contiguous_map.go
@@ -1,11 +1,16 @@
 package testutils
 
+import "sync"
+
 // ContiguousMap is a mapping that is contiguous in the host virtual address space.
 // This is used for testing purposes.
 type ContiguousMap struct {
 	start    uintptr
 	size     uint64
 	pagesize uint64
+
+	accessedOffsets []uintptr
+	mu              sync.RWMutex
 }
 
 func NewContiguousMap(start uintptr, size, pagesize uint64) *ContiguousMap {
@@ -20,5 +25,19 @@ func (m *ContiguousMap) GetOffset(addr uintptr) (int64, uint64, error) {
 	offset := addr - m.start
 	pagesize := m.pagesize
 
+	m.mu.Lock()
+	m.accessedOffsets = append(m.accessedOffsets, offset)
+	m.mu.Unlock()
+
 	return int64(offset), pagesize, nil
+}
+
+func (m *ContiguousMap) Accessed() []uintptr {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	accessedOffsets := make([]uintptr, len(m.accessedOffsets))
+	copy(accessedOffsets, m.accessedOffsets)
+
+	return accessedOffsets
 }
