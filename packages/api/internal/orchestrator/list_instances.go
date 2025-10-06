43ffbcdd602df7691031d380468d667382661444
Jakub Novak <jakub@e2b.dev>
2025-10-06 13:12:39 +0200

    Fix team id

--- packages/api/internal/orchestrator/list_instances.go
+++ packages/api/internal/orchestrator/list_instances.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	_ "embed"
 
+	"github.com/google/uuid"
+
 	"github.com/e2b-dev/infra/packages/api/internal/sandbox"
 )
 
 // GetSandboxes returns all instances for a given node.
-func (o *Orchestrator) GetSandboxes(ctx context.Context, options ...sandbox.ItemsOption) []sandbox.Sandbox {
+func (o *Orchestrator) GetSandboxes(ctx context.Context, teamID uuid.UUID, options ...sandbox.ItemsOption) []sandbox.Sandbox {
 	_, childSpan := tracer.Start(ctx, "get-sandboxes")
 	defer childSpan.End()
 
-	return o.sandboxStore.Items(options...)
+	return o.sandboxStore.Items(&teamID, options...)
 }
