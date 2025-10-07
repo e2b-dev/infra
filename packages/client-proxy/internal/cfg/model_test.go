5a7f2f344187c34af60cf7d9a53ac2712d404c8a
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-10-07 14:16:59 -0700

    make cursorbot happy

--- packages/client-proxy/internal/cfg/model_test.go
+++ packages/client-proxy/internal/cfg/model_test.go
@@ -9,16 +9,16 @@ import (
 
 func TestParse(t *testing.T) {
 	t.Setenv("LOKI_URL", "loki-url")
-	t.Setenv("SD_ORCHESTRATOR_PROVIDER", "loki-user")
+	t.Setenv("SD_ORCHESTRATOR_PROVIDER", "STATIC")
 
 	t.Run("create parse provider", func(t *testing.T) {
-		t.Setenv("SD_EDGE_PROVIDER", "sd-edge-provider")
+		t.Setenv("SD_EDGE_PROVIDER", "STATIC")
 		t.Setenv("SD_EDGE_DNS_QUERY", "sd-edge-dns-query")
 
 		config, err := Parse()
 		require.NoError(t, err)
 
-		assert.Equal(t, "sd-edge-provider", config.EdgeServiceDiscovery.Provider)
+		assert.Equal(t, "STATIC", config.EdgeServiceDiscovery.Provider)
 		assert.Equal(t, []string{"sd-edge-dns-query"}, config.EdgeServiceDiscovery.DNSQuery)
 	})
 }
