908d0d53d309c980dc69d575ce666734d7e75454
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2025-10-06 23:35:43 -0700

    fix useless init

--- packages/api/main.go
+++ packages/api/main.go
@@ -39,7 +39,6 @@ import (
 	l "github.com/e2b-dev/infra/packages/shared/pkg/logger"
 	sbxlogger "github.com/e2b-dev/infra/packages/shared/pkg/logger/sandbox"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
-	"github.com/e2b-dev/infra/packages/shared/pkg/vault"
 )
 
 const (
@@ -266,11 +265,6 @@ func run() int {
 	)
 	defer sbxLoggerExternal.Sync()
 	sbxlogger.SetSandboxLoggerExternal(sbxLoggerExternal)
-
-	_, err = vault.NewClientFromEnv(ctx)
-	if err != nil {
-		zap.L().Fatal("failed to create secret vault client", zap.Error(err))
-	}
 
 	sbxLoggerInternal := sbxlogger.NewLogger(
 		ctx,
