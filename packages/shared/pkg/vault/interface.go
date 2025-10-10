e481639c1d24909f013cc96584828a789b58ae3d
Jonas Scholz <Jonas.Scholz@bbscholz.de>
2025-10-10 14:14:27 -0700

    add interface comment

--- packages/shared/pkg/vault/interface.go
+++ packages/shared/pkg/vault/interface.go
@@ -4,6 +4,7 @@ import (
 	"context"
 )
 
+// If you self-host E2B and don't want to use Hashicorp Vault, you can implement this interface to use your own vault backend
 type VaultBackend interface {
 	GetSecret(ctx context.Context, path string) (string, map[string]any, error)
 
