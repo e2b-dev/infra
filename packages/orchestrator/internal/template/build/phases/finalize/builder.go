a8d150d44809db43c1c36655410ea0159bf3c3f5
Jakub Dobry <jakub.dobry8@gmail.com>
2025-10-14 16:01:57 +0200

    feat: keep user and workdir from the template in the sandbox (#1349)

--- packages/orchestrator/internal/template/build/phases/finalize/builder.go
+++ packages/orchestrator/internal/template/build/phases/finalize/builder.go
@@ -24,6 +24,8 @@ import (
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/build/storage/cache"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/metadata"
 	"github.com/e2b-dev/infra/packages/shared/pkg/storage"
+	"github.com/e2b-dev/infra/packages/shared/pkg/templates"
+	"github.com/e2b-dev/infra/packages/shared/pkg/utils"
 )
 
 var finalizeTimeout = configurationTimeout + readyCommandTimeout + 5*time.Minute
@@ -108,6 +110,7 @@ func (ppb *PostProcessingBuilder) Layer(
 func (ppb *PostProcessingBuilder) Build(
 	ctx context.Context,
 	userLogger *zap.Logger,
+	_ string,
 	sourceLayer phases.LayerResult,
 	currentLayer phases.LayerResult,
 ) (phases.LayerResult, error) {
@@ -116,6 +119,20 @@ func (ppb *PostProcessingBuilder) Build(
 	))
 	defer span.End()
 
+	defaultUser := utils.ToPtr(currentLayer.Metadata.Context.User)
+	defaultWorkdir := currentLayer.Metadata.Context.WorkDir
+
+	ok, err := utils.IsGTEVersion(ppb.Version, templates.TemplateDefaultUserVersion)
+	if err != nil {
+		return phases.LayerResult{}, fmt.Errorf("error checking build version: %w", err)
+	}
+	if !ok {
+		// For older builds, always use "user" as the default user
+		// and do not set a default workdir (defaults to the user homedir).
+		defaultUser = utils.ToPtr("user")
+		defaultWorkdir = nil
+	}
+
 	// Configure sandbox for final layer
 	sbxConfig := sandbox.Config{
 		Vcpu:      ppb.Config.VCpuCount,
@@ -123,7 +140,9 @@ func (ppb *PostProcessingBuilder) Build(
 		HugePages: ppb.Config.HugePages,
 
 		Envd: sandbox.EnvdMetadata{
-			Version: ppb.EnvdVersion,
+			Version:        ppb.EnvdVersion,
+			DefaultUser:    defaultUser,
+			DefaultWorkdir: defaultWorkdir,
 		},
 	}
 
