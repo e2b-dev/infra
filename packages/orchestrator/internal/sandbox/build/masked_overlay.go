64be1177c494fd9b66a0ba88026c64bcea95d910
ValentaTomas <valenta.and.thomas@gmail.com>
2025-10-20 15:22:39 -0700

    [WIP] Update masked overlay

--- packages/orchestrator/internal/sandbox/build/masked_overlay.go
+++ packages/orchestrator/internal/sandbox/build/masked_overlay.go
@@ -23,6 +23,20 @@ func NewMaskedOverlay(lower, upper block.ReadonlyDevice) (*MaskedOverlay, error)
 		return nil, fmt.Errorf("lower and upper block sizes do not match")
 	}
 
+	lowerSize, err := lower.Size()
+	if err != nil {
+		return nil, err
+	}
+
+	upperSize, err := upper.Size()
+	if err != nil {
+		return nil, err
+	}
+
+	if lowerSize != upperSize {
+		return nil, fmt.Errorf("lower and upper sizes do not match")
+	}
+
 	upperMapping := slices.Collect(upper.Header().FilterMapping(upper.Header().Metadata.BuildId))
 	mask := block.NewTrackerFromMapping(upperMapping, upper.BlockSize())
 
@@ -45,24 +59,37 @@ func (m *MaskedOverlay) Header() *header.Header {
 	return m.lower.Header()
 }
 
-func (m *MaskedOverlay) ReadAt(ctx context.Context, p []byte, off int64) (int, error) {
-	// TODO: Implement the selective read at for this
+// TODO: We should be able to read by bigger chunks than just the block size
+func (m *MaskedOverlay) ReadAt(ctx context.Context, p []byte, off int64) (n int, err error) {
+	for n < len(p) {
+		currentOff := off + int64(n)
+		if m.mask.Has(currentOff) {
+			readN, err := m.upper.ReadAt(ctx, p[n:], currentOff)
+
+			n += readN
+
+			if err != nil {
+				return n, err
+			}
+		} else {
+			readN, err := m.lower.ReadAt(ctx, p[n:], currentOff)
+
+			n += readN
+
+			if err != nil {
+				return n, err
+			}
+		}
+	}
+
+	return n, nil
 }
 
 func (m *MaskedOverlay) Size() (int64, error) {
-	lowerSize, err := m.lower.Size()
-	if err != nil {
-		return 0, err
-	}
-
-	upperSize, err := m.upper.Size()
-	if err != nil {
-		return 0, err
-	}
-
-	return max(lowerSize, upperSize), nil
+	return m.lower.Size()
 }
 
+// TODO: We should handle the slice even for more than one block size
 func (m *MaskedOverlay) Slice(ctx context.Context, off, length int64) ([]byte, error) {
 	if m.mask.Has(off) {
 		return m.upper.Slice(ctx, off, length)
@@ -70,4 +97,3 @@ func (m *MaskedOverlay) Slice(ctx context.Context, off, length int64) ([]byte, e
 
 	return m.lower.Slice(ctx, off, length)
 }
-
