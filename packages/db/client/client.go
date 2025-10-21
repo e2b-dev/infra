5c7742f7608d2e04fdc82908af4ee86def6c4b48
Joe Lombrozo <joe@djeebus.net>
2025-10-20 20:02:26 -0700

    clean up creating pools

--- packages/db/client/client.go
+++ packages/db/client/client.go
@@ -8,7 +8,6 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	_ "github.com/lib/pq"
-	"go.uber.org/zap"
 
 	database "github.com/e2b-dev/infra/packages/db/queries"
 	"github.com/e2b-dev/infra/packages/shared/pkg/utils"
@@ -40,9 +39,7 @@ func NewClient(ctx context.Context, options ...Option) (*Client, error) {
 	// Parse the connection pool configuration
 	config, err := pgxpool.ParseConfig(databaseURL)
 	if err != nil {
-		zap.L().Error("Unable to parse database URL", zap.Error(err))
-
-		return nil, err
+		return nil, fmt.Errorf("failed to parse connection pool config: %w", err)
 	}
 
 	// Set the default number of connections
@@ -56,7 +53,7 @@ func NewClient(ctx context.Context, options ...Option) (*Client, error) {
 	// Create the connection pool
 	pool, err := pgxpool.NewWithConfig(ctx, config)
 	if err != nil {
-		zap.L().Error("Unable to create connection pool", zap.Error(err))
+		return nil, fmt.Errorf("failed to create connection pool: %w", err)
 	}
 
 	// expose otel metrics
