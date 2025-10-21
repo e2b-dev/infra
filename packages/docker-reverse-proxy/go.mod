733b0a52de8484cb726bf58cfb40281188f6b58e
Joe Lombrozo <joe@djeebus.net>
2025-10-20 19:50:43 -0700

    Enable otel for sql connections

--- packages/docker-reverse-proxy/go.mod
+++ packages/docker-reverse-proxy/go.mod
@@ -12,9 +12,12 @@ require (
 require (
 	ariga.io/atlas v0.15.0 // indirect
 	entgo.io/ent v0.12.5 // indirect
+	github.com/XSAM/otelsql v0.40.0 // indirect
 	github.com/agext/levenshtein v1.2.3 // indirect
 	github.com/apparentlymart/go-textseg/v15 v15.0.0 // indirect
 	github.com/dchest/uniuri v1.2.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/inflect v0.21.0 // indirect
 	github.com/go-test/deep v1.0.8 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
@@ -23,6 +26,10 @@ require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mitchellh/go-wordwrap v1.0.1 // indirect
 	github.com/zclconf/go-cty v1.14.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.0 // indirect
+	go.opentelemetry.io/otel v1.38.0 // indirect
+	go.opentelemetry.io/otel/metric v1.38.0 // indirect
+	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	golang.org/x/mod v0.27.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
 	golang.org/x/text v0.29.0 // indirect
--- packages/shared/pkg/db/client.go
+++ packages/shared/pkg/db/client.go
@@ -6,8 +6,10 @@ import (
 	"time"
 
 	"entgo.io/ent/dialect"
-	"entgo.io/ent/dialect/sql"
+	entsql "entgo.io/ent/dialect/sql"
+	"github.com/XSAM/otelsql"
 	_ "github.com/lib/pq"
+	semconv "go.opentelemetry.io/otel/semconv/v1.28.0"
 
 	"github.com/e2b-dev/infra/packages/shared/pkg/models"
 )
@@ -22,16 +24,23 @@ func NewClient(maxConns, maxIdle int) (*DB, error) {
 		return nil, fmt.Errorf("database URL is empty")
 	}
 
-	drv, err := sql.Open(dialect.Postgres, databaseURL)
+	db, err := otelsql.Open(dialect.Postgres, databaseURL, otelsql.WithAttributes(
+		semconv.DBSystemPostgreSQL,
+	))
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to open db: %w", err)
+	}
+
+	if err = otelsql.RegisterDBStatsMetrics(db, otelsql.WithAttributes(semconv.DBSystemMySQL)); err != nil {
+		return nil, fmt.Errorf("failed to register db stats metrics: %w", err)
 	}
 
 	// Get the underlying sql.DB object of the driver.
-	db := drv.DB()
 	db.SetMaxOpenConns(maxConns)
 	db.SetMaxIdleConns(maxIdle)
 	db.SetConnMaxLifetime(time.Minute * 30)
+
+	drv := entsql.OpenDB(dialect.Postgres, db)
 
 	client := models.NewClient(models.Driver(drv))
 
