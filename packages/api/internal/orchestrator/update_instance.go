3c5c68e20674985124fb74941e261b343314251a
Jakub Novak <jakub@e2b.dev>
2025-10-27 19:50:27 +0100

    Fix imports

--- packages/api/internal/orchestrator/update_instance.go
+++ packages/api/internal/orchestrator/update_instance.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/e2b-dev/infra/packages/api/internal/utils"
 	"github.com/google/uuid"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
@@ -14,6 +13,7 @@ import (
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
 
+	"github.com/e2b-dev/infra/packages/api/internal/utils"
 	"github.com/e2b-dev/infra/packages/shared/pkg/grpc/orchestrator"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
