88e0de64ed93f40e407dafeeabaac94d8860c097
Jakub Dobry <jakub.dobry8@gmail.com>
2025-10-29 12:27:04 +0100

    fix: return correct metadata for failed templates

--- packages/db/queries/get_team_templates.sql
+++ packages/db/queries/get_team_templates.sql
@@ -1,6 +1,6 @@
 -- name: GetTeamTemplates :many
 SELECT sqlc.embed(e),
-       eb.id as build_id, eb.vcpu as build_vcpu, eb.ram_mb as build_ram_mb, eb.total_disk_size_mb as build_total_disk_size_mb, eb.envd_version as build_envd_version, eb.firecracker_version as build_firecracker_version, eb.status as build_status,
+       eb.id as build_id, eb.vcpu as build_vcpu, eb.ram_mb as build_ram_mb, eb.total_disk_size_mb as build_total_disk_size_mb, eb.envd_version as build_envd_version, eb.firecracker_version as build_firecracker_version, eba.status as build_status,
        u.id as creator_id, u.email as creator_email,
        COALESCE(ea.aliases, ARRAY[]::text[])::text[] AS aliases
 FROM public.envs AS e
@@ -13,10 +13,17 @@ LEFT JOIN LATERAL (
 LEFT JOIN LATERAL (
     SELECT b.*
     FROM public.env_builds AS b
-    WHERE b.env_id = e.id
+    WHERE b.env_id = e.id AND b.status = 'uploaded'
     ORDER BY b.finished_at DESC
     LIMIT 1
 ) eb ON TRUE
+LEFT JOIN LATERAL (
+    SELECT b.*
+    FROM public.env_builds AS b
+    WHERE b.env_id = e.id
+    ORDER BY b.finished_at DESC
+    LIMIT 1
+) eba ON TRUE
 WHERE
   e.team_id = $1
   AND eb.id IS NOT NULL
