9fc656edffd011af6399e595f63b97f7566b7637
ValentaTomas <valenta.and.thomas@gmail.com>
2025-11-10 11:09:52 -0800

    Add comments

--- packages/orchestrator/internal/sandbox/uffd/userfaultfd/userfaultfd.go
+++ packages/orchestrator/internal/sandbox/uffd/userfaultfd/userfaultfd.go
@@ -162,6 +162,8 @@ outerLoop:
 		}
 
 		// Handle write to missing page (WRITE flag)
+		// If the event has WRITE flag, it was a write to a missing page.
+		// For the write to be executed, we first need to copy the page from the source to the guest memory.
 		if flags&UFFD_PAGEFAULT_FLAG_WRITE != 0 {
 			err := u.handleMissing(ctx, fdExit.SignalExit, addr, offset, pagesize)
 			if err != nil {
@@ -172,6 +174,7 @@ outerLoop:
 		}
 
 		// Handle read to missing page ("MISSING" flag)
+		// If the event has no flags, it was a read to a missing page and we need to copy the page from the source to the guest memory.
 		if flags == 0 {
 			err := u.handleMissing(ctx, fdExit.SignalExit, addr, offset, pagesize)
 			if err != nil {
