6956f4d0d1fa21bc9510c18accaafb95595e9b3c
ValentaTomas <valenta.and.thomas@gmail.com>
2025-11-10 11:02:42 -0800

    Improve comments

--- packages/orchestrator/internal/sandbox/uffd/memory/region.go
+++ packages/orchestrator/internal/sandbox/uffd/memory/region.go
@@ -2,12 +2,12 @@ package memory
 
 // Region is a mapping of a region of memory of the guest to a region of memory on the host.
 // The serialization is based on the Firecracker UFFD protocol communication.
+// https://github.com/firecracker-microvm/firecracker/blob/ceeca6a14284537ae0b2a192cd2ffef10d3a81e2/src/vmm/src/persist.rs#L96
 type Region struct {
 	BaseHostVirtAddr uintptr `json:"base_host_virt_addr"`
 	Size             uintptr `json:"size"`
 	Offset           uintptr `json:"offset"`
-	// This is actually in bytes.
-	// This field is deprecated in the newer version of the Firecracer with a new field `page_size`.
+	// This field is deprecated in the newer version of the Firecracker with a new field `page_size`.
 	PageSize uintptr `json:"page_size_kib"`
 }
 
