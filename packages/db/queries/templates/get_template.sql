b294deeaf476baaefd1eee50e8791eb833f0f82e
Jakub Novak <jakub@e2b.dev>
2025-11-11 10:45:18 +0100

    Fix query

--- packages/db/queries/templates/get_template.sql
+++ packages/db/queries/templates/get_template.sql
@@ -1,7 +1,8 @@
 -- name: GetTemplateByIdOrAlias :one
 SELECT e.* FROM "public"."envs" e
-LEFT JOIN "public"."env_aliases" ea ON ea.env_id = e.id
-WHERE (
-  e.id = @template_id_or_alias OR
-  ea.alias = @template_id_or_alias
+WHERE e.id IN (
+    SELECT e.id FROM "public"."envs" e
+    LEFT JOIN "public"."env_aliases" ea ON ea.env_id = e.id
+    WHERE e.id = @template_id_or_alias
+    OR ea.alias = @template_id_or_alias
 );
