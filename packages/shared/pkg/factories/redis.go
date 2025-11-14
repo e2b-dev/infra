f4d934d0929fcf881c07804bf427bba4a100a7cd
Jakub Novák <jakub@e2b.dev>
2025-11-14 13:28:28 +0100

    Use redis with tls in orchestrator (#1488)

--- packages/shared/pkg/factories/redis.go
+++ packages/shared/pkg/factories/redis.go
@@ -2,17 +2,26 @@ package factories
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/base64"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/redis/go-redis/v9"
-
-	"github.com/e2b-dev/infra/packages/orchestrator/internal/cfg"
+	"go.uber.org/zap"
 )
 
 var ErrRedisDisabled = errors.New("redis is disabled")
 
-func NewRedisClient(ctx context.Context, config cfg.Config) (redis.UniversalClient, error) {
+type RedisConfig struct {
+	RedisURL         string
+	RedisClusterURL  string
+	RedisTLSCABase64 string
+}
+
+func NewRedisClient(ctx context.Context, config RedisConfig) (redis.UniversalClient, error) {
 	var redisClient redis.UniversalClient
 
 	switch {
@@ -21,10 +30,38 @@ func NewRedisClient(ctx context.Context, config cfg.Config) (redis.UniversalClie
 		// > Redis node endpoints can change and can be recycled as nodes are added and removed over time.
 		// https://cloud.google.com/memorystore/docs/cluster/cluster-node-specification#cluster_endpoints
 		// https://cloud.google.com/memorystore/docs/cluster/client-library-code-samples#go-redis
-		redisClient = redis.NewClusterClient(&redis.ClusterOptions{
+		clusterOpts := &redis.ClusterOptions{
 			Addrs:        []string{config.RedisClusterURL},
 			MinIdleConns: 1,
-		})
+		}
+
+		if config.RedisTLSCABase64 != "" {
+			cert, err := base64.StdEncoding.DecodeString(config.RedisTLSCABase64)
+			if err != nil {
+				zap.L().Error("Failed to decode Redis cluster TLS CA certificate from base64", zap.Error(err))
+
+				return nil, err
+			}
+
+			certPool := x509.NewCertPool()
+			if !certPool.AppendCertsFromPEM(cert) {
+				zap.L().Error("Failed to parse Redis cluster TLS CA certificate")
+
+				return nil, fmt.Errorf("failed to parse Redis cluster TLS CA certificate")
+			}
+
+			// Remove the port if present
+			serverName := strings.Split(config.RedisClusterURL, ":")[0]
+			clusterOpts.TLSConfig = &tls.Config{
+				RootCAs:    certPool,
+				MinVersion: tls.VersionTLS12,
+				ServerName: serverName,
+			}
+
+			zap.L().Info("Redis cluster will be started with TLS enabled")
+		}
+
+		redisClient = redis.NewClusterClient(clusterOpts)
 	case config.RedisURL != "":
 		redisClient = redis.NewClient(&redis.Options{
 			Addr:         config.RedisURL,
