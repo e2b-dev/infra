f2843e3254576b30aa7cf9e7987ca069700662f8
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-11-17 11:44:44 -0800

    bring back a few more

--- packages/shared/pkg/telemetry/tracing.go
+++ packages/shared/pkg/telemetry/tracing.go
@@ -82,6 +82,7 @@ func ReportCriticalError(ctx context.Context, message string, err error, attrs .
 	errorAttrs := append(attrs, attribute.String("error.message", message))
 
 	span.RecordError(fmt.Errorf("%s: %w", message, err),
+		trace.WithStackTrace(true),
 		trace.WithAttributes(
 			errorAttrs...,
 		),
@@ -97,6 +98,7 @@ func ReportError(ctx context.Context, message string, err error, attrs ...attrib
 	zap.L().With(attributesToZapFields(attrs...)...).Warn(message, zap.Stringp("debug_id", debugID), zap.Error(err))
 
 	span.RecordError(fmt.Errorf("%s: %w", message, err),
+		trace.WithStackTrace(true),
 		trace.WithAttributes(
 			attrs...,
 		),
