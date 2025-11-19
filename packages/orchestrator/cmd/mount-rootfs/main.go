aa33f092575c86c2620eb5176ff300f0b5973a23
Tomas Valenta <valenta.and.thomas@gmail.com>
2025-11-18 21:40:24 -0800

    Add rootfs tests (#1498)
    Co-authored-by: Jakub Dobry <jakub.dobry8@gmail.com>

--- packages/orchestrator/cmd/mount-rootfs/main.go
+++ packages/orchestrator/cmd/mount-rootfs/main.go
@@ -4,63 +4,34 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"os/signal"
 	"path/filepath"
 	"time"
 
 	"github.com/google/uuid"
-	"go.opentelemetry.io/otel/metric/noop"
-	"golang.org/x/sys/unix"
+	"go.uber.org/zap"
 
-	"github.com/e2b-dev/infra/packages/orchestrator/internal/cfg"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/block"
-	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/block/metrics"
-	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/build"
-	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/nbd"
-	featureflags "github.com/e2b-dev/infra/packages/shared/pkg/feature-flags"
-	"github.com/e2b-dev/infra/packages/shared/pkg/storage"
-	"github.com/e2b-dev/infra/packages/shared/pkg/storage/header"
+	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/nbd/testutils"
 )
 
-var _ block.ReadonlyDevice = (*mountReadonlyDevice)(nil)
-
-type mountReadonlyDevice struct {
-	*build.File
-
-	header    *header.Header
-	blockSize int64
-}
-
-func newReadonlyDevice(file *build.File, header *header.Header, blockSize int64) *mountReadonlyDevice {
-	return &mountReadonlyDevice{
-		File:      file,
-		header:    header,
-		blockSize: blockSize,
-	}
-}
-
-func (m *mountReadonlyDevice) Close() error {
-	return nil
-}
-
-func (m *mountReadonlyDevice) BlockSize() int64 {
-	return m.blockSize
-}
-
-func (m *mountReadonlyDevice) Header() *header.Header {
-	return m.header
-}
-
-func (m *mountReadonlyDevice) Size() (int64, error) {
-	return int64(m.header.Metadata.Size), nil
-}
-
 func main() {
-	buildId := flag.String("build", "", "build id")
-	mountPath := flag.String("mount", "", "mount path")
+	buildId := flag.String("build", "", "build id (only used when empty flag is false)")
+	mountPath := flag.String("mount", "", "mount path (only used when empty flag is false)")
+	verify := flag.Bool("verify", false, "verify rootfs integrity (only used when empty flag is false)")
+	logging := flag.Bool("log", false, "enable logging (it is pretty spammy)")
+	empty := flag.Bool("empty", false, "create an empty rootfs")
+	size := flag.Int64("size", 1024*1024*1024, "size of the rootfs (only used when empty flag is true)")
+	blockSize := flag.Int64("block-size", 4096, "block size of the rootfs (only used when empty flag is true)")
 
 	flag.Parse()
+
+	if *verify && *mountPath == "" {
+		log.Fatalf("verify flag is only supported when mount path is provided")
+	}
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -68,12 +39,14 @@ func main() {
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
 
-	// Disabling the logger for normal use—is very spammy, because Populate on device pool periodically logs errors if the number of acquirable devices is less than the number of requested devices.
-	// logger, err := zap.NewDevelopment()
-	// if err != nil {
-	// 	log.Fatalf("failed to create logger: %s", err)
-	// }
-	// zap.ReplaceGlobals(logger)
+	// Logger is very spammy, because Populate on device pool periodically logs errors if the number of acquirable devices is less than the number of requested devices.
+	if *logging {
+		logger, err := zap.NewDevelopment()
+		if err != nil {
+			panic(fmt.Errorf("failed to create logger: %w", err))
+		}
+		zap.ReplaceGlobals(logger)
+	}
 
 	go func() {
 		<-done
@@ -84,103 +57,32 @@ func main() {
 	// We use a separate ctx for majority of the operations as cancelling context for the NBD+storage and *then* doing cleanup for these often resulted in deadlocks.
 	nbdContext := context.Background()
 
-	err := mountRootfs(ctx, nbdContext, *buildId, *mountPath)
-	if err != nil {
-		panic(fmt.Errorf("failed to mount rootfs: %w", err))
+	if *empty {
+		err := runEmpty(ctx, nbdContext, *size, *blockSize)
+		if err != nil {
+			panic(fmt.Errorf("failed to create empty rootfs: %w", err))
+		}
+	} else {
+		err := run(ctx, nbdContext, *buildId, *mountPath, *verify)
+		if err != nil {
+			panic(fmt.Errorf("failed to mount rootfs: %w", err))
+		}
 	}
 }
 
-func mountRootfs(ctx, nbdContext context.Context, buildID, mountPath string) error {
-	files := storage.TemplateFiles{
-		BuildID: buildID,
-	}
+func runEmpty(ctx, nbdContext context.Context, size int64, blockSize int64) error {
+	cowCachePath := filepath.Join(os.TempDir(), fmt.Sprintf("rootfs.ext4.cow.cache-%s", uuid.New().String()))
 
-	s, err := storage.GetTemplateStorageProvider(nbdContext, nil)
+	emptyDevice, err := testutils.NewZeroDevice(size, blockSize)
 	if err != nil {
-		return fmt.Errorf("failed to get storage provider: %w", err)
+		return fmt.Errorf("failed to create zero device: %w", err)
 	}
-
-	obj, err := s.OpenObject(nbdContext, files.StorageRootfsHeaderPath(), storage.RootFSHeaderObjectType)
-	if err != nil {
-		return fmt.Errorf("failed to open object: %w", err)
-	}
-
-	h, err := header.Deserialize(nbdContext, obj)
-	if err != nil {
-		id, err := uuid.Parse(buildID)
-		if err != nil {
-			return fmt.Errorf("failed to parse build id: %w", err)
-		}
-
-		r, err := s.OpenSeekableObject(nbdContext, files.StorageRootfsPath(), storage.RootFSObjectType)
-		if err != nil {
-			return fmt.Errorf("failed to open object: %w", err)
-		}
-
-		size, err := r.Size(nbdContext)
-		if err != nil {
-			return fmt.Errorf("failed to get object size: %w", err)
-		}
-
-		h, err = header.NewHeader(&header.Metadata{
-			BuildId:     id,
-			BaseBuildId: id,
-			Size:        uint64(size),
-			Version:     1,
-			BlockSize:   header.RootfsBlockSize,
-			Generation:  1,
-		}, nil)
-		if err != nil {
-			return fmt.Errorf("failed to create header for rootfs without header: %w", err)
-		}
-	}
-
-	diffCacheDir := filepath.Join(os.TempDir(), fmt.Sprintf("%s-rootfs.diff.cache-%s", buildID, uuid.New().String()))
-
-	err = os.MkdirAll(diffCacheDir, 0o755)
-	if err != nil {
-		return fmt.Errorf("failed to create diff cache directory: %w", err)
-	}
-
-	defer os.RemoveAll(diffCacheDir)
-
-	flags, err := featureflags.NewClient()
-	if err != nil {
-		return fmt.Errorf("failed to create feature flags client: %w", err)
-	}
-
-	store, err := build.NewDiffStore(
-		nbdContext,
-		cfg.Config{},
-		flags,
-		diffCacheDir,
-		24*time.Hour,
-		24*time.Hour,
-	)
-	if err != nil {
-		return fmt.Errorf("failed to create diff store: %w", err)
-	}
-
-	defer store.Close()
-
-	fmt.Printf("caching diffs to: %+v\n", diffCacheDir)
-
-	m, err := metrics.NewMetrics(noop.NewMeterProvider())
-	if err != nil {
-		return fmt.Errorf("failed to create metrics: %w", err)
-	}
-
-	rootfs := build.NewFile(h, store, build.Rootfs, s, m)
-
-	readonlyDevice := newReadonlyDevice(rootfs, h, int64(h.Metadata.BlockSize))
-
-	cowCachePath := filepath.Join(os.TempDir(), fmt.Sprintf("%s-rootfs.ext4.cow.cache-%s", buildID, uuid.New().String()))
 
 	defer os.RemoveAll(cowCachePath)
 
 	cache, err := block.NewCache(
-		int64(h.Metadata.Size),
-		int64(h.Metadata.BlockSize),
+		size,
+		blockSize,
 		cowCachePath,
 		false,
 	)
@@ -190,96 +92,113 @@ func mountRootfs(ctx, nbdContext context.Context, buildID, mountPath string) err
 
 	fmt.Printf("caching writes to: %+v\n", cowCachePath)
 
-	overlay := block.NewOverlay(readonlyDevice, cache)
+	overlay := block.NewOverlay(emptyDevice, cache)
 	defer overlay.Close()
 
-	devicePool, err := nbd.NewDevicePool()
+	devicePath, deviceCleanup, err := testutils.GetNBDDevice(nbdContext, testutils.NewLoggerOverlay(overlay))
+	defer deviceCleanup.Run(ctx, 30*time.Second)
 	if err != nil {
-		return fmt.Errorf("failed to create device pool: %w", err)
+		return fmt.Errorf("failed to get nbd device: %w", err)
 	}
-
-	poolClosed := make(chan struct{})
-
-	defer func() { //nolint:contextcheck // we need to use separate context otherwise the cleanup can be problematic
-		<-poolClosed
-
-		cleanupCtx, cancelCleanup := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancelCleanup()
-
-		err = devicePool.Close(cleanupCtx)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to close device pool: %v\n", err)
-		}
-	}()
-
-	poolCtx, poolCancel := context.WithCancel(nbdContext)
-	defer poolCancel()
-
-	go func() {
-		devicePool.Populate(poolCtx)
-		close(poolClosed)
-	}()
-
-	mnt := nbd.NewDirectPathMount(overlay, devicePool)
-
-	mntIndex, err := mnt.Open(nbdContext)
-	if err != nil {
-		return fmt.Errorf("failed to open nbd mount: %w", err)
-	}
-
-	defer func() { //nolint:contextcheck // we need to use separate context otherwise the cleanup can be problematic
-		cleanupCtx, cancelCleanup := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancelCleanup()
-
-		err = mnt.Close(cleanupCtx)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to close nbd mount: %v\n", err)
-		}
-	}()
-
-	devicePath := nbd.GetDevicePath(mntIndex)
 
 	fmt.Printf("rootfs exposed as device: %s\n", devicePath)
 
-	err = os.MkdirAll(mountPath, 0o755)
+	<-ctx.Done()
+
+	fmt.Println("closing rootfs mount")
+
+	return nil
+}
+
+func run(ctx, nbdContext context.Context, buildID, mountPath string, verify bool) error {
+	rootfs, rootfsCleanup, err := testutils.TemplateRootfs(ctx, buildID)
+	defer rootfsCleanup.Run(ctx, 30*time.Second)
 	if err != nil {
-		return fmt.Errorf("failed to create mount path directory: %w", err)
+		return fmt.Errorf("failed to get template rootfs: %w", err)
 	}
 
-	fmt.Fprintf(os.Stdout, "creating mount path directory: %s\n", mountPath)
+	cowCachePath := filepath.Join(os.TempDir(), fmt.Sprintf("%s-rootfs.ext4.cow.cache-%s", buildID, uuid.New().String()))
 
-	// We don't remote the dir as it might have been user created.
+	defer os.RemoveAll(cowCachePath)
 
-	err = unix.Mount(devicePath, mountPath, "ext4", unix.MS_RDONLY, "")
+	cache, err := block.NewCache(
+		int64(rootfs.Header().Metadata.Size),
+		int64(rootfs.Header().Metadata.BlockSize),
+		cowCachePath,
+		false,
+	)
 	if err != nil {
-		return fmt.Errorf("failed to mount device to mount path: %w", err)
+		return fmt.Errorf("failed to create cache: %w", err)
 	}
 
-	defer func() {
-		ticker := time.NewTicker(600 * time.Millisecond)
-		defer ticker.Stop()
+	fmt.Printf("caching writes to: %+v\n", cowCachePath)
 
-		cleanupCtx, cleanupCancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cleanupCancel()
+	overlay := block.NewOverlay(rootfs, cache)
+	defer overlay.Close()
 
-		for {
-			select {
-			case <-cleanupCtx.Done():
-				fmt.Fprintf(os.Stderr, "failed to unmount device from mount path in time\n")
+	devicePath, deviceCleanup, err := testutils.GetNBDDevice(nbdContext, overlay)
+	defer deviceCleanup.Run(ctx, 30*time.Second)
+	if err != nil {
+		return fmt.Errorf("failed to get nbd device: %w", err)
+	}
 
-				return
-			case <-ticker.C:
-				err = unix.Unmount(mountPath, 0)
-				if err == nil {
-					return
-				}
+	fmt.Printf("rootfs exposed as device: %s\n", devicePath)
 
-				fmt.Fprintf(os.Stderr, "failed to unmount device from mount path: %v\n", err)
+	if mountPath != "" {
+		err = os.MkdirAll(mountPath, 0o755)
+		if err != nil {
+			return fmt.Errorf("failed to create mount path directory: %w", err)
+		}
+
+		fmt.Fprintf(os.Stdout, "creating mount path directory: %s\n", mountPath)
+
+		mountCleanup, err := testutils.MountNBDDevice(devicePath, mountPath)
+		defer mountCleanup.Run(ctx, 30*time.Second)
+		if err != nil {
+			return fmt.Errorf("failed to mount device to mount path: %w", err)
+		}
+
+		// We don't remove the dir as it might have been user created.
+
+		fmt.Printf("rootfs mounted at path: %s\n", mountPath)
+	}
+
+	if verify {
+		fmt.Println("\nverifying rootfs integrity...")
+
+		cmd := exec.CommandContext(ctx, "e2fsck", "-nfv", devicePath)
+
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Run()
+		if err != nil {
+			return fmt.Errorf("failed to verify rootfs integrity: %w", err)
+		}
+
+		fmt.Println("\nrootfs integrity verified")
+
+		journalDir := filepath.Join(mountPath, "var", "log", "journal")
+		journalFiles, err := os.ReadDir(journalDir)
+		if err != nil {
+			return fmt.Errorf("failed to read journal directory: %w", err)
+		}
+
+		for _, journalFile := range journalFiles {
+			cmd := exec.CommandContext(ctx, "journalctl", "--verify", "--directory", filepath.Join(journalDir, journalFile.Name()))
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+
+			err := cmd.Run()
+			if err != nil {
+				return fmt.Errorf("failed to verify journal file: %w", err)
 			}
 		}
-	}()
 
-	fmt.Printf("rootfs mounted at path: %s\n", mountPath)
+		fmt.Println("\njournal files verified")
+
+		return nil
+	}
 
 	<-ctx.Done()
 
---
+++ packages/orchestrator/internal/sandbox/nbd/testutils/zero_device.go
@@ -0,0 +1,71 @@
+package testutils
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/block"
+	"github.com/e2b-dev/infra/packages/shared/pkg/storage/header"
+)
+
+var _ block.ReadonlyDevice = (*ZeroDevice)(nil)
+
+type ZeroDevice struct {
+	blockSize int64
+	size      int64
+	header    *header.Header
+}
+
+func NewZeroDevice(size int64, blockSize int64) (*ZeroDevice, error) {
+	h, err := header.NewHeader(header.NewTemplateMetadata(
+		uuid.Nil,
+		uint64(blockSize),
+		uint64(size),
+	),
+		[]*header.BuildMap{
+			{
+				Offset:             0,
+				Length:             uint64(size),
+				BuildId:            uuid.Nil,
+				BuildStorageOffset: 0,
+			},
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create header: %w", err)
+	}
+
+	return &ZeroDevice{
+		size:      size,
+		blockSize: blockSize,
+		header:    h,
+	}, nil
+}
+
+func (z *ZeroDevice) ReadAt(_ context.Context, p []byte, _ int64) (n int, err error) {
+	clear(p)
+
+	return len(p), nil
+}
+
+func (z *ZeroDevice) BlockSize() int64 {
+	return z.blockSize
+}
+
+func (z *ZeroDevice) Slice(_ context.Context, _, length int64) ([]byte, error) {
+	return make([]byte, length), nil
+}
+
+func (z *ZeroDevice) Header() *header.Header {
+	return z.header
+}
+
+func (z *ZeroDevice) Close() error {
+	return nil
+}
+
+func (z *ZeroDevice) Size() (int64, error) {
+	return z.size, nil
+}
