fb9df3cbc668aef99258b49c935b3fd8ffd93d58
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-11-19 14:02:07 -0800

    linting

--- packages/orchestrator/ioc/hyperloop.go
+++ packages/orchestrator/ioc/hyperloop.go
@@ -42,7 +42,7 @@ func newHyperloopServer(
 	}
 
 	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
+		OnStart: func(context.Context) error {
 			go func() {
 				err := hyperloopSrv.ListenAndServe()
 				if err != nil && !errors.Is(err, http.ErrServerClosed) {
