4a5bd14255c5a7c96622033ef4e510017fd6b880
Jakub Novák <jakub@e2b.dev>
2025-11-19 09:50:38 +0100

    Template delete - Fix nil dereference (#1506)

--- packages/db/queries/builds/get_template_builds_by_id_or_alias.sql
+++ packages/db/queries/builds/get_template_builds_by_id_or_alias.sql
@@ -2,7 +2,7 @@
 SELECT sqlc.embed(e), eb.id as build_id, eb.cluster_node_id FROM "public"."envs" e
 LEFT JOIN "public"."env_builds" eb ON eb.env_id = e.id
 WHERE
-    e.team_id = @team_id AND e.id in (
+    e.id in (
     SELECT e.id FROM "public"."envs" e
     LEFT JOIN "public"."env_aliases" ea ON ea.env_id = e.id
     WHERE e.id = @template_id_or_alias OR
