36d04071ac0070a1b62cc1cd6b086aaddf2639c8
Jakub Dobry <jakub.dobry8@gmail.com>
2025-11-21 22:55:22 +0100

    fix gin context

--- packages/api/internal/auth/middleware.go
+++ packages/api/internal/auth/middleware.go
@@ -198,9 +198,6 @@ func CreateAuthenticationFunc(
 	}
 
 	return func(ctx context.Context, input *openapi3filter.AuthenticationInput) error {
-		c := middleware.GetGinContext(ctx)
-		ctx = c.Request.Context()
-
 		ctx, span := tracer.Start(ctx, "authenticate")
 		defer span.End()
 
