e3ecd79371b00bf2a2ac233351d90144075e9872
Jakub Novak <jakub@e2b.dev>
2025-11-24 12:09:21 +0100

    Make it unique

--- packages/db/migrations/20251124110249_users_teams_unique.sql
+++ packages/db/migrations/20251124110249_users_teams_unique.sql
@@ -1,6 +1,6 @@
 -- +goose NO TRANSACTION
 -- +goose Up
-CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_users_teams_unique ON "public"."users_teams"(user_id, team_id);
+CREATE UNIQUE INDEX CONCURRENTLY IF NOT EXISTS idx_users_teams_unique ON "public"."users_teams"(user_id, team_id);
 
 -- +goose Down
 DROP INDEX CONCURRENTLY IF EXISTS idx_users_teams_unique;
