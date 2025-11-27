412990bff2004bd24e0b1b443dca85a6d94290c3
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-11-27 13:02:45 -0800

    move files layer into embedded templates
    this makes it easier to read the individual files and for linters to parse them.

---
+++ packages/orchestrator/internal/template/build/core/rootfs/rootfs_test.go
@@ -0,0 +1,71 @@
+package rootfs
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/e2b-dev/infra/packages/orchestrator/internal/cfg"
+	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/build/buildcontext"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAdditionalOCILayers(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		tempDir := t.TempDir()
+
+		envdPath := tempDir + "/envd"
+		err := os.WriteFile(envdPath, []byte("echo hello"), 0o755)
+		require.NoError(t, err)
+
+		buildContext := buildcontext.BuildContext{
+			BuilderConfig: cfg.BuilderConfig{
+				HostEnvdPath: envdPath,
+			},
+		}
+		provisionScript := "provision.sh"
+		provisionLogPrefix := "provision.log"
+		provisionResultPath := "provision.result"
+
+		layers, err := additionalOCILayers(buildContext, provisionScript, provisionLogPrefix, provisionResultPath)
+		require.NoError(t, err)
+
+		require.Len(t, layers, 2)
+		layer1 := layers[0]
+		filesLayer, err := layer1.Uncompressed()
+		require.NoError(t, err)
+		t.Cleanup(func() {
+			err = filesLayer.Close()
+			assert.NoError(t, err)
+		})
+
+		actualFiles := map[string]string{}
+		filesTarReader := tar.NewReader(filesLayer)
+		for {
+			header, err := filesTarReader.Next()
+			if err == io.EOF {
+				break
+			}
+			require.NoError(t, err)
+
+			if header.Typeflag != tar.TypeReg {
+				// we're only verifying files for now
+				continue
+			}
+
+			filename := header.Name
+			var buffer bytes.Buffer
+			count, err := io.CopyN(&buffer, filesTarReader, header.Size)
+			require.NoError(t, err)
+			assert.Equal(t, header.Size, count)
+			actualFiles[filename] = buffer.String()
+		}
+
+		assert.Equal(t, 13, len(actualFiles))
+		assert.Equal(t, "e2b.local", actualFiles["etc/hostname"])
+		assert.Equal(t, "nameserver 8.8.8.8", actualFiles["etc/resolv.conf"])
+	})
+}
