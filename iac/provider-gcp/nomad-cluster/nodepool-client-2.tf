0c90f780bddf78f9934e2213f2fbb4732e9714ad
Jakub Novak <jakub@e2b.dev>
2025-12-03 16:49:20 +0100

    Remove fallback

--- iac/provider-gcp/nomad-cluster/nodepool-client-2.tf
+++ iac/provider-gcp/nomad-cluster/nodepool-client-2.tf
@@ -1,6 +1,6 @@
 locals {
-  build_pool_name = "${var.prefix}orch-build"
-  build_startup_script = templatefile("${path.module}/scripts/start-client-c4.sh", {
+  client_pool_name_c4 = "${var.prefix}${var.client_cluster_name}-c4"
+  client_startup_script_c4 = templatefile("${path.module}/scripts/start-client-c4.sh", {
     CLUSTER_TAG_NAME             = var.cluster_tag_name
     SCRIPTS_BUCKET               = var.cluster_setup_bucket_name
     FC_KERNELS_BUCKET_NAME       = var.fc_kernels_bucket_name
@@ -20,14 +20,15 @@ locals {
     NFS_MOUNT_SUBDIR             = local.nfs_mount_subdir
     NFS_MOUNT_OPTS               = local.nfs_mount_opts
     USE_FILESTORE_CACHE          = var.filestore_cache_enabled
-    NODE_POOL                    = var.build_node_pool
-    BASE_HUGEPAGES_PERCENTAGE    = var.build_base_hugepages_percentage
-    LOCAL_CACHE_DISK_COUNT       = var.build_cluster_cache_disk_count
+    NODE_POOL                    = var.orchestrator_node_pool
+    BASE_HUGEPAGES_PERCENTAGE    = var.orchestrator_base_hugepages_percentage
+    LOCAL_CACHE_DISK_COUNT       = var.client_cluster_cache_disk_count
   })
 }
 
-resource "google_compute_health_check" "build_nomad_check" {
-  name                = "${local.build_pool_name}-nomad-build-cluster-check"
+
+resource "google_compute_health_check" "client_nomad_check_c4" {
+  name                = "${local.client_pool_name_c4}-nomad-client-check-c4"
   check_interval_sec  = 15
   timeout_sec         = 10
   healthy_threshold   = 2
@@ -43,52 +44,68 @@ resource "google_compute_health_check" "build_nomad_check" {
   }
 }
 
-resource "google_compute_instance_group_manager" "build_pool" {
-  name = "${local.build_pool_name}-ig"
+resource "google_compute_region_autoscaler" "client_c4" {
+  count = var.client_cluster_size < var.client_cluster_size_max ? 1 : 0
+
+  name   = "${local.client_pool_name_c4}-client-autoscale-c4r"
+  region = var.gcp_region
+  target = google_compute_region_instance_group_manager.client_pool_c4.id
+
+  autoscaling_policy {
+    max_replicas    = var.client_cluster_size_max
+    min_replicas    = var.client_cluster_size
+    cooldown_period = 240
+    # Turn off autoscaling when the cluster size is equal to the maximum size.
+    mode = "ONLY_SCALE_OUT"
+
+    cpu_utilization {
+      target = 0.6
+    }
+  }
+}
+
+resource "google_compute_region_instance_group_manager" "client_pool_c4" {
+  name   = "${local.client_pool_name_c4}-ig"
+  region = var.gcp_region
+
+  target_size = var.client_cluster_size < var.client_cluster_size_max ? null : var.client_cluster_size
 
   version {
-    name              = google_compute_instance_template.build.id
-    instance_template = google_compute_instance_template.build.id
-  }
-
-  # TODO: (2025-10-01) - keep this here for a migration period (at least until 2025-10-15)
-  named_port {
-    name = var.docker_reverse_proxy_port.name
-    port = var.docker_reverse_proxy_port.port
+    name              = google_compute_instance_template.client_c4.id
+    instance_template = google_compute_instance_template.client_c4.id
   }
 
   auto_healing_policies {
-    health_check      = google_compute_health_check.build_nomad_check.id
+    health_check      = google_compute_health_check.client_nomad_check_c4.id
     initial_delay_sec = 600
   }
+
+  distribution_policy_target_shape = "BALANCED"
 
   # Server is a stateful cluster, so the update strategy used to roll out a new GCE Instance Template must be
   # a rolling update.
   update_policy {
-    type                    = var.environment == "dev" ? "PROACTIVE" : "OPPORTUNISTIC"
-    minimal_action          = "REPLACE"
-    max_surge_fixed         = 1
-    max_surge_percent       = null
-    max_unavailable_fixed   = 1
-    max_unavailable_percent = null
-    replacement_method      = "SUBSTITUTE"
+    type                         = var.environment == "dev" ? "PROACTIVE" : "OPPORTUNISTIC"
+    minimal_action               = "REPLACE"
+    max_surge_fixed              = 10
+    max_surge_percent            = null
+    max_unavailable_fixed        = 5
+    max_unavailable_percent      = null
+    replacement_method           = "SUBSTITUTE"
+    instance_redistribution_type = "NONE"
   }
 
-  base_instance_name = local.build_pool_name
-  target_size        = var.build_cluster_size
+  base_instance_name = local.client_pool_name_c4
   target_pools       = []
 
   depends_on = [
-    google_compute_instance_template.build,
+    google_compute_instance_template.client_c4,
   ]
 }
 
-data "google_compute_image" "build_source_image" {
-  family = var.build_image_family
-}
 
-resource "google_compute_instance_template" "build" {
-  name_prefix = "${local.build_pool_name}-"
+resource "google_compute_instance_template" "client_c4" {
+  name_prefix = "${local.client_pool_name_c4}-c4-"
 
   instance_description = null
   machine_type         = "c4-standard-8"
@@ -101,22 +118,21 @@ resource "google_compute_instance_template" "build" {
     } : {})
   )
   tags                    = [var.cluster_tag_name]
-  metadata_startup_script = local.build_startup_script
-  metadata = merge(
-    {
-      enable-osconfig         = "TRUE",
-      enable-guest-attributes = "TRUE",
-    },
-  )
+  metadata_startup_script = local.client_startup_script_c4
+  metadata = {
+    enable-osconfig         = "TRUE",
+    enable-guest-attributes = "TRUE",
+  }
 
   scheduling {
     on_host_maintenance = "MIGRATE"
   }
 
   disk {
+    auto_delete  = true
     boot         = true
-    source_image = data.google_compute_image.build_source_image.id
-    disk_size_gb = var.build_cluster_root_disk_size_gb
+    source_image = data.google_compute_image.client_source_image.id
+    disk_size_gb = 300
     disk_type    = "hyperdisk-balanced"
   }
 
@@ -127,6 +143,7 @@ resource "google_compute_instance_template" "build" {
     disk_size_gb = 300
     disk_type    = "hyperdisk-balanced"
   }
+
 
   network_interface {
     network = var.network_name
@@ -158,6 +175,7 @@ resource "google_compute_instance_template" "build" {
   }
 
   depends_on = [
-    google_storage_bucket_object.setup_config_objects,
+    google_storage_bucket_object.setup_config_objects["scripts/run-nomad.sh"],
+    google_storage_bucket_object.setup_config_objects["scripts/run-consul.sh"]
   ]
 }
---
+++ iac/provider-gcp/nomad-cluster/scripts/start-client-c4.sh
@@ -0,0 +1,249 @@
+#!/usr/bin/env bash
+
+# This script is meant to be run in the User Data of each EC2 Instance while it's booting. The script uses the
+# run-nomad and run-consul scripts to configure and start Nomad and Consul in client mode. Note that this script
+# assumes it's running in an AMI built from the Packer template in examples/nomad-consul-ami/nomad-consul.json.
+
+set -euo pipefail
+
+# Set timestamp format
+PS4='[\D{%Y-%m-%d %H:%M:%S}] '
+# Enable command tracing
+set -x
+
+# Send the log output from this script to user-data.log, syslog, and the console
+# Inspired by https://alestic.com/2010/12/ec2-user-data-output/
+exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&1
+
+# Add cache disk for orchestrator and swapfile
+# TODO: Parametrize this
+DISK="/dev/disk/by-id/google-persistent-disk-1"
+MOUNT_POINT="/orchestrator"
+
+# Step 1: Format the disk with XFS and 65K block size
+sudo mkfs.xfs -f -b size=4096 $DISK
+
+# Step 2: Create the mount point
+sudo mkdir -p $MOUNT_POINT
+
+# Step 3: Mount the disk with
+sudo mount -o noatime $DISK $MOUNT_POINT
+
+sudo mkdir -p /orchestrator/sandbox
+sudo mkdir -p /orchestrator/template
+sudo mkdir -p /orchestrator/build
+
+# Add swapfile
+SWAPFILE="/swapfile"
+sudo fallocate -l 100G $SWAPFILE
+sudo chmod 600 $SWAPFILE
+sudo mkswap $SWAPFILE
+sudo swapon $SWAPFILE
+
+# Make swapfile persistent
+echo "$SWAPFILE none swap sw 0 0" | sudo tee -a /etc/fstab
+
+# Set swap settings
+sudo sysctl vm.swappiness=10
+sudo sysctl vm.vfs_cache_pressure=50
+
+# TODO: Optimize the mount more according to https://cloud.google.com/filestore/docs/mounting-fileshares
+%{ if USE_FILESTORE_CACHE }
+# Mount NFS
+sudo mkdir -p "${NFS_MOUNT_PATH}"
+echo "${NFS_IP_ADDRESS}:/store ${NFS_MOUNT_PATH} nfs ${NFS_MOUNT_OPTS} 0 0" | sudo tee -a /etc/fstab
+sudo mount "${NFS_MOUNT_PATH}"
+sudo mkdir -p "${NFS_MOUNT_PATH}/${NFS_MOUNT_SUBDIR}" && chmod +w "${NFS_MOUNT_PATH}/${NFS_MOUNT_SUBDIR}"
+%{ endif }
+
+# Add tmpfs for snapshotting
+# TODO: Parametrize this
+sudo mkdir -p /mnt/snapshot-cache
+sudo mount -t tmpfs -o size=65G tmpfs /mnt/snapshot-cache
+
+ulimit -n 1048576
+export GOMAXPROCS='nproc'
+
+sudo tee -a /etc/sysctl.conf <<EOF
+# Increase the maximum number of socket connections
+net.core.somaxconn = 65535
+
+# Increase the maximum number of backlogged connections
+net.core.netdev_max_backlog = 65535
+
+# Increase maximum number of TCP sockets
+net.ipv4.tcp_max_syn_backlog = 65535
+
+# Increase the maximum number of memory map areas
+vm.max_map_count=1048576
+
+EOF
+sudo sysctl -p
+
+echo "Disabling inotify for NBD devices"
+# https://lore.kernel.org/lkml/20220422054224.19527-1-matthew.ruffell@canonical.com/
+cat <<EOH >/etc/udev/rules.d/97-nbd-device.rules
+# Disable inotify watching of change events for NBD devices
+ACTION=="add|change", KERNEL=="nbd*", OPTIONS:="nowatch"
+EOH
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+# Load the nbd module with 4096 devices
+sudo modprobe nbd nbds_max=4096
+
+# Create the directory for the fc mounts
+mkdir -p /fc-vm
+
+# Create the config file for gcsfuse
+fuse_cache="/fuse/cache"
+mkdir -p $fuse_cache
+
+fuse_config="/fuse/config.yaml"
+
+cat >$fuse_config <<EOF
+file-cache:
+  max-size-mb: -1
+  cache-file-for-range-read: false
+
+metadata-cache:
+  ttl-secs: -1
+
+cache-dir: $fuse_cache
+EOF
+
+# Mount envd buckets
+envd_dir="/fc-envd"
+mkdir -p $envd_dir
+gcsfuse -o=allow_other,ro --file-mode 755 --implicit-dirs "${FC_ENV_PIPELINE_BUCKET_NAME}" $envd_dir
+
+# Mount kernels
+kernels_dir="/fc-kernels"
+mkdir -p $kernels_dir
+gcsfuse -o=allow_other,ro --file-mode 755 --config-file $fuse_config --implicit-dirs "${FC_KERNELS_BUCKET_NAME}" $kernels_dir
+
+# Mount FC versions
+fc_versions_dir="/fc-versions"
+mkdir -p $fc_versions_dir
+gcsfuse -o=allow_other,ro --file-mode 755 --config-file $fuse_config --implicit-dirs "${FC_VERSIONS_BUCKET_NAME}" $fc_versions_dir
+
+# These variables are passed in via Terraform template interpolation
+gsutil cp "gs://${SCRIPTS_BUCKET}/run-consul-${RUN_CONSUL_FILE_HASH}.sh" /opt/consul/bin/run-consul.sh
+gsutil cp "gs://${SCRIPTS_BUCKET}/run-nomad-${RUN_NOMAD_FILE_HASH}.sh" /opt/nomad/bin/run-nomad.sh
+
+chmod +x /opt/consul/bin/run-consul.sh /opt/nomad/bin/run-nomad.sh
+
+mkdir -p /root/docker
+touch /root/docker/config.json
+cat <<EOF >/root/docker/config.json
+{
+    "auths": {
+        "${GCP_REGION}-docker.pkg.dev": {
+            "username": "_json_key_base64",
+            "password": "${GOOGLE_SERVICE_ACCOUNT_KEY}",
+            "server_address": "https://${GCP_REGION}-docker.pkg.dev"
+        }
+    }
+}
+EOF
+
+mkdir -p /etc/systemd/resolved.conf.d/
+touch /etc/systemd/resolved.conf.d/consul.conf
+cat <<EOF >/etc/systemd/resolved.conf.d/consul.conf
+[Resolve]
+DNS=127.0.0.1:8600
+DNSSEC=false
+Domains=~consul
+EOF
+systemctl restart systemd-resolved
+
+# Set up huge pages
+# We are not enabling Transparent Huge Pages for now, as they are not swappable and may result in slowdowns + we are not using swap right now.
+# The THP are by default set to madvise
+# We are allocating the hugepages at the start when the memory is not fragmented yet
+echo "[Setting up huge pages]"
+sudo mkdir -p /mnt/hugepages
+mount -t hugetlbfs none /mnt/hugepages
+# Increase proactive compaction to reduce memory fragmentation for using overcomitted huge pages
+
+available_ram=$(grep MemTotal /proc/meminfo | awk '{print $2}') # in KiB
+available_ram=$(($available_ram / 1024))                        # in MiB
+echo "- Total memory: $available_ram MiB"
+
+min_normal_ram=$((4 * 1024))                             # 4 GiB
+min_normal_percentage_ram=$(($available_ram * 16 / 100)) # 16% of the total memory
+max_normal_ram=$((42 * 1024))                            # 42 GiB
+
+max() {
+    if (($1 > $2)); then
+        echo "$1"
+    else
+        echo "$2"
+    fi
+}
+
+min() {
+    if (($1 < $2)); then
+        echo "$1"
+    else
+        echo "$2"
+    fi
+}
+
+ensure_even() {
+    if (($1 % 2 == 0)); then
+        echo "$1"
+    else
+        echo $(($1 - 1))
+    fi
+}
+
+remove_decimal() {
+    echo "$(echo $1 | sed 's/\..*//')"
+}
+
+reserved_normal_ram=$(max $min_normal_ram $min_normal_percentage_ram)
+reserved_normal_ram=$(min $reserved_normal_ram $max_normal_ram)
+echo "- Reserved RAM: $reserved_normal_ram MiB"
+
+# The huge pages RAM should still be usable for normal pages in most cases.
+hugepages_ram=$(($available_ram - $reserved_normal_ram))
+hugepages_ram=$(remove_decimal $hugepages_ram)
+hugepages_ram=$(ensure_even $hugepages_ram)
+echo "- RAM for hugepages: $hugepages_ram MiB"
+
+hugepage_size_in_mib=2
+echo "- Huge page size: $hugepage_size_in_mib MiB"
+hugepages=$(($hugepages_ram / $hugepage_size_in_mib))
+
+# This percentage will be permanently allocated for huge pages and in monitoring it will be shown as used.
+base_hugepages_percentage=${BASE_HUGEPAGES_PERCENTAGE}
+base_hugepages=$(($hugepages * $base_hugepages_percentage / 100))
+base_hugepages=$(remove_decimal $base_hugepages)
+echo "- Allocating $base_hugepages huge pages ($base_hugepages_percentage%) for base usage"
+echo $base_hugepages >/proc/sys/vm/nr_hugepages
+
+overcommitment_hugepages_percentage=$((100 - $base_hugepages_percentage))
+overcommitment_hugepages=$(($hugepages * $overcommitment_hugepages_percentage / 100))
+overcommitment_hugepages=$(remove_decimal $overcommitment_hugepages)
+echo "- Allocating $overcommitment_hugepages huge pages ($overcommitment_hugepages_percentage%) for overcommitment"
+echo $overcommitment_hugepages >/proc/sys/vm/nr_overcommit_hugepages
+
+# These variables are passed in via Terraform template interpolation
+/opt/consul/bin/run-consul.sh --client \
+    --consul-token "${CONSUL_TOKEN}" \
+    --cluster-tag-name "${CLUSTER_TAG_NAME}" \
+    --enable-gossip-encryption \
+    --gossip-encryption-key "${CONSUL_GOSSIP_ENCRYPTION_KEY}" \
+    --dns-request-token "${CONSUL_DNS_REQUEST_TOKEN}" &
+
+/opt/nomad/bin/run-nomad.sh --client --consul-token "${CONSUL_TOKEN}" --node-pool "${NODE_POOL}" &
+
+# Add alias for ssh-ing to sbx
+echo '_sbx_ssh() {
+  local address=$(dig @127.0.0.4 $1. A +short 2>/dev/null)
+  ssh -o StrictHostKeyChecking=accept-new "root@$address"
+}
+
+alias sbx-ssh=_sbx_ssh' >>/etc/profile
