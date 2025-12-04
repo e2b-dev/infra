0bdffa73723650230cf877dcf1ce8130f49cdac6
Joe Lombrozo <joe.lombrozo@e2b.dev>
2025-12-04 15:33:47 -0800

    add a test to ensure that the test is valid

--- packages/envd/internal/services/process/handler/cgroup_test.go
+++ packages/envd/internal/services/process/handler/cgroup_test.go
@@ -18,62 +18,88 @@ const oneByte = 1
 const kilobyte = 1024 * oneByte
 
 func TestCgroupRoundTrip(t *testing.T) {
+	t.Parallel()
+
 	if os.Geteuid() != 0 {
 		t.Skip("must run as root")
 		return
 	}
 
 	maxMemory := int64(100 * kilobyte)
-	maxTimeout := time.Second * 10
+	maxTimeout := time.Second * 5
 
-	// find current process' cgroup
-	cgroupOfParent := "envdcommands.slice" //getTestCgroupName(t)
+	t.Run("process does not die without cgroups", func(t *testing.T) {
+		t.Parallel()
 
-	// create a child cgroup with a low memory limit
-	cgroupOfTest := "test-commands.service"
+		// create new child process
+		cmd := startProcess(t)
 
-	// create manager
-	m := NewCGroupManager(cgroupOfParent, cgroupOfTest, &cgroup2.Resources{
-		Memory: &cgroup2.Memory{
-			Swap: utils.ToPtr(int64(0)),
-			High: utils.ToPtr(int64(float64(maxMemory) * 0.8)),
-			Max:  &maxMemory,
-		},
+		// wait for child process to die
+		err := waitForProcess(t, cmd, maxTimeout)
+
+		require.ErrorIs(t, err, context.DeadlineExceeded)
 	})
-	require.NotNil(t, m)
-	require.NotNil(t, m.mgr)
 
-	// create new child process
+	t.Run("process dies with cgroups", func(t *testing.T) {
+		t.Parallel()
+
+		// find current process' cgroup
+		cgroupOfParent := "envdcommands.slice" //getTestCgroupName(t)
+
+		// create a child cgroup with a low memory limit
+		cgroupOfTest := "test-commands.service"
+
+		// create manager
+		m := NewCGroupManager(cgroupOfParent, cgroupOfTest, &cgroup2.Resources{
+			Memory: &cgroup2.Memory{
+				Swap: utils.ToPtr(int64(0)),
+				High: utils.ToPtr(int64(float64(maxMemory) * 0.8)),
+				Max:  &maxMemory,
+			},
+		})
+		require.NotNil(t, m)
+		require.NotNil(t, m.mgr)
+
+		// create new child process
+		cmd := startProcess(t)
+
+		// put child process in the child cgroup
+		err := m.Assign(cmd.Process.Pid)
+		require.NoError(t, err)
+
+		// wait for child process to die
+		err = waitForProcess(t, cmd, maxTimeout)
+
+		// verify process exited correctly
+		var exitErr *exec.ExitError
+		require.ErrorAs(t, err, &exitErr)
+		assert.Equal(t, "signal: killed", exitErr.Error())
+		assert.False(t, exitErr.Exited())
+		assert.False(t, exitErr.Success())
+		assert.Equal(t, -1, exitErr.ExitCode())
+
+		// dig a little deeper
+		ws, ok := exitErr.Sys().(syscall.WaitStatus)
+		require.True(t, ok)
+		assert.Equal(t, syscall.SIGKILL, ws.Signal())
+		assert.Equal(t, true, ws.Signaled())
+		assert.Equal(t, false, ws.Stopped())
+		assert.Equal(t, false, ws.Continued())
+		assert.Equal(t, false, ws.CoreDump())
+		assert.Equal(t, false, ws.Exited())
+		assert.Equal(t, -1, ws.ExitStatus())
+	})
+}
+
+func startProcess(t *testing.T) *exec.Cmd {
+	t.Helper()
+
 	cmdName, args := "bash", []string{"-c", `sleep 1 && tail /dev/zero`}
 	cmd := exec.CommandContext(t.Context(), cmdName, args...)
 	err := cmd.Start()
 	require.NoError(t, err)
 
-	// put child process in the child cgroup
-	err = m.Assign(cmd.Process.Pid)
-	require.NoError(t, err)
-
-	// wait for child process to die
-	err = waitForProcess(t, cmd, maxTimeout)
-
-	// verify process exited correctly
-	var exitErr *exec.ExitError
-	require.ErrorAs(t, err, &exitErr)
-	assert.Equal(t, "signal: killed", exitErr.Error())
-	assert.False(t, exitErr.Exited())
-	assert.False(t, exitErr.Success())
-	assert.Equal(t, -1, exitErr.ExitCode())
-
-	// dig a little deeper
-	ws, ok := exitErr.Sys().(syscall.WaitStatus)
-	require.True(t, ok)
-	assert.Equal(t, syscall.SIGKILL, ws.Signal())
-	assert.Equal(t, true, ws.Signaled())
-	assert.Equal(t, false, ws.Stopped())
-	assert.Equal(t, false, ws.Continued())
-	assert.Equal(t, false, ws.CoreDump())
-	assert.Equal(t, false, ws.Exited())
-	assert.Equal(t, -1, ws.ExitStatus())
+	return cmd
 }
 
 func waitForProcess(t *testing.T, cmd *exec.Cmd, timeout time.Duration) error {
@@ -91,8 +117,7 @@ func waitForProcess(t *testing.T, cmd *exec.Cmd, timeout time.Duration) error {
 
 	select {
 	case <-ctx.Done():
-		require.Fail(t, "process did not die on its own")
-		return nil
+		return ctx.Err()
 	case err := <-done:
 		return err
 	}
