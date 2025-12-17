a92c961c5fbbd3252b5a7f0dfdf92d986b27c524
Jakub Novak <jakub@e2b.dev>
2025-12-17 10:53:43 +0100

    Add comment

--- packages/api/internal/sandbox/storage/memory/operations.go
+++ packages/api/internal/sandbox/storage/memory/operations.go
@@ -17,6 +17,7 @@ import (
 func (s *Storage) Add(ctx context.Context, sbx sandbox.Sandbox) error {
 	added := s.items.SetIfAbsent(sbx.SandboxID, newMemorySandbox(sbx))
 	if !added {
+		// There's a race condition when the sandbox is added from sync and then the sandbox create fails
 		logger.L().Warn(ctx, "Sandbox already exists in cache", logger.WithSandboxID(sbx.SandboxID))
 	}
 
