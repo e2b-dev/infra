7608780cce993993ce8e1f0c0dbc16a4fc7a1ecf
ValentaTomas <valenta.and.thomas@gmail.com>
2026-01-12 06:53:09 -0800

    Fix lint

--- packages/shared/pkg/utils/promise_test.go
+++ packages/shared/pkg/utils/promise_test.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPromiseSuccess(t *testing.T) {
@@ -18,7 +19,7 @@ func TestPromiseSuccess(t *testing.T) {
 	})
 
 	value, err := p.Wait(context.Background())
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, 42, value)
 }
 
@@ -31,7 +32,7 @@ func TestPromiseError(t *testing.T) {
 	})
 
 	value, err := p.Wait(context.Background())
-	assert.ErrorIs(t, err, expectedErr)
+	require.ErrorIs(t, err, expectedErr)
 	assert.Equal(t, 0, value)
 }
 
@@ -40,11 +41,12 @@ func TestPromiseDelayedResult(t *testing.T) {
 
 	p := NewPromise(func() (string, error) {
 		time.Sleep(50 * time.Millisecond)
+
 		return "delayed", nil
 	})
 
 	value, err := p.Wait(context.Background())
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, "delayed", value)
 }
 
@@ -53,6 +55,7 @@ func TestPromiseContextCancelled(t *testing.T) {
 
 	p := NewPromise(func() (int, error) {
 		time.Sleep(1 * time.Second)
+
 		return 42, nil
 	})
 
@@ -68,6 +71,7 @@ func TestPromiseMultipleWaiters(t *testing.T) {
 
 	p := NewPromise(func() (int, error) {
 		time.Sleep(50 * time.Millisecond)
+
 		return 42, nil
 	})
 
@@ -75,7 +79,7 @@ func TestPromiseMultipleWaiters(t *testing.T) {
 	results := make([]int, 5)
 	errs := make([]error, 5)
 
-	for i := 0; i < 5; i++ {
+	for i := range 5 {
 		wg.Add(1)
 		go func(idx int) {
 			defer wg.Done()
@@ -85,8 +89,8 @@ func TestPromiseMultipleWaiters(t *testing.T) {
 
 	wg.Wait()
 
-	for i := 0; i < 5; i++ {
-		assert.NoError(t, errs[i])
+	for i := range 5 {
+		require.NoError(t, errs[i])
 		assert.Equal(t, 42, results[i])
 	}
 }
@@ -96,6 +100,7 @@ func TestPromiseDone(t *testing.T) {
 
 	p := NewPromise(func() (int, error) {
 		time.Sleep(50 * time.Millisecond)
+
 		return 42, nil
 	})
 
@@ -109,7 +114,7 @@ func TestPromiseDone(t *testing.T) {
 	<-p.Done()
 
 	value, err := p.Result()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, 42, value)
 }
 
@@ -118,6 +123,7 @@ func TestPromiseResultBeforeResolve(t *testing.T) {
 
 	p := NewPromise(func() (int, error) {
 		time.Sleep(100 * time.Millisecond)
+
 		return 42, nil
 	})
 
@@ -135,11 +141,11 @@ func TestPromiseResultAfterResolve(t *testing.T) {
 	<-p.Done()
 
 	value, err := p.Result()
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	assert.Equal(t, 42, value)
 
 	// Multiple calls should return the same result
 	value2, err2 := p.Result()
-	assert.NoError(t, err2)
+	require.NoError(t, err2)
 	assert.Equal(t, 42, value2)
 }
