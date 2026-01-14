724596aea4cafcc5c6caa73446c2d6f1c7d9e22c
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-01-13 16:13:18 -0800

    fix benchmark test

--- packages/orchestrator/benchmark_test.go
+++ packages/orchestrator/benchmark_test.go
@@ -6,6 +6,7 @@
 package main
 
 import (
+	"context"
 	"net/http"
 	"net/url"
 	"os"
@@ -27,6 +28,7 @@ import (
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/nbd"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/network"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/sandbox/template"
+	"github.com/e2b-dev/infra/packages/orchestrator/internal/tcpfirewall"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/build"
 	buildconfig "github.com/e2b-dev/infra/packages/orchestrator/internal/template/build/config"
 	"github.com/e2b-dev/infra/packages/orchestrator/internal/template/build/metrics"
@@ -83,10 +85,12 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 		spanExporter, err := telemetry.NewSpanExporter(b.Context(),
 			otlptracegrpc.WithEndpoint(endpoint),
 		)
-		defer func() {
-			err := spanExporter.Shutdown(b.Context())
+		b.Cleanup(func() {
+			ctx := context.WithoutCancel(b.Context())
+			err := spanExporter.Shutdown(ctx)
 			assert.NoError(b, err)
-		}()
+		})
+
 		require.NoError(b, err)
 		resource, err := telemetry.GetResource(b.Context(), "node-id", "BenchmarkBaseImageLaunch", "service-commit", "service-version", "service-instance-id")
 		require.NoError(b, err)
@@ -135,10 +139,11 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 		networkPool.Populate(b.Context())
 		l.Info(b.Context(), "network pool populated")
 	}()
-	defer func() {
-		err := networkPool.Close(b.Context())
+	b.Cleanup(func() {
+		ctx := context.WithoutCancel(b.Context())
+		err := networkPool.Close(ctx)
 		assert.NoError(b, err)
-	}()
+	})
 
 	devicePool, err := nbd.NewDevicePool()
 	require.NoError(b, err, "do you have the nbd kernel module installed?")
@@ -146,17 +151,19 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 		devicePool.Populate(b.Context())
 		l.Info(b.Context(), "device pool populated")
 	}()
-	defer func() {
-		err := devicePool.Close(b.Context())
+	b.Cleanup(func() {
+		ctx := context.WithoutCancel(b.Context())
+		err := devicePool.Close(ctx)
 		assert.NoError(b, err)
-	}()
+	})
 
 	featureFlags, err := featureflags.NewClient()
 	require.NoError(b, err)
-	defer func() {
-		err := featureFlags.Close(b.Context())
+	b.Cleanup(func() {
+		ctx := context.WithoutCancel(b.Context())
+		err := featureFlags.Close(ctx)
 		assert.NoError(b, err)
-	}()
+	})
 
 	limiter, err := limit.New(b.Context(), featureFlags)
 	require.NoError(b, err)
@@ -181,10 +188,10 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 
 	dockerhubRepository, err := dockerhub.GetRemoteRepository(b.Context())
 	require.NoError(b, err)
-	defer func() {
+	b.Cleanup(func() {
 		err := dockerhubRepository.Close()
 		assert.NoError(b, err)
-	}()
+	})
 
 	accessToken := "access-token"
 	sandboxConfig := sandbox.Config{
@@ -225,16 +232,33 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 
 	sandboxes := sandbox.NewSandboxesMap()
 
+	tcpFirewall := tcpfirewall.New(
+		l,
+		config.NetworkConfig,
+		sandboxes,
+		noop.NewMeterProvider(),
+	)
+	go func() {
+		err := tcpFirewall.Start(b.Context())
+		assert.NoError(b, err)
+	}()
+	b.Cleanup(func() {
+		ctx := context.WithoutCancel(b.Context())
+		err := tcpFirewall.Close(ctx)
+		assert.NoError(b, err)
+	})
+
 	sandboxProxy, err := proxy.NewSandboxProxy(noop.MeterProvider{}, proxyPort, sandboxes)
 	require.NoError(b, err)
 	go func() {
 		err := sandboxProxy.Start(b.Context())
 		assert.ErrorIs(b, http.ErrServerClosed, err)
 	}()
-	defer func() {
-		err := sandboxProxy.Close(b.Context())
+	b.Cleanup(func() {
+		ctx := context.WithoutCancel(b.Context())
+		err := sandboxProxy.Close(ctx)
 		assert.NoError(b, err)
-	}()
+	})
 
 	buildMetrics, err := metrics.NewBuildMetrics(noop.MeterProvider{})
 	require.NoError(b, err)
@@ -265,7 +289,7 @@ func BenchmarkBaseImageLaunch(b *testing.B) {
 			Force:              &force,
 			VCpuCount:          sandboxConfig.Vcpu,
 			MemoryMB:           sandboxConfig.RamMB,
-			StartCmd:           "echo 'start cmd debug' && sleep 10 && echo 'done starting command debug'",
+			StartCmd:           "echo 'start cmd debug' && sleep .1 && echo 'done starting command debug'",
 			DiskSizeMB:         sandboxConfig.TotalDiskSizeMB,
 			HugePages:          sandboxConfig.HugePages,
 			KernelVersion:      kernelVersion,
