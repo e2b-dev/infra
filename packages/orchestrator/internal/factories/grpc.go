945bf6fa25862f8b5a4b06c998f02ee3ac71a0ff
Jiri Sveceny <jiri.sveceny@icloud.com>
2026-01-26 13:47:28 +0100

    Make compatible with older api deployment

--- packages/orchestrator/internal/factories/grpc.go
+++ packages/orchestrator/internal/factories/grpc.go
@@ -31,7 +31,7 @@ func NewGRPCServer(tel *telemetry.Client) *grpc.Server {
 
 	srv := grpc.NewServer(
 		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
-			MinTime:             15 * time.Second, // Minimum time between pings from client
+			MinTime:             5 * time.Second, // Minimum time between pings from client
 			PermitWithoutStream: true,             // Allow pings even when no active streams
 		}),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
