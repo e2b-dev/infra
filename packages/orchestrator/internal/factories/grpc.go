d97e48408a60046360ea4b9794fc22d4e6039d68
Jiri Sveceny <jiri.sveceny@icloud.com>
2026-01-26 14:02:48 +0100

    Format

--- packages/orchestrator/internal/factories/grpc.go
+++ packages/orchestrator/internal/factories/grpc.go
@@ -32,7 +32,7 @@ func NewGRPCServer(tel *telemetry.Client) *grpc.Server {
 	srv := grpc.NewServer(
 		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
 			MinTime:             5 * time.Second, // Minimum time between pings from client
-			PermitWithoutStream: true,             // Allow pings even when no active streams
+			PermitWithoutStream: true,            // Allow pings even when no active streams
 		}),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
 			Time:    15 * time.Second, // Server sends keepalive pings every 15s
