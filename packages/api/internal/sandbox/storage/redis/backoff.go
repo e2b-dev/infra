6ffef28de9eb9724a2e1a4b96d14f463fbd6bd35
Jakub Novak <jakub@e2b.dev>
2026-01-27 16:10:04 +0100

    Increase the factor

--- packages/api/internal/sandbox/storage/redis/backoff.go
+++ packages/api/internal/sandbox/storage/redis/backoff.go
@@ -16,7 +16,7 @@ func NewConstantBackoff(backoff time.Duration) *ConstantBackoff {
 const jitter = 0.25 // ±25%
 
 func (b *ConstantBackoff) NextBackoff() time.Duration {
-	factor := 1 + jitter*(rand.Float64()-0.5)
+	factor := 1 + jitter*(2*(rand.Float64()-0.5))
 
 	return time.Duration(float64(b.backoff) * factor)
 }
