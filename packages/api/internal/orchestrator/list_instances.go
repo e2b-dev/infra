452f8aea701a4c31c0205a27dec6eb2bedc0192a
Jakub Novak <jakub@e2b.dev>
2026-01-28 15:36:39 +0100

    Fix comment

--- packages/api/internal/orchestrator/list_instances.go
+++ packages/api/internal/orchestrator/list_instances.go
@@ -8,7 +8,7 @@ import (
 	"github.com/e2b-dev/infra/packages/api/internal/sandbox"
 )
 
-// GetSandboxes returns all instances for a given node.
+// GetSandboxes returns all instances for a given team.
 func (o *Orchestrator) GetSandboxes(ctx context.Context, teamID uuid.UUID, states []sandbox.State) ([]sandbox.Sandbox, error) {
 	_, childSpan := tracer.Start(ctx, "get-sandboxes")
 	defer childSpan.End()
