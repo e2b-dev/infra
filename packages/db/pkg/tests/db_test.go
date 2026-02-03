aa95efd152a000743207884fd4ef2422b1383ca3
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-02-03 12:05:45 -0800

    simplify the test

--- packages/db/pkg/tests/db_test.go
+++ packages/db/pkg/tests/db_test.go
@@ -18,22 +18,15 @@ func TestRequireRowLevelSecurity(t *testing.T) {
 	require.NoError(t, err)
 	assert.NotEmpty(t, results)
 
+	var checked int
 	for _, item := range results {
-		if item.NamespaceName == "pg_catalog" {
-			// built ins don't need security
+		if item.NamespaceName != "public" {
+			// only the "public" namespace requires row level security
 			continue
 		}
 
-		if item.NamespaceName == "information_schema" {
-			// information schema doesn't have row level security
-			continue
-		}
-
-		if item.NamespaceName == "auth" && item.TableName == "users" {
-			// this table does not need row level security
-			continue
-		}
-
+		checked++
 		assert.Truef(t, item.RowLevelSecurity, "row level security not enabled for %s.%s", item.NamespaceName, item.TableName)
 	}
+	assert.NotEmpty(t, checked)
 }
