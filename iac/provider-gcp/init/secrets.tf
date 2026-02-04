90a21f5fc35a1347e614fb7baa876392a9393b38
Jakub Novak <jakub@e2b.dev>
2026-02-04 15:40:25 -0800

    fix:secret api wait interval

--- iac/provider-gcp/init/secrets.tf
+++ iac/provider-gcp/init/secrets.tf
@@ -1,3 +1,155 @@
+
+resource "google_secret_manager_secret" "cloudflare_api_token" {
+  secret_id = "${var.prefix}cloudflare-api-token"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret" "consul_acl_token" {
+  secret_id = "${var.prefix}consul-secret-id"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "random_uuid" "consul_acl_token" {}
+
+resource "google_secret_manager_secret_version" "consul_acl_token" {
+  secret      = google_secret_manager_secret.consul_acl_token.name
+  secret_data = random_uuid.consul_acl_token.result
+}
+
+resource "google_secret_manager_secret" "nomad_acl_token" {
+  secret_id = "${var.prefix}nomad-secret-id"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "random_uuid" "nomad_acl_token" {}
+
+resource "google_secret_manager_secret_version" "nomad_acl_token" {
+  secret      = google_secret_manager_secret.nomad_acl_token.name
+  secret_data = random_uuid.nomad_acl_token.result
+}
+
+
+
+# grafana api key
+resource "google_secret_manager_secret" "grafana_api_key" {
+  secret_id = "${var.prefix}grafana-api-key"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret" "launch_darkly_api_key" {
+  secret_id = "${var.prefix}launch-darkly-api-key"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret_version" "launch_darkly_api_key" {
+  secret      = google_secret_manager_secret.launch_darkly_api_key.name
+  secret_data = " "
+
+  lifecycle {
+    ignore_changes = [secret_data]
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret_version" "grafana_api_key" {
+  secret      = google_secret_manager_secret.grafana_api_key.name
+  secret_data = " "
+
+  lifecycle {
+    ignore_changes = [secret_data]
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+resource "google_secret_manager_secret" "analytics_collector_host" {
+  secret_id = "${var.prefix}analytics-collector-host"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret_version" "analytics_collector_host" {
+  secret      = google_secret_manager_secret.analytics_collector_host.name
+  secret_data = " "
+
+  lifecycle {
+    ignore_changes = [secret_data]
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret" "analytics_collector_api_token" {
+  secret_id = "${var.prefix}analytics-collector-api-token"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret_version" "analytics_collector_api_token" {
+  secret      = google_secret_manager_secret.analytics_collector_api_token.name
+  secret_data = " "
+
+  lifecycle {
+    ignore_changes = [secret_data]
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret" "routing_domains" {
+  secret_id = "${var.prefix}routing-domains"
+
+  replication {
+    auto {}
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
+resource "google_secret_manager_secret_version" "routing_domains" {
+  secret      = google_secret_manager_secret.routing_domains.name
+  secret_data = jsonencode([])
+
+  lifecycle {
+    ignore_changes = [secret_data]
+  }
+
+  depends_on = [time_sleep.secrets_api_wait_60_seconds]
+}
+
 resource "google_secret_manager_secret" "postgres_connection_string" {
   secret_id = "${var.prefix}postgres-connection-string"
 
