b7cd4c413560fe76e646b5ef70d87a0f4a4fb6a0
Matt Brockman <matt.brockman@e2b.dev>
2026-02-04 11:33:17 -0800

    cleanup: rm testing darkly debug

--- packages/client-proxy/main.go
+++ packages/client-proxy/main.go
@@ -9,7 +9,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"sync"
 	"sync/atomic"
 	"syscall"
@@ -147,12 +146,6 @@ func run() int {
 	if !config.AutoResumeEnabled {
 		autoResumeEnabled = false
 	}
-	launchDarklyEnabled := strings.TrimSpace(os.Getenv("LAUNCH_DARKLY_API_KEY")) != ""
-	l.Info(ctx, "auto-resume configuration",
-		zap.Bool("auto_resume_enabled", autoResumeEnabled),
-		zap.Bool("auto_resume_env_enabled", config.AutoResumeEnabled),
-		zap.Bool("launch_darkly_enabled", launchDarklyEnabled),
-	)
 
 	trafficProxy, err := e2bproxy.NewClientProxyWithPausedChecker(
 		tel.MeterProvider,
