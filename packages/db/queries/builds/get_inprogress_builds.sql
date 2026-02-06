f5d84dece25a406d5778792405eec35381ad7233
Jakub Dobry <jakub.dobry8@gmail.com>
2026-02-06 01:34:21 +0100

    sql: filter in-progress builds by template source

--- packages/db/queries/builds/get_inprogress_builds.sql
+++ packages/db/queries/builds/get_inprogress_builds.sql
@@ -5,5 +5,6 @@ JOIN public.env_build_assignments eba ON eba.build_id = b.id
 JOIN public.envs e ON e.id = eba.env_id
 JOIN public.teams t ON e.team_id = t.id
 WHERE b.status IN ('waiting', 'building', 'snapshotting', 'pending', 'in_progress')
+  AND e.source = 'template'
 ORDER BY b.id, b.created_at DESC;
 
