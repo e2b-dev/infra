ce437d99be4fe4ef26f9521cddb5f9ebdb4172f0
Jakub Dobry <jakub.dobry8@gmail.com>
2026-02-17 21:50:23 -0800

    linter

--- packages/orchestrator/internal/nfsproxy/proxy.go
+++ packages/orchestrator/internal/nfsproxy/proxy.go
@@ -35,7 +35,7 @@ var (
 )
 
 func getPrefixFromSandbox(sandboxes *sandbox.Map, filesystemsByType map[string]billy.Filesystem) jailed.GetPrefix {
-	return func(ctx context.Context, remoteAddr net.Addr, request nfs.MountRequest) (billy.Filesystem, string, error) {
+	return func(_ context.Context, remoteAddr net.Addr, request nfs.MountRequest) (billy.Filesystem, string, error) {
 		sbx, err := sandboxes.GetByHostPort(remoteAddr.String())
 		if err != nil {
 			return nil, "", err
