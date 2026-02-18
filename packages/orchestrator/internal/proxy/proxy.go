24f998892d1f83e8c194eadd60f32a933a27495e
Matt Brockman <matt.brockman@e2b.dev>
2026-02-17 20:30:02 -0800

    token comparison uses ConstantTimeCompare

--- packages/orchestrator/internal/proxy/proxy.go
+++ packages/orchestrator/internal/proxy/proxy.go
@@ -2,6 +2,7 @@ package proxy
 
 import (
 	"context"
+	"crypto/subtle"
 	"fmt"
 	"net/http"
 	"net/url"
@@ -85,7 +86,7 @@ func NewSandboxProxy(meterProvider metric.MeterProvider, port uint16, sandboxes 
 				accessTokenRaw := r.Header.Get(trafficAccessTokenHeader)
 				if accessTokenRaw == "" {
 					return nil, reverseproxy.NewErrMissingTrafficAccessToken(sandboxId, trafficAccessTokenHeader)
-				} else if accessTokenRaw != *accessToken {
+				} else if subtle.ConstantTimeCompare([]byte(accessTokenRaw), []byte(*accessToken)) != 1 {
 					return nil, reverseproxy.NewErrInvalidTrafficAccessToken(sandboxId, trafficAccessTokenHeader)
 				}
 			}
