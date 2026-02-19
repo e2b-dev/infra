4e8eeedb6a42832df286f8f7b873a4a53c9f9392
Tomas Valenta <valenta.and.thomas@gmail.com>
2026-02-18 21:18:13 -0800

    Add field explanation

--- iac/provider-gcp/init/buckets.tf
+++ iac/provider-gcp/init/buckets.tf
@@ -134,8 +134,9 @@ resource "google_storage_bucket" "fc_template_bucket" {
       # Delete non-current versions after 2 days.
       # This triggers the soft-delete policy, so objects are then deleted after another 10 days.
       days_since_noncurrent_time = 2
-      send_age_if_zero           = false
-      with_state                 = "ARCHIVED"
+      # Added because of https://github.com/hashicorp/terraform-provider-google/issues/17990
+      send_age_if_zero = false
+      with_state       = "ARCHIVED"
     }
   }
 
