71f7219995652e5f324263fe71707395b699c1de
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-02-23 12:06:19 -0800

    code review

--- packages/api/internal/handlers/me.go
+++ packages/api/internal/handlers/me.go
@@ -7,21 +7,19 @@ import (
 	"github.com/google/uuid"
 
 	"github.com/e2b-dev/infra/packages/api/internal/api"
-	"github.com/e2b-dev/infra/packages/api/internal/auth"
-	"github.com/e2b-dev/infra/packages/auth/pkg/types"
 )
 
 func (a *APIStore) GetMe(c *gin.Context) {
 	var tokenInfo api.TokenInfo
 
-	teamInfo, teamOK := c.Value(auth.TeamContextKey).(*types.Team)
-	userID, userOK := c.Value(auth.UserIDContextKey).(uuid.UUID)
+	teamInfo := a.GetTeamInfo(c)
+	userID := a.GetUserID(c)
 
 	switch {
-	case teamOK && teamInfo != nil:
+	case teamInfo != nil:
 		tokenInfo.TeamID = &teamInfo.ID
 		tokenInfo.TeamName = &teamInfo.Name
-	case userOK && userID != uuid.Nil:
+	case userID != uuid.Nil:
 		tokenInfo.UserID = &userID
 	default:
 		a.sendAPIStoreError(c, http.StatusUnauthorized, "no credentials found")
