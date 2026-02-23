adb39c1c6e76968f4395a9e1edfe9942b0bfa741
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-02-23 11:17:06 -0800

    clean up code

--- packages/api/internal/handlers/me.go
+++ packages/api/internal/handlers/me.go
@@ -1,33 +1,32 @@
 package handlers
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+
 	"github.com/e2b-dev/infra/packages/api/internal/api"
 	"github.com/e2b-dev/infra/packages/api/internal/auth"
 	"github.com/e2b-dev/infra/packages/auth/pkg/types"
-	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func (a *APIStore) GetMe(c *gin.Context) {
-	ctx := c.Request.Context()
-
 	var tokenInfo api.TokenInfo
 
 	teamInfo, teamOK := c.Value(auth.TeamContextKey).(*types.Team)
 	userID, userOK := c.Value(auth.UserIDContextKey).(uuid.UUID)
 
-	if !(teamOK || userOK) {
-		c.JSON(401, gin.H{"error": "Unauthorized"})
-		return
-	}
-
-	if teamOK && teamInfo != nil {
+	switch {
+	case teamOK && teamInfo != nil:
 		tokenInfo.TeamID = &teamInfo.ID
 		tokenInfo.TeamName = &teamInfo.Name
-	}
-
-	if userOK && userID != uuid.Nil {
+	case userOK && userID != uuid.Nil:
 		tokenInfo.UserID = &userID
+	default:
+		a.sendAPIStoreError(c, http.StatusUnauthorized, "no credentials found")
+
+		return
 	}
 
 	c.JSON(200, tokenInfo)
