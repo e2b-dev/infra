4ecbd1b3fc2ac99ad6fb542d02db5f00d2341c13
Babis Chalios <mail@bchalios.io>
2026-02-25 09:07:03 +0100

    orchestrator: get dirty memory from Firecracker (#1937)
    * feat(fc): use fc versions with /memory/dirty endpoint
    
    Use new v1.10 and v1.12 Firecracker versions with support for getting
    information about dirty memory calling the /memory/dirty endpoint.
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>
    
    * feat(fc): update Firecracker client
    
    Bump it to version v1.12 which now supports the logic for getting dirty
    memory via the /memory/dirty endpoint. It also brings a few new features
    from Firecracker, e.g. NetworkOverrides during loading snapshots.
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>
    
    * feat(fc): switch dirty tracking
    
    Use Firecracker's /memory/dirty endpoint for getting information about
    the guest's dirty memory.
    
    * Hide method
    
    * Reproduce WP bug
    
    * Fix lint
    
    * fix: apply write protection for read page faults
    
    Otherwise calling copy() on the UFFD will clear it. Also fix comment
    regarding the structure of pagemap.
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>
    
    * Improve wp test; Add FC version smoke test
    
    * fix lint
    
    * Fix FC client compatibility
    
    ---------
    
    Signed-off-by: Babis Chalios <babis.chalios@e2b.dev>
    Co-authored-by: ValentaTomas <valenta.and.thomas@gmail.com>

--- packages/shared/pkg/feature-flags/flags.go
+++ packages/shared/pkg/feature-flags/flags.go
@@ -192,12 +192,12 @@ func newStringFlag(name string, fallback string) StringFlag {
 // The Firecracker version the last tag + the short SHA (so we can build our dev previews)
 // TODO: The short tag here has only 7 characters — the one from our build pipeline will likely have exactly 8 so this will break.
 const (
-	DefaultFirecackerV1_10Version = "v1.10.1_fb257a1"
-	DefaultFirecackerV1_12Version = "v1.12.1_717921c"
+	DefaultFirecackerV1_10Version = "v1.10.1_30cbb07"
+	DefaultFirecackerV1_12Version = "v1.12.1_a41d3fb"
 	DefaultFirecrackerVersion     = DefaultFirecackerV1_12Version
 )
 
-var firecrackerVersions = map[string]string{
+var FirecrackerVersionMap = map[string]string{
 	"v1.10": DefaultFirecackerV1_10Version,
 	"v1.12": DefaultFirecackerV1_12Version,
 }
@@ -208,7 +208,7 @@ var (
 	BuildIoEngine               = newStringFlag("build-io-engine", "Sync")
 	DefaultPersistentVolumeType = newStringFlag("default-persistent-volume-type", "")
 	BuildNodeInfo               = newJSONFlag("preferred-build-node", ldvalue.Null())
-	FirecrackerVersions         = newJSONFlag("firecracker-versions", ldvalue.FromJSONMarshal(firecrackerVersions))
+	FirecrackerVersions         = newJSONFlag("firecracker-versions", ldvalue.FromJSONMarshal(FirecrackerVersionMap))
 )
 
 // defaultTrackedTemplates is the default map of template aliases tracked for metrics.
