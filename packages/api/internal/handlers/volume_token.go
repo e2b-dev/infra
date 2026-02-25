1d29627ec6081e0522594cd1db52e0cf83393498
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-02-25 11:56:12 -0800

    fill out more claims

--- packages/api/internal/handlers/volume_token.go
+++ packages/api/internal/handlers/volume_token.go
@@ -4,12 +4,16 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/e2b-dev/infra/packages/shared/pkg/clusters"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 
 	"github.com/e2b-dev/infra/packages/api/internal/api"
 	"github.com/e2b-dev/infra/packages/shared/pkg/telemetry"
 )
+
+const gracePeriod = time.Minute
 
 func (a *APIStore) PostVolumesVolumeIDToken(c *gin.Context, volumeID api.VolumeID) {
 	volume, team, ok := a.getVolume(c, volumeID)
@@ -17,14 +21,26 @@ func (a *APIStore) PostVolumesVolumeIDToken(c *gin.Context, volumeID api.VolumeI
 		return
 	}
 
-	expiration := time.Now().Add(a.config.VolumesToken.Expiration)
+	clusterID := clusters.WithClusterFallback(team.ClusterID)
+
+	now := time.Now()
+	notBefore := now.Add(-1 * gracePeriod)
+	expiration := now.Add(a.config.VolumesToken.Expiration)
 
 	claims := jwt.MapClaims{
-		"iss":     a.config.VolumesToken.Issuer,
-		"exp":     jwt.NewNumericDate(expiration),
+		// registered
+		"aud": clusterID.String(),
+		"exp": jwt.NewNumericDate(expiration),
+		"iat": jwt.NewNumericDate(now),
+		"iss": a.config.VolumesToken.Issuer,
+		"jti": uuid.NewString(),
+		"nbf": jwt.NewNumericDate(notBefore),
+		"sub": team.ID.String(),
+
+		// custom
+		"teamid":  team.ID,
 		"volid":   volume.ID.String(),
 		"voltype": volume.VolumeType,
-		"tid":     team.ID,
 	}
 
 	token := jwt.NewWithClaims(a.config.VolumesToken.SigningMethod, claims)
