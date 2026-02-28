0309d82753662c8206fb56fb82c5cd1523800920
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-02-27 17:50:05 -0800

    write a comment describing how scary this function is

--- packages/orchestrator/internal/nfsproxy/jailed/fs.go
+++ packages/orchestrator/internal/nfsproxy/jailed/fs.go
@@ -14,59 +14,10 @@ type jailedFS struct {
 	inner  billy.Filesystem
 }
 
-func (j jailedFS) String() string {
-	return fmt.Sprintf("jailedFS{prefix=%s, inner=%v}", j.prefix, j.inner)
-}
-
 var _ billy.Filesystem = (*jailedFS)(nil)
 
-func tryWrapFS(fs billy.Filesystem, prefix string) billy.Filesystem {
-	if fs == nil {
-		return nil
-	}
-
-	return jailedFS{
-		prefix: prefix,
-		inner:  fs,
-	}
-}
-
-func (j jailedFS) Unwrap() billy.Filesystem {
-	return j.inner
-}
-
-func (j jailedFS) Create(filename string) (billy.File, error) {
-	f, err := j.inner.Create(filename)
-
-	return tryWrapBillyFile(f, j.prefix), err
-}
-
-func (j jailedFS) Open(filename string) (billy.File, error) {
-	f, err := j.inner.Open(filename)
-
-	return tryWrapBillyFile(f, j.prefix), err
-}
-
-func (j jailedFS) OpenFile(filename string, flag int, perm os.FileMode) (billy.File, error) {
-	file, err := j.inner.OpenFile(filename, flag, perm)
-
-	return tryWrapBillyFile(file, j.prefix), err
-}
-
-func (j jailedFS) Stat(filename string) (os.FileInfo, error) {
-	file, err := j.inner.Stat(filename)
-
-	return tryWrapOSFile(file, j.prefix), err
-}
-
-func (j jailedFS) Rename(oldpath, newpath string) error {
-	return j.inner.Rename(oldpath, newpath)
-}
-
-func (j jailedFS) Remove(filename string) error {
-	return j.inner.Remove(filename)
-}
-
+// Join expands the request into the chrooted filesystem. It is called by willscott/go-nfs
+// on every request, and is responsible for making sure that the request is not escaping into the rest of the OS.
 func (j jailedFS) Join(elem ...string) string {
 	// Start with inner's join and normalize separators and dots.
 	p := j.inner.Join(elem...)
@@ -104,6 +55,46 @@ func (j jailedFS) Join(elem ...string) string {
 	}
 
 	return filepath.Join(j.prefix, s)
+}
+
+func (j jailedFS) Unwrap() billy.Filesystem {
+	return j.inner
+}
+
+func (j jailedFS) Create(filename string) (billy.File, error) {
+	f, err := j.inner.Create(filename)
+
+	return tryWrapBillyFile(f, j.prefix), err
+}
+
+func (j jailedFS) Open(filename string) (billy.File, error) {
+	f, err := j.inner.Open(filename)
+
+	return tryWrapBillyFile(f, j.prefix), err
+}
+
+func (j jailedFS) String() string {
+	return fmt.Sprintf("jailedFS{prefix=%s, inner=%v}", j.prefix, j.inner)
+}
+
+func (j jailedFS) OpenFile(filename string, flag int, perm os.FileMode) (billy.File, error) {
+	file, err := j.inner.OpenFile(filename, flag, perm)
+
+	return tryWrapBillyFile(file, j.prefix), err
+}
+
+func (j jailedFS) Stat(filename string) (os.FileInfo, error) {
+	file, err := j.inner.Stat(filename)
+
+	return tryWrapOSFile(file, j.prefix), err
+}
+
+func (j jailedFS) Rename(oldpath, newpath string) error {
+	return j.inner.Rename(oldpath, newpath)
+}
+
+func (j jailedFS) Remove(filename string) error {
+	return j.inner.Remove(filename)
 }
 
 func (j jailedFS) TempFile(dir, prefix string) (billy.File, error) {
@@ -152,4 +143,15 @@ func (j jailedFS) Chroot(path string) (billy.Filesystem, error) {
 
 func (j jailedFS) Root() string {
 	return j.inner.Root()
+}
+
+func tryWrapFS(fs billy.Filesystem, prefix string) billy.Filesystem {
+	if fs == nil {
+		return nil
+	}
+
+	return jailedFS{
+		prefix: prefix,
+		inner:  fs,
+	}
 }
