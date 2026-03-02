d50eb328d55b7ec11abae6904143c8515a431f5e
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-03-02 14:45:57 -0800

    oops

--- packages/shared/pkg/proxy/proxy_test.go
+++ packages/shared/pkg/proxy/proxy_test.go
@@ -918,12 +918,9 @@ func TestChangeResponseHeader(t *testing.T) {
 	}
 
 	require.NotNil(t, rsp, "response should not be nil")
-	content, err := io.ReadAll(rsp.Body)
-	require.NoError(t, err)
-	require.Equal(t, 200, rsp.StatusCode, string(content))
-
 	body, err := io.ReadAll(rsp.Body)
 	require.NoError(t, err)
+	require.Equal(t, 200, rsp.StatusCode, string(body))
 
 	var data data
 	err = json.Unmarshal(body, &data)
