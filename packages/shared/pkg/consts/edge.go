31ee097aef6a69d11ab0426e584030ddfd2727e4
ben-fornefeld <ben.fornefeld@gmail.com>
2026-03-02 16:45:39 -0800

    chore: rename compatibility header

--- packages/shared/pkg/consts/edge.go
+++ packages/shared/pkg/consts/edge.go
@@ -1,8 +1,8 @@
 package consts
 
 const (
-	EdgeApiAuthHeader                              = "X-API-Key"
-	EdgeRpcAuthHeader                              = "authorization"
-	EdgeRpcServiceInstanceIDHeader                 = "service-instance-id"
-	EdgeFeatureLogsLevelTextFilteringEnabledHeader = "X-E2B-Edge-Feature-Logs-Level-Text-Filtering-Enabled"
+	EdgeApiAuthHeader                                     = "X-API-Key"
+	EdgeRpcAuthHeader                                     = "authorization"
+	EdgeRpcServiceInstanceIDHeader                        = "service-instance-id"
+	EdgeFeatureSandboxLogsLevelTextFilteringEnabledHeader = "X-E2B-Edge-Feature-Sandbox-Logs-Level-Text-Filtering-Enabled"
 )
