e01e8218a0347d0bf4a719bcfb99ece263e72177
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-03-05 10:00:21 -0800

    continue to support the old variable

--- iac/provider-gcp/main.tf
+++ iac/provider-gcp/main.tf
@@ -40,6 +40,12 @@ data "google_secret_manager_secret_version" "routing_domains" {
 
 locals {
   additional_domains = nonsensitive(jsondecode(data.google_secret_manager_secret_version.routing_domains.secret_data))
+  additional_api_services = (
+    length(var.additional_api_services) > 0 ? var.additional_api_services :
+    var.additional_api_services_json != "" ? jsondecode(var.additional_api_services_json) :
+    []
+  )
+
 
   // Check if all clusters has size greater than 1
   template_manages_clusters_size_gt_1 = alltrue([for c in var.build_clusters_config : (c.cluster_size > 1)])
@@ -127,7 +133,7 @@ module "cluster" {
   domain_name                  = var.domain_name
 
   additional_domains      = local.additional_domains
-  additional_api_services = var.additional_api_services_json
+  additional_api_services = local.additional_api_services
 
   docker_contexts_bucket_name = module.init.envs_docker_context_bucket_name
   cluster_setup_bucket_name   = module.init.cluster_setup_bucket_name
--- iac/provider-gcp/variables.tf
+++ iac/provider-gcp/variables.tf
@@ -307,6 +307,26 @@ variable "domain_name" {
 }
 
 variable "additional_api_services_json" {
+  type        = string
+  description = <<EOT
+Deprecated. Use `additional_api_services` instead.
+
+Additional path rules to add to the API path matcher.
+Format: json string of an array of objects with 'path' and 'service' keys.
+Example:
+[
+  {
+    "paths": ["/api/v1"],
+    "service_id": "projects/e2b/global/backendServices/example",
+    "api_node_group_port_name": "example-port",
+    "api_node_group_port": 8080
+  }
+]
+EOT
+  default     = ""
+}
+
+variable "additional_api_services" {
   type = list(object({
     paths                    = list(string)
     service_id               = string
@@ -315,6 +335,7 @@ variable "additional_api_services_json" {
   }))
   description = "Additional path rules to add to the API path matcher."
   default     = []
+
 }
 
 variable "prefix" {
