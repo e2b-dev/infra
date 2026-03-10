937e5d3c9c8ade185a8fbdb22f541347ff59666b
Joseph Lombrozo <joe.lombrozo@e2b.dev>
2026-03-10 15:53:05 -0700

    Add support for traefik customization, clean terraform vars (#2059)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- iac/provider-gcp/variables.tf
+++ iac/provider-gcp/variables.tf
@@ -131,6 +131,17 @@ variable "ingress_count" {
   default = 1
 }
 
+variable "additional_api_paths_handled_by_ingress" {
+  type        = list(string)
+  description = "Additional paths to forward to nomad's ingress"
+  default     = []
+}
+
+variable "additional_traefik_arguments" {
+  type    = list(string)
+  default = []
+}
+
 variable "client_proxy_resources_memory_mb" {
   type    = number
   default = 1024
@@ -309,6 +320,8 @@ variable "domain_name" {
 variable "additional_api_services_json" {
   type        = string
   description = <<EOT
+Deprecated. Use `additional_api_services` instead.
+
 Additional path rules to add to the API path matcher.
 Format: json string of an array of objects with 'path' and 'service' keys.
 Example:
@@ -322,6 +335,17 @@ Example:
 ]
 EOT
   default     = ""
+}
+
+variable "additional_api_services" {
+  type = list(object({
+    paths                    = list(string)
+    service_id               = string
+    api_node_group_port_name = string
+    api_node_group_port      = number
+  }))
+  description = "Additional path rules to add to the API path matcher."
+  default     = []
 }
 
 variable "prefix" {
