766aad18626aae0153e04b63c66b1f0da980ecdc
Matt Brockman <matt.brockman@e2b.dev>
2026-03-18 15:52:58 -0700

    pull out splitoversizedranges to shared util

--- packages/orchestrator/internal/sandbox/block/cache_process_other.go
+++ packages/orchestrator/internal/sandbox/block/cache_process_other.go
@@ -14,22 +14,3 @@ func (c *Cache) copyProcessMemory(
 ) error {
 	return fmt.Errorf("copying process memory is only supported on Linux")
 }
-
-func splitOversizedRanges(ranges []Range, maxSize int64) (result []Range) {
-	for _, r := range ranges {
-		if r.Size <= maxSize {
-			result = append(result, r)
-
-			continue
-		}
-
-		for offset := int64(0); offset < r.Size; offset += maxSize {
-			result = append(result, Range{
-				Start: r.Start + offset,
-				Size:  min(r.Size-offset, maxSize),
-			})
-		}
-	}
-
-	return result
-}
