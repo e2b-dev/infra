01bd32ac312c69d4928efe7142e7799e1b985d72
Matt Brockman <matt.brockman@e2b.dev>
2026-03-20 17:08:41 -0700

    put in right place

--- packages/orchestrator/pkg/template/build/phases/base/provision.sh
+++ packages/orchestrator/pkg/template/build/phases/base/provision.sh
@@ -15,7 +15,7 @@ is_package_installed() {
 }
 
 # Install required packages if not already installed
-PACKAGES="systemd systemd-sysv openssh-server sudo chrony socat curl ca-certificates fuse3 iptables git nfs-common"
+PACKAGES="systemd systemd-sysv openssh-server sudo chrony socat curl ca-certificates fuse3 iptables git nfs-common e2fsprogs"
 echo "Checking presence of the following packages: $PACKAGES"
 
 MISSING=""
