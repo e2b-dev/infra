6a79d8d555f15d432d74004baf61f6d91d7d2c7c
Jiri Sveceny <jiri.sveceny@icloud.com>
2026-03-24 11:49:51 +0100

    Put data sources directly into main fail

--- iac/provider-aws/nomad/main.tf
+++ iac/provider-aws/nomad/main.tf
@@ -12,6 +12,26 @@ locals {
   orchestrator_artifact_source = "s3::https://${var.fc_env_pipeline_bucket_name}.s3.${var.aws_region}.amazonaws.com/orchestrator?etag=${data.aws_s3_object.orchestrator.etag}"
 }
 
+data "aws_ecr_image" "api" {
+  repository_name = var.api_repository_name
+  image_tag       = "latest"
+}
+
+data "aws_ecr_image" "db_migrator" {
+  repository_name = var.db_migrator_repository_name
+  image_tag       = "latest"
+}
+
+data "aws_ecr_image" "client_proxy" {
+  repository_name = var.client_proxy_repository_name
+  image_tag       = "latest"
+}
+
+data "aws_ecr_image" "clickhouse_migrator" {
+  repository_name = var.clickhouse_migrator_repository_name
+  image_tag       = "latest"
+}
+
 module "otel_collector" {
   source = "../../modules/job-otel-collector"
 
