1be339c70b527db9eb66490dc3473b21128a4b99
Jakub Novák <jakub@e2b.dev>
2026-03-27 14:16:24 +0100

    chore(orch): add additional check for instanceID (#2242)
    * chore(orch): add additional check for instanceID
    
    * chore: type safe check
    
    * fix: use old field name
    
    * chore: clean up the logic
    
    * chore: remove sandbox id from logs as they aren't related
    
    * fix: logs
    
    * fix: check
    
    * chore: add comment

--- packages/orchestrator/pkg/hyperloopserver/handlers/logs.go
+++ packages/orchestrator/pkg/hyperloopserver/handlers/logs.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
 
@@ -28,6 +29,14 @@ func (h *APIStore) Logs(c *gin.Context) {
 	payload := make(map[string]any)
 	if err := c.ShouldBindJSON(&payload); err != nil {
 		h.sendAPIStoreError(c, http.StatusBadRequest, "Invalid body for logs")
+		h.logger.Error(ctx, "error when parsing sandbox logs request", zap.Error(err), logger.WithSandboxID(sbxID))
+
+		return
+	}
+
+	err = h.validatePayloadSandboxID(payload, sbxID)
+	if err != nil {
+		h.sendAPIStoreError(c, http.StatusBadRequest, "Invalid sandboxID in logs payload")
 		h.logger.Error(ctx, "error when parsing sandbox logs request", zap.Error(err), logger.WithSandboxID(sbxID))
 
 		return
@@ -64,4 +73,22 @@ func (h *APIStore) Logs(c *gin.Context) {
 	defer response.Body.Close()
 
 	c.Status(http.StatusOK)
+}
+
+// validatePayloadSandboxID checks if the payload contains correct instanceID to prevent slow requests to contaminating the logs of other sandboxes.
+func (h *APIStore) validatePayloadSandboxID(payload map[string]any, sbxID string) error {
+	if payload["instanceID"] == nil {
+		return fmt.Errorf("missing sandboxID in logs payload")
+	}
+
+	payloadSandboxID, ok := payload["instanceID"].(string)
+	if !ok {
+		return fmt.Errorf("instanceID in logs payload is not a string: %v", payload["instanceID"])
+	}
+
+	if payloadSandboxID != sbxID {
+		return fmt.Errorf("sandboxID in logs payload does not match the sandboxID of the source sandbox (%s != %s)", payloadSandboxID, sbxID)
+	}
+
+	return nil
 }
