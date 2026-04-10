6f83d521459b57c2ab9ded6344bed4ee451db2ca
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-04-10 14:40:09 -0700

    defer span in case next causes a panic

--- packages/orchestrator/pkg/nfsproxy/o11y/tracing.go
+++ packages/orchestrator/pkg/nfsproxy/o11y/tracing.go
@@ -14,21 +14,22 @@ import (
 func Tracing(skipOps map[string]bool) middleware.Interceptor {
 	tracer := otel.Tracer("github.com/e2b-dev/infra/packages/orchestrator/pkg/nfsproxy/o11y")
 
-	return func(ctx context.Context, op string, args []any, next func(context.Context) error) error {
+	return func(ctx context.Context, op string, args []any, next func(context.Context) error) (err error) {
 		if skipOps[op] {
 			return next(ctx)
 		}
 
-		ctx, span := tracer.Start(ctx, op, trace.WithAttributes(argsToAttrs(op, args)...)) //nolint:spancheck // span.End called below
-		err := next(ctx)
-		if err != nil {
-			span.RecordError(err)
-			if !isUserError(err) {
-				span.SetStatus(codes.Error, err.Error())
+		ctx, span := tracer.Start(ctx, op, trace.WithAttributes(argsToAttrs(op, args)...))
+		defer func() {
+			if err != nil {
+				span.RecordError(err)
+				if !isUserError(err) {
+					span.SetStatus(codes.Error, err.Error())
+				}
 			}
-		}
-		span.End()
+			span.End()
+		}()
 
-		return err
+		return next(ctx)
 	}
 }
