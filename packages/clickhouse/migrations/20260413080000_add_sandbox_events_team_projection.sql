a8496f2521c27a8cca6c191167de346991e1a432
Jakub Novak <jakub@e2b.dev>
2026-04-13 13:28:26 +0000

    chore: add comment

--- packages/clickhouse/migrations/20260413080000_add_sandbox_events_team_projection.sql
+++ packages/clickhouse/migrations/20260413080000_add_sandbox_events_team_projection.sql
@@ -1,8 +1,10 @@
 -- +goose Up
--- Add a projection ordered by (sandbox_team_id, sandbox_id, timestamp) so that
--- queries filtering by sandbox_team_id can skip the full table scan.
+-- TEMPORARY: Add a projection ordered by (sandbox_team_id, sandbox_id, timestamp)
+-- so that queries filtering by sandbox_team_id can skip the full table scan.
 -- The base ORDER BY is (sandbox_id, timestamp) which doesn't help when only
 -- sandbox_team_id is provided.
+-- This projection should be removed once we migrate the table's ORDER BY to
+-- (sandbox_team_id, sandbox_id, timestamp) via a CREATE + RENAME swap.
 ALTER TABLE sandbox_events_local
     ADD PROJECTION proj_team_id (
         SELECT * ORDER BY sandbox_team_id, sandbox_id, timestamp
