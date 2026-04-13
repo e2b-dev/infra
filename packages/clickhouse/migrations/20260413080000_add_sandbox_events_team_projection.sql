f5815c3e72f511bfbb37162d38fc2bf7a98f1af2
Jakub Novak <jakub@e2b.dev>
2026-04-13 13:06:30 +0000

    chore(clickhouse): add projection for events table

---
+++ packages/clickhouse/migrations/20260413080000_add_sandbox_events_team_projection.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- Add a projection ordered by (sandbox_team_id, sandbox_id, timestamp) so that
+-- queries filtering by sandbox_team_id can skip the full table scan.
+-- The base ORDER BY is (sandbox_id, timestamp) which doesn't help when only
+-- sandbox_team_id is provided.
+ALTER TABLE sandbox_events_local
+    ADD PROJECTION proj_team_id (
+        SELECT * ORDER BY sandbox_team_id, sandbox_id, timestamp
+    );
+
+ALTER TABLE sandbox_events_local
+    MATERIALIZE PROJECTION proj_team_id;
+
+-- +goose Down
+ALTER TABLE sandbox_events_local
+    DROP PROJECTION IF EXISTS proj_team_id;
