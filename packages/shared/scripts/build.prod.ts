44840c2a4ed6caf1e11a1670c4bc879757f5d0eb
Joe Lombrozo <joe.lombrozo@e2b.dev>
2026-04-16 14:02:51 -0700

    use cache flag correctly

--- packages/shared/scripts/build.prod.ts
+++ packages/shared/scripts/build.prod.ts
@@ -2,10 +2,15 @@ import { Template } from "e2b";
 import { template } from "./template.js";
 
 async function main() {
+  // default to not using cache, unless they ask for it
+  const useCache = process.env.SKIP_CACHE === 'true';
+
+  console.log(`cache = ${useCache ? 'enabled' : 'disabled'}`);
+
   await Template.build(template, {
     alias: "base",
     memoryMB: 512,
-    skipCache: process.env.SKIP_CACHE !== 'false',
+    skipCache: !useCache,
     onBuildLogs: (it) => console.log(it.toString()),
   });
 }
