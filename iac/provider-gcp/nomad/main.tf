6b8a5808290b36339d748bb4b2dbd76d6a9db978
Jakub Novak <jakub@e2b.dev>
2026-04-26 16:38:21 +0200

    chore: clean up gcp zone

--- iac/provider-gcp/nomad/main.tf
+++ iac/provider-gcp/nomad/main.tf
@@ -190,7 +190,6 @@ module "redis" {
 resource "nomad_job" "docker_reverse_proxy" {
   jobspec = templatefile("${path.module}/jobs/docker-reverse-proxy.hcl",
     {
-      gcp_zone                      = var.gcp_zone
       node_pool                     = var.api_node_pool
       image_name                    = data.google_artifact_registry_docker_image.docker_reverse_proxy_image.self_link
       postgres_connection_string    = data.google_secret_manager_secret_version.postgres_connection_string.secret_data
