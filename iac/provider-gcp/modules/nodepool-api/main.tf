6aa7261bd4cfbe8b88831a2862fc959738d15741
Jakub Novak <jakub@e2b.dev>
2026-04-26 16:31:33 +0200

    chore: keep the module

--- iac/provider-gcp/modules/nodepool-api/main.tf
+++ iac/provider-gcp/modules/nodepool-api/main.tf
@@ -1,27 +1,20 @@
 locals {
-  loki_pool_name = "${var.prefix}orch-loki"
-
-  loki_startup_script = templatefile("${path.module}/../modules/nodepool-api/scripts/start-api.sh", {
+  startup_script = templatefile("${path.module}/scripts/start-api.sh", {
     CLUSTER_TAG_NAME             = var.cluster_tag_name
     SCRIPTS_BUCKET               = var.cluster_setup_bucket_name
-    FC_KERNELS_BUCKET_NAME       = var.fc_kernels_bucket_name
-    FC_VERSIONS_BUCKET_NAME      = var.fc_versions_bucket_name
-    FC_ENV_PIPELINE_BUCKET_NAME  = var.fc_env_pipeline_bucket_name
-    DOCKER_CONTEXTS_BUCKET_NAME  = var.docker_contexts_bucket_name
     GCP_REGION                   = var.gcp_region
     GOOGLE_SERVICE_ACCOUNT_KEY   = var.google_service_account_key
-    NOMAD_TOKEN                  = var.nomad_acl_token_secret
     CONSUL_TOKEN                 = var.consul_acl_token_secret
-    RUN_CONSUL_FILE_HASH         = local.file_hash["scripts/run-consul.sh"]
-    RUN_NOMAD_FILE_HASH          = local.file_hash["scripts/run-nomad.sh"]
-    CONSUL_GOSSIP_ENCRYPTION_KEY = google_secret_manager_secret_version.consul_gossip_encryption_key.secret_data
-    CONSUL_DNS_REQUEST_TOKEN     = google_secret_manager_secret_version.consul_dns_request_token.secret_data
-    NODE_POOL                    = var.loki_node_pool
+    RUN_CONSUL_FILE_HASH         = var.file_hash["scripts/run-consul.sh"]
+    RUN_NOMAD_FILE_HASH          = var.file_hash["scripts/run-nomad.sh"]
+    CONSUL_GOSSIP_ENCRYPTION_KEY = var.consul_gossip_encryption_key_secret_data
+    CONSUL_DNS_REQUEST_TOKEN     = var.consul_dns_request_token_secret_data
+    NODE_POOL                    = var.node_pool
   })
 }
 
-resource "google_compute_health_check" "loki_nomad_check" {
-  name                = "${local.loki_pool_name}-nomad-loki-check"
+resource "google_compute_health_check" "nomad_check" {
+  name                = "${var.cluster_name}-nomad-check"
   check_interval_sec  = 15
   timeout_sec         = 10
   healthy_threshold   = 2
@@ -37,50 +30,84 @@ resource "google_compute_health_check" "loki_nomad_check" {
   }
 }
 
-resource "google_compute_instance_group_manager" "loki_pool" {
-  name = "${local.loki_pool_name}-ig"
+data "google_compute_zones" "region_zones" {
+  region = var.gcp_region
+}
+
+resource "google_compute_region_instance_group_manager" "pool" {
+  name   = "${var.cluster_name}-rig"
+  region = var.gcp_region
 
   version {
-    name              = google_compute_instance_template.loki.id
-    instance_template = google_compute_instance_template.loki.id
+    name              = google_compute_instance_template.template.id
+    instance_template = google_compute_instance_template.template.id
   }
 
+  named_port {
+    name = var.client_proxy_health_port.name
+    port = var.client_proxy_health_port.port
+  }
+
+  named_port {
+    name = var.client_proxy_port.name
+    port = var.client_proxy_port.port
+  }
+
+  named_port {
+    name = var.api_port.name
+    port = var.api_port.port
+  }
+
+  named_port {
+    name = var.docker_reverse_proxy_port.name
+    port = var.docker_reverse_proxy_port.port
+  }
+
+  named_port {
+    name = var.ingress_port.name
+    port = var.ingress_port.port
+  }
 
   auto_healing_policies {
-    health_check      = google_compute_health_check.loki_nomad_check.id
+    health_check      = google_compute_health_check.nomad_check.id
     initial_delay_sec = 600
   }
+
+  distribution_policy_target_shape = "BALANCED"
 
   # Server is a stateful cluster, so the update strategy used to roll out a new GCE Instance Template must be
   # a rolling update.
   update_policy {
     type                    = var.environment == "dev" ? "PROACTIVE" : "OPPORTUNISTIC"
     minimal_action          = "REPLACE"
-    max_surge_fixed         = 1
     max_surge_percent       = null
-    max_unavailable_fixed   = 1
     max_unavailable_percent = null
     replacement_method      = "SUBSTITUTE"
+
+    max_surge_fixed       = length(data.google_compute_zones.region_zones.names)
+    max_unavailable_fixed = 0
+
+    instance_redistribution_type = "NONE"
   }
 
-  base_instance_name = local.loki_pool_name
-  target_size        = var.loki_cluster_size
+  base_instance_name = var.cluster_name
+  target_size        = var.cluster_size
   target_pools       = []
 
   depends_on = [
-    google_compute_instance_template.loki,
+    google_compute_instance_template.template,
   ]
 }
 
-data "google_compute_image" "loki_source_image" {
-  family = var.api_image_family
+data "google_compute_image" "source_image" {
+  family = var.image_family
 }
 
-resource "google_compute_instance_template" "loki" {
-  name_prefix = "${local.loki_pool_name}-"
+resource "google_compute_instance_template" "template" {
+  name_prefix = "${var.cluster_name}-"
 
   instance_description = null
-  machine_type         = var.loki_machine_type
+  machine_type         = var.machine_type
 
   labels = merge(
     var.labels,
@@ -89,9 +116,9 @@ resource "google_compute_instance_template" "loki" {
     } : {})
   )
   tags                    = [var.cluster_tag_name]
-  metadata_startup_script = local.loki_startup_script
+  metadata_startup_script = local.startup_script
   metadata = merge(
-    { loki_cluster = "TRUE" },
+    { api_cluster = "TRUE" },
     {
       enable-osconfig         = "TRUE",
       enable-guest-attributes = "TRUE",
@@ -104,16 +131,16 @@ resource "google_compute_instance_template" "loki" {
 
   disk {
     boot         = true
-    source_image = data.google_compute_image.loki_source_image.id
+    source_image = data.google_compute_image.source_image.id
     disk_size_gb = 200
-    disk_type    = var.loki_boot_disk_type
+    disk_type    = var.boot_disk_type
   }
 
   network_interface {
     network = var.network_name
 
     dynamic "access_config" {
-      for_each = ["public_ip"]
+      for_each = var.api_use_nat ? [] : ["public_ip"]
       content {}
     }
   }
@@ -136,9 +163,9 @@ resource "google_compute_instance_template" "loki" {
   # which this Terraform resource depends will also need this lifecycle statement.
   lifecycle {
     create_before_destroy = true
-  }
 
-  depends_on = [
-    google_storage_bucket_object.setup_config_objects,
-  ]
+    # TODO: Temporary workaround to avoid unnecessary updates to the instance template.
+    #  This should be removed once cluster size is removed from the metadata
+    ignore_changes = [metadata]
+  }
 }
