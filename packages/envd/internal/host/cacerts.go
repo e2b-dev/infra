e0ca9601c992417eee5269cc4f5ee1e2f3517186
ValentaTomas <valenta.and.thomas@gmail.com>
2026-05-17 22:50:17 -0700

    nolint contextcheck on detached cleanup goroutine

--- packages/envd/internal/host/cacerts.go
+++ packages/envd/internal/host/cacerts.go
@@ -112,7 +112,7 @@ func (c *CACertInstaller) install(ctx context.Context, certPEM, bundlePath, extr
 		Dur("append_duration", time.Since(start)).
 		Msg("CA cert appended to bundle")
 
-	go func() {
+	go func() { //nolint:contextcheck // background cleanup must outlive the caller's ctx
 		cleanStart := time.Now()
 
 		_ = c.mu.Acquire(context.Background(), 1)
