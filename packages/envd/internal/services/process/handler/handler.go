56dfd6c36be2831bef363afd2a75eb7466707de1
ValentaTomas <valenta.and.thomas@gmail.com>
2026-05-20 13:10:58 -0700

    fix(envd): skip pty event clones without subscribers

--- packages/envd/internal/services/process/handler/handler.go
+++ packages/envd/internal/services/process/handler/handler.go
@@ -207,14 +207,17 @@ func New(
 
 				if n > 0 {
 					h.ptyBytes.Add(int64(n))
-					data := slices.Clone(readBuf[:n])
 
-					outMultiplex.Source <- rpc.ProcessEvent_Data{
-						Data: &rpc.ProcessEvent_DataEvent{
-							Output: &rpc.ProcessEvent_DataEvent_Pty{
-								Pty: data,
+					if outMultiplex.HasSubscribers() {
+						data := slices.Clone(readBuf[:n])
+
+						outMultiplex.Source <- rpc.ProcessEvent_Data{
+							Data: &rpc.ProcessEvent_DataEvent{
+								Output: &rpc.ProcessEvent_DataEvent_Pty{
+									Pty: data,
+								},
 							},
-						},
+						}
 					}
 				}
 
