e24bbc8c6914a8071fdbc1645996f4dcfe533625
ValentaTomas <valenta.and.thomas@gmail.com>
2026-05-20 12:03:08 -0700

    fix(envd): start pty after stream subscription
    Open the pty in handler setup but start the process only after Start registers output subscribers.

--- packages/envd/internal/services/process/handler/handler.go
+++ packages/envd/internal/services/process/handler/handler.go
@@ -53,9 +53,10 @@ type Handler struct {
 
 	logger *zerolog.Logger
 
-	Tag *string
-	cmd *exec.Cmd
-	tty *os.File
+	Tag      *string
+	cmd      *exec.Cmd
+	tty      *os.File
+	ttySlave *os.File
 
 	cancel context.CancelFunc
 
@@ -196,15 +197,28 @@ func New(
 	}
 
 	if req.GetPty() != nil {
-		// The pty should ideally start only in the Start method, but the package does not support that and we would have to code it manually.
-		// The output of the pty should correctly be passed though.
-		tty, err := pty.StartWithSize(cmd, &pty.Winsize{
+		tty, ttySlave, err := pty.Open()
+		if err != nil {
+			return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("error opening pty for command '%s': %w", userCmd, err))
+		}
+
+		if err := pty.Setsize(tty, &pty.Winsize{
 			Cols: uint16(req.GetPty().GetSize().GetCols()),
 			Rows: uint16(req.GetPty().GetSize().GetRows()),
-		})
-		if err != nil {
-			return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("error starting pty with command '%s' in dir '%s' with '%d' cols and '%d' rows: %w", userCmd, cmd.Dir, req.GetPty().GetSize().GetCols(), req.GetPty().GetSize().GetRows(), err))
+		}); err != nil {
+			_ = tty.Close()
+			_ = ttySlave.Close()
+
+			return nil, connect.NewError(connect.CodeInvalidArgument, fmt.Errorf("error sizing pty for command '%s': %w", userCmd, err))
 		}
+
+		cmd.Stdin = ttySlave
+		cmd.Stdout = ttySlave
+		cmd.Stderr = ttySlave
+		cmd.SysProcAttr.Setsid = true
+		cmd.SysProcAttr.Setctty = true
+		h.tty = tty
+		h.ttySlave = ttySlave
 
 		outWg.Go(func() {
 			readBufPtr := outputBufferPool.Get().(*[]byte)
@@ -241,8 +255,6 @@ func New(
 				}
 			}
 		})
-
-		h.tty = tty
 	} else {
 		stdout, err := cmd.StdoutPipe()
 		if err != nil {
@@ -433,12 +445,22 @@ func (p *Handler) WriteTty(data []byte) error {
 }
 
 func (p *Handler) Start(requestTimeout time.Duration) (uint32, error) {
-	// Pty is already started in the New method
-	if p.tty == nil {
-		err := p.cmd.Start()
-		if err != nil {
-			return 0, fmt.Errorf("error starting process '%s': %w", p.userCommand(), err)
+	err := p.cmd.Start()
+	if err != nil {
+		if p.tty != nil {
+			_ = p.tty.Close()
 		}
+
+		if p.ttySlave != nil {
+			_ = p.ttySlave.Close()
+		}
+
+		return 0, fmt.Errorf("error starting process '%s': %w", p.userCommand(), err)
+	}
+
+	if p.ttySlave != nil {
+		_ = p.ttySlave.Close()
+		p.ttySlave = nil
 	}
 
 	p.logger.
@@ -458,7 +480,9 @@ func (p *Handler) Wait() {
 
 	err := p.cmd.Wait()
 
-	p.tty.Close()
+	if p.tty != nil {
+		_ = p.tty.Close()
+	}
 
 	var errMsg *string
 
