42ef409d7d17859ec8da951dad7ff9cae7b88bf6
Igor Katsuba <i.katsuba@manifestlabs.com>
2026-05-21 16:55:32 +0200

    fix(db): set base_v1 disk_mb to 8 GB instead of 16 GB

--- packages/db/migrations/20260521120000_bump_base_v1_disk_mb.sql
+++ packages/db/migrations/20260521120000_bump_base_v1_disk_mb.sql
@@ -1,13 +1,13 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE tiers
-    ALTER COLUMN "disk_mb" SET DEFAULT '16384'::bigint;
-UPDATE tiers SET "disk_mb" = 16384 WHERE id = 'base_v1' AND "disk_mb" = 512;
+    ALTER COLUMN "disk_mb" SET DEFAULT '8192'::bigint;
+UPDATE tiers SET "disk_mb" = 8192 WHERE id = 'base_v1' AND "disk_mb" = 512;
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 ALTER TABLE tiers
     ALTER COLUMN "disk_mb" SET DEFAULT '512'::bigint;
-UPDATE tiers SET "disk_mb" = 512 WHERE id = 'base_v1' AND "disk_mb" = 16384;
+UPDATE tiers SET "disk_mb" = 512 WHERE id = 'base_v1' AND "disk_mb" = 8192;
 -- +goose StatementEnd
