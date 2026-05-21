fad7adf0778a91fdde1da75dd59b3bd82babf211
Jakub Novak <jakub@e2b.dev>
2026-05-21 05:18:39 +0000

    chore(tests): add auto resume to periodic tests

--- tests/periodic-test/snapshot-and-resume.ts
+++ tests/periodic-test/snapshot-and-resume.ts
@@ -1,7 +1,10 @@
 import { Sandbox } from "@e2b/code-interpreter";
 import { DEBUG_TIMEOUT_MS, log, runTestWithSandbox } from "./utils.ts";
 
-const sbx = await Sandbox.create({ timeoutMs: DEBUG_TIMEOUT_MS });
+const sbx = await Sandbox.create({
+  timeoutMs: DEBUG_TIMEOUT_MS,
+  lifecycle: { autoResume: true, onTimeout: "pause" },
+});
 log("ℹ️ sandbox created", sbx.sandboxId);
 
 await runTestWithSandbox(sbx, "snapshot-and-resume", async () => {
@@ -10,14 +13,14 @@ await runTestWithSandbox(sbx, "snapshot-and-resume", async () => {
   });
   log("Sandbox code executed");
 
-  const success = await sbx.betaPause();
+  let success = await sbx.betaPause();
   log("Sandbox paused", success);
 
   // Resume the sandbox from the same state
-  const sameSbx = await Sandbox.connect(sbx.sandboxId);
+  let sameSbx = await Sandbox.connect(sbx.sandboxId);
   log("Sandbox resumed", sameSbx.sandboxId);
 
-  const execution = await sameSbx.runCode("x+=1; x", {
+  let execution = await sameSbx.runCode("x+=1; x", {
     requestTimeoutMs: 10000,
   });
   // Output result
@@ -28,4 +31,20 @@ await runTestWithSandbox(sbx, "snapshot-and-resume", async () => {
     throw new Error("Failed to runCode in resumed sandbox");
   }
   log("Sandbox resumed successfully");
+
+  success = await sbx.betaPause();
+  log("Sandbox paused", success);
+
+  execution = await sameSbx.runCode("x+=1; x", {
+    requestTimeoutMs: 10000,
+  });
+  // Output result
+  log("RunCode Output:", execution.text);
+
+  if (execution.text !== "3") {
+    log("Test failed:", "The expected runCode output doesn't match");
+    throw new Error("Failed to runCode in auto resumed sandbox");
+  }
+
+  log("Sandbox auto resumed successfully");
 });
