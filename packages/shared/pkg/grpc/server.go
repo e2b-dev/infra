a0377ed2b236239b6845da0162a5519637eba70e
uweltman <uweltman@box.com>
2026-07-07 23:08:41 -0700

    Add opt-in TLS for inter-service gRPC connections
    (cherry picked from commit fe3d95764)

--- packages/shared/pkg/grpc/server.go
+++ packages/shared/pkg/grpc/server.go
@@ -2,7 +2,10 @@ package grpc
 
 import (
 	"context"
+	"crypto/tls"
 	"time"
+
+	"go.uber.org/zap"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
@@ -10,6 +13,7 @@ import (
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.opentelemetry.io/otel/attribute"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/metadata"
 
@@ -22,12 +26,32 @@ type ServerOption func(*serverOptions)
 
 type serverOptions struct {
 	withSandboxResumeMetrics bool
+	certFile                 string
+	keyFile                  string
+	certPEM                  []byte
+	keyPEM                   []byte
 }
 
 // WithSandboxResumeMetrics adds sandbox.resume attribute to otelgrpc metrics,
 // read from incoming gRPC metadata.
 func WithSandboxResumeMetrics() ServerOption {
 	return func(o *serverOptions) { o.withSandboxResumeMetrics = true }
+}
+
+// WithTLS configures server-side TLS using the given certificate and key files.
+func WithTLS(certFile, keyFile string) ServerOption {
+	return func(o *serverOptions) {
+		o.certFile = certFile
+		o.keyFile = keyFile
+	}
+}
+
+// WithTLSFromPEM configures server-side TLS using in-memory PEM-encoded certificate and key.
+func WithTLSFromPEM(certPEM, keyPEM []byte) ServerOption {
+	return func(o *serverOptions) {
+		o.certPEM = certPEM
+		o.keyPEM = keyPEM
+	}
 }
 
 func NewGRPCServer(tel *telemetry.Client, opts ...ServerOption) *grpc.Server {
@@ -57,7 +81,7 @@ func NewGRPCServer(tel *telemetry.Client, opts ...ServerOption) *grpc.Server {
 		otelOpts = append(otelOpts, otelgrpc.WithMetricAttributesFn(extractSandboxResumeAttrs))
 	}
 
-	return grpc.NewServer(
+	serverOpts := []grpc.ServerOption{
 		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
 			MinTime:             5 * time.Second,
 			PermitWithoutStream: true,
@@ -82,7 +106,30 @@ func NewGRPCServer(tel *telemetry.Client, opts ...ServerOption) *grpc.Server {
 				ignoredLoggingRoutes,
 			),
 		),
-	)
+	}
+
+	if cfg.certFile != "" && cfg.keyFile != "" {
+		creds, err := credentials.NewServerTLSFromFile(cfg.certFile, cfg.keyFile)
+		if err != nil {
+			logger.L().Fatal(context.Background(), "failed to load gRPC TLS credentials",
+				zap.String("certFile", cfg.certFile),
+				zap.String("keyFile", cfg.keyFile),
+				zap.Error(err),
+			)
+		}
+
+		serverOpts = append(serverOpts, grpc.Creds(creds))
+	} else if len(cfg.certPEM) > 0 && len(cfg.keyPEM) > 0 {
+		cert, err := tls.X509KeyPair(cfg.certPEM, cfg.keyPEM)
+		if err != nil {
+			logger.L().Fatal(context.Background(), "failed to parse gRPC TLS PEM credentials", zap.Error(err))
+		}
+
+		creds := credentials.NewServerTLSFromCert(&cert)
+		serverOpts = append(serverOpts, grpc.Creds(creds))
+	}
+
+	return grpc.NewServer(serverOpts...)
 }
 
 // extractSandboxResumeAttrs reads sandbox.resume from gRPC metadata set by the
